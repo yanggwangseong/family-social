aeeff2ca441ab29f6c4279a1c2aeda45a74ea4ad
YangGwangSeong <rhkdtjd_12@naver.com>
2023-08-08 05:06:50 -0700

    chore: backend api prefix설정

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import Image from 'next/image';
 import { Inter } from 'next/font/google';
 
@@ -11,9 +12,10 @@ export default function Home() {
 			<div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
 				<p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
 					Get started by editing&nbsp;
-					<code className="font-mono font-bold">11</code>
+					<code className="font-mono font-bold">22222222222222</code>
 				</p>
 			</div>
+			<div className="flex bg-violet-900">hihi</div>
 
 			<div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
 				<a
