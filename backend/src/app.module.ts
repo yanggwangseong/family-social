ea67b07c5f09be5d7279bd9098ea66c2c05161f2
YangGwangSeong <rhkdtjd_12@naver.com>
2023-08-09 05:53:21 -0700

    chore: backend cors 설정

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,8 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+	imports: [],
+	controllers: [AppController],
+	providers: [AppService],
 })
 export class AppModule {}
+// TODO config module 설정
+// TODO TypeOrmModule module 설정
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import cookieParser from 'cookie-parser';
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
+	const options = {
+		origin: true,
+		methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+		preflightContinue: false,
+		credentials: true,
+		allowedHeaders: 'Content-Type, Accept, Authorization',
+	};
+	app.enableCors(options);
+	app.use(cookieParser());
 
 	app.setGlobalPrefix('api');
 	await app.listen(5001);
