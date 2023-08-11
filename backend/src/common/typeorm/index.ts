b9730ab175feb6233573bb8f0699db36513f13b1
YangGwangSeong <rhkdtjd_12@naver.com>
2023-08-11 07:18:01 -0700

    chore: db connet test

--- backend/src/common/typeorm/index.ts
+++ backend/src/common/typeorm/index.ts
@@ -7,18 +7,25 @@ export const TypeOrmModuleOptions = {
 		const NODE_ENV = configService.get('NODE_ENV');
 
 		const option = {
-			type: configService.get('DB_TYPE'),
-			host: configService.get('DB_HOST'),
-			port: Number(configService.get<number>('DB_PORT')),
-			username: configService.get('DB_USERNAME'),
-			database: configService.get('DB_DATABASE'),
-			password: configService.get('DB_PASSWORD'),
-			entities: [__dirname + '/../../**/*.entity.{js,ts}'],
+			// type: configService.get('DB_TYPE'),
+			// host: configService.get('DB_HOST'),
+			// port: Number(configService.get<number>('DB_PORT')),
+			// username: configService.get('DB_USERNAME'),
+			// database: configService.get('DB_DATABASE'),
+			// password: configService.get('DB_PASSWORD'),
+			// entities: [__dirname + '/../../**/*.entity.{js,ts}'],
 			// synchronize: NODE_ENV === 'development' ? true : false,
+			// ...(NODE_ENV === 'development'
+			// 	? { retryAttempts: 10, logging: true }
+			// 	: { logging: false }),
+			type: 'postgres',
+			host: process.env.DB_HOST,
+			port: 5432,
+			username: process.env.DB_USERNAME,
+			password: process.env.DB_PASSWORD,
+			database: process.env.DB_DATABASE,
+			entities: [__dirname + '/../../**/*.entity.{js,ts}'],
 			synchronize: true,
-			...(NODE_ENV === 'development'
-				? { retryAttempts: 10, logging: true }
-				: { logging: false }),
 		};
 
 		return option;
