9695c84e6f1393a3ed3f9b0b7241defe617bb137
YangGwangSeong <rhkdtjd_12@naver.com>
2023-08-10 20:25:12 -0700

    chore: config module global setting

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-	imports: [],
+	imports: [
+		ConfigModule.forRoot({
+			envFilePath: [`${__dirname}/../.${process.env.NODE_ENV}.env`],
+			isGlobal: true,
+		}),
+	],
 	controllers: [AppController],
 	providers: [AppService],
 })
