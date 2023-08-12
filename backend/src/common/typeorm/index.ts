e0f6652a973b1236216e177648bead84dbe94537
YangGwangSeong <rhkdtjd_12@naver.com>
2023-08-11 21:09:51 -0700

    chore: typeorm forRootAsync로 다시 변경

--- backend/src/common/typeorm/index.ts
+++ backend/src/common/typeorm/index.ts
@@ -1,39 +1,39 @@
 import { ConfigModule, ConfigService } from '@nestjs/config';
-import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
-// export const TypeOrmModuleOptions = {
-// 	imports: [ConfigModule],
-// 	inject: [ConfigService],
-// 	useFactory: async (configService: ConfigService) => {
-// 		const NODE_ENV = configService.get('NODE_ENV');
+export const TypeOrmModuleOptions = {
+	imports: [ConfigModule],
+	inject: [ConfigService],
+	useFactory: async (configService: ConfigService) => {
+		const NODE_ENV = configService.get('NODE_ENV');
 
-// 		const option = {
-// 			type: configService.get('DB_TYPE'),
-// 			host: configService.get('DB_HOST'),
-// 			port: Number(configService.get<number>('DB_PORT')),
-// 			username: configService.get('DB_USERNAME'),
-// 			database: configService.get('DB_DATABASE'),
-// 			password: configService.get('DB_PASSWORD'),
-// 			entities: [__dirname + '/../../**/*.entity.{js,ts}'],
-// 			synchronize: NODE_ENV === 'development' ? true : false,
-// 			...(NODE_ENV === 'development'
-// 				? { retryAttempts: 10, logging: true }
-// 				: { logging: false }),
-// 		};
+		const option = {
+			type: configService.get('DB_TYPE'),
+			host: configService.get('DB_HOST'),
+			port: Number(configService.get<number>('DB_PORT')),
+			username: configService.get('DB_USERNAME'),
+			database: configService.get('DB_DATABASE'),
+			password: configService.get('DB_PASSWORD'),
+			entities: [__dirname + '/../../**/*.entity.{js,ts}'],
+			synchronize: NODE_ENV === 'development' ? true : false,
+			ssl: NODE_ENV === 'development' ? true : { rejectUnauthorized: false },
+			...(NODE_ENV === 'development'
+				? { retryAttempts: 10, logging: true }
+				: { logging: false }),
+		};
 
-// 		return option;
-// 	},
-// };
-export const typeORMConfig: TypeOrmModuleOptions = {
-	type: 'postgres',
-	host: process.env.DB_HOST,
-	port: 5432,
-	username: process.env.DB_USERNAME,
-	password: process.env.DB_PASSWORD,
-	database: process.env.DB_DATABASE,
-	entities: [__dirname + '/../../**/*.entity.{js,ts}'],
-	synchronize: true,
-	ssl: {
-		rejectUnauthorized: false,
+		return option;
 	},
 };
+// export const typeORMConfig: TypeOrmModuleOptions = {
+// 	type: 'postgres',
+// 	host: process.env.DB_HOST,
+// 	port: 5432,
+// 	username: process.env.DB_USERNAME,
+// 	password: process.env.DB_PASSWORD,
+// 	database: process.env.DB_DATABASE,
+// 	entities: [__dirname + '/../../**/*.entity.{js,ts}'],
+// 	synchronize: true,
+// 	ssl: {
+// 		rejectUnauthorized: false,
+// 	},
+// };
