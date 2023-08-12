bc90a808cd9ec7f96998e95c282404593d4d45ff
YangGwangSeong <rhkdtjd_12@naver.com>
2023-08-12 00:20:41 -0700

    :sparkles: Feat: swagger 추가 (#1)

--- backend/src/users/users.controller.ts
+++ backend/src/users/users.controller.ts
@@ -4,9 +4,4 @@ import { UsersService } from './users.service';
 @Controller('users')
 export class UsersController {
 	constructor(private readonly usersService: UsersService) {}
-
-	@Get()
-	async test() {
-		return await this.usersService.test();
-	}
 }
--- backend/src/users/users.service.ts
+++ backend/src/users/users.service.ts
@@ -1,23 +1,6 @@
-import { UserEntity } from '@/entities/user.entity';
 import { Injectable } from '@nestjs/common';
-import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
-import { v4 as uuidv4 } from 'uuid';
 
 @Injectable()
 export class UsersService {
-	constructor(
-		@InjectRepository(UserEntity)
-		private usersRepository: Repository<UserEntity>,
-	) {}
-
-	async test() {
-		const user = new UserEntity();
-		user.id = uuidv4();
-		user.name = 'test';
-		const createUser = await this.usersRepository.save(user);
-		//return createUser;
-
-		return await this.usersRepository.find();
-	}
+	constructor() {}
 }
