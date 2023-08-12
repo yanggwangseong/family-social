7647d959d554b230f1258ee80052b2738c7a606d
YangGwangSeong <rhkdtjd_12@naver.com>
2023-08-11 20:20:07 -0700

    chore: rejectUnauthorized 설정

--- backend/src/common/typeorm/index.ts
+++ backend/src/common/typeorm/index.ts
@@ -26,11 +26,14 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 // };
 export const typeORMConfig: TypeOrmModuleOptions = {
 	type: 'postgres',
-	host: process.env.DB_HOST,
+	host: `${process.env.DB_HOST}`,
 	port: 5432,
-	username: process.env.DB_USERNAME,
-	password: process.env.DB_PASSWORD,
-	database: process.env.DB_DATABASE,
+	username: `${process.env.DB_USERNAME}`,
+	password: `${process.env.DB_PASSWORD}`,
+	database: `${process.env.DB_DATABASE}`,
 	entities: [__dirname + '/../../**/*.entity.{js,ts}'],
 	synchronize: true,
+	ssl: {
+		rejectUnauthorized: false,
+	},
 };
