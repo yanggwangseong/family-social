513f2d4a87be266a280d9bab0ce582a76339dd1e
YangGwangSeong <rhkdtjd_12@naver.com>
2023-08-13 04:35:15 -0700

    :sparkles: feat: 클라이언트 전역 상태관리를 위한 recoil 설치 및 적용 (#3)

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,6 +1,23 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
+import React from 'react';
+import { Hydrate, QueryClient, QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
+import { RecoilRoot } from 'recoil';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+	const queryClient = React.useRef(new QueryClient());
+
+	return (
+		<QueryClientProvider client={queryClient.current}>
+			<Hydrate state={pageProps.dehydratedState}>
+				<RecoilRoot>
+					<Component {...pageProps} />
+					{process.env.NODE_ENV === 'development' && (
+						<ReactQueryDevtools initialIsOpen={false} />
+					)}
+				</RecoilRoot>
+			</Hydrate>
+		</QueryClientProvider>
+	);
 }
