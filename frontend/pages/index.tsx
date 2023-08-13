4557ef124796067bc4fa59f0b203e5b7ee246858
YangGwangSeong <rhkdtjd_12@naver.com>
2023-08-13 04:35:15 -0700

    :sparkles: feat: 클라이언트 전역 상태관리를 위한 recoil 설치 및 적용 (#3)

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -5,14 +5,25 @@ import axios from 'axios';
 import Lottie from 'lottie-react';
 import groovyWalkAnimation from '@/assets/lottie/groovyWalk.json';
 import { Report } from 'notiflix/build/notiflix-report-aio';
+import { useRecoilState } from 'recoil';
+import { todoAtom } from '../atoms';
+import { useQuery } from 'react-query';
 
 const HomePage: NextPage = () => {
+	const { data, isLoading, isError } = useQuery(
+		['main'],
+		async () => await axios.get('/api/users'),
+	);
+
 	useEffect(() => {
-		const response = axios.get('/api/users').then(data => console.log(data));
+		const response = axios.get('/api/users').then();
 	}, []);
+
+	const [text, setText] = useRecoilState(todoAtom);
 
 	const handleClick = () => {
 		Report.success('Title', 'Message', '확인');
+		setText('아톰상태변경 확인!');
 	};
 
 	return (
@@ -21,6 +32,8 @@ const HomePage: NextPage = () => {
 				<div className="w-6/12 h-3/6">
 					<Lottie animationData={groovyWalkAnimation} />
 				</div>
+				<div>{data?.data.data}</div>
+				<div>{text}</div>
 				<div>
 					<button type="button" onClick={handleClick}>
 						클릭!
