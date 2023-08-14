663cc524069c21660dbba2377132cea5c43b37aa
YangGwangSeong <rhkdtjd_12@naver.com>
2023-08-14 09:47:19 -0700

    :sparkles: feat: 메인 Format 컴포넌트 스타일링 작업 (#6)

--- frontend/components/ui/layout/Format.tsx
+++ frontend/components/ui/layout/Format.tsx
@@ -1,12 +1,13 @@
 import Meta from '@/utils/meta/Meta';
 import { IMeta } from '@/utils/meta/meta.interface';
 import { FC, PropsWithChildren } from 'react';
+import styles from './Format.module.scss';
 
 const Format: FC<PropsWithChildren<IMeta>> = ({ children, ...meta }) => {
 	return (
 		<>
 			<Meta {...meta} />
-			<main>{children}</main>
+			<main className={styles.main}>{children}</main>
 		</>
 	);
 };
