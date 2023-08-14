82dc5eaeab685aa3d11deeb63b8dc12dec8e6cb3
YangGwangSeong <rhkdtjd_12@naver.com>
2023-08-14 13:46:15 -0700

    :sparkles: feat: screens home 컴포넌트 생성 (#6)

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,47 +1,9 @@
-import Format from '@/components/ui/layout/Format';
 import { NextPage } from 'next';
-import React, { useEffect } from 'react';
-import axios from 'axios';
-import Lottie from 'lottie-react';
-import groovyWalkAnimation from '@/assets/lottie/groovyWalk.json';
-import { Report } from 'notiflix/build/notiflix-report-aio';
-import { useRecoilState } from 'recoil';
-import { todoAtom } from '../atoms';
-import { useQuery } from 'react-query';
+import React from 'react';
+import Home from '@/components/screens/home/Home';
 
 const HomePage: NextPage = () => {
-	const { data, isLoading, isError } = useQuery(
-		['main'],
-		async () => await axios.get('/api/users'),
-	);
-
-	useEffect(() => {
-		const response = axios.get('/api/users').then();
-	}, []);
-
-	const [text, setText] = useRecoilState(todoAtom);
-
-	const handleClick = () => {
-		Report.success('Title', 'Message', '확인');
-		setText('아톰상태변경 확인!');
-	};
-
-	return (
-		<Format title={'main'}>
-			<div className="flex items-center justify-center">
-				<div className="w-1/4 h-1/4">
-					<Lottie animationData={groovyWalkAnimation} />
-				</div>
-				<div>{data?.data.data}</div>
-				<div>{text}</div>
-				<div>
-					<button type="button" onClick={handleClick}>
-						클릭!
-					</button>
-				</div>
-			</div>
-		</Format>
-	);
+	return <Home />;
 };
 
 export default HomePage;
