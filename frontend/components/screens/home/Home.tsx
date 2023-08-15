b323253d691c77c2cdf3c9dabda5f8d49c74cf8f
YangGwangSeong <rhkdtjd_12@naver.com>
2023-08-14 20:49:28 -0700

    :sparkles: feat: 메인 랜딩페이지 작업 (#6)
    메인 페이지 작업
     - 랜딩페이지에 사용 할 lottie 애니메이션 추가
     - 로그인 페이지로 이동하는 버튼 추가
     - 구글 로그인 버튼 추가
    
    관련: #6

--- frontend/components/screens/home/Home.tsx
+++ frontend/components/screens/home/Home.tsx
@@ -1,5 +1,5 @@
 import Format from '@/components/ui/layout/Format';
-import mainLandingAnimation from '@/assets/lottie/mainLanding.json';
+import mainLandingAnimation from '@/assets/lottie/landing.json';
 import Lottie from 'lottie-react';
 import React, { FC } from 'react';
 import Link from 'next/link';
@@ -9,8 +9,8 @@ const Home: FC = () => {
 		<Format title={'main'}>
 			<div className="flex justify-center h-full items-center">
 				<div className="h-1/2 flex">
-					<div className="w-1/2 h-10">
-						<Lottie animationData={mainLandingAnimation} loop={false} />
+					<div className="w-1/2 flex justify-center">
+						<Lottie animationData={mainLandingAnimation} />
 					</div>
 					<div className="p-10 ml-20">
 						<div className="font-bold text-4xl text-customDark">
@@ -23,15 +23,23 @@ const Home: FC = () => {
 						<div className="mt-6">
 							<Link
 								href={'/login'}
-								className="text-customDark text-center w-full inline-block border border-solid border-customDark rounded-3xl py-4 font-bold"
-								style={{ height: '56px', width: '504px' }}
+								className="text-customDark text-center w-full inline-block border border-solid border-customDark py-4 font-bold"
+								style={{ height: '56px', width: '504px', borderRadius: '40px' }}
 							>
 								로그인
 							</Link>
 						</div>
+						<div className="mt-4">
+							<Link
+								href={'/login'}
+								className="text-customDark text-center w-full inline-block border border-solid border-customDark py-4 font-bold"
+								style={{ height: '56px', width: '504px', borderRadius: '40px' }}
+							>
+								구글 로그인
+							</Link>
+						</div>
 					</div>
 				</div>
-				<div></div>
 			</div>
 		</Format>
 	);
