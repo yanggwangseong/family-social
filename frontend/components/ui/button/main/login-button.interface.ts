141617e73c4dc2c71deedfd02c2e29ba2efeec7e
YangGwangSeong <rhkdtjd_12@naver.com>
2023-08-14 23:19:37 -0700

    :sparkles: feat: 메인 페이지 로그인 버튼 컴포넌트화 (#6)

---
+++ frontend/components/ui/button/main/login-button.interface.ts
@@ -0,0 +1,10 @@
+import { IconType } from 'react-icons';
+
+export interface LoginButtonProps {
+	link: string;
+	imgUrn?: string;
+	Icon?: IconType;
+	IconColor?: string;
+	IconSize?: number;
+	text: string;
+}
