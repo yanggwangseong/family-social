ec272cf9f5f8692e41543bfa0cbd1a9b83ab463e
YangGwangSeong <rhkdtjd_12@naver.com>
2023-08-16 04:41:29 -0700

    :sparkles: feat: email 전송을 위한 config설정 (#4)
    email 전송을 위한 config설정
     - 메일 전송 테스트
    
    관련: #4

--- backend/src/api/users/users.service.ts
+++ backend/src/api/users/users.service.ts
@@ -1,12 +1,24 @@
 import { EntityNotFoundException } from '@/common/exception/service.exception';
+import { MailerService } from '@nestjs-modules/mailer';
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class UsersService {
-	constructor() {}
+	constructor(private readonly mailerService: MailerService) {}
 
 	async test(args?: string) {
 		//if (!args) throw EntityNotFoundException('공지사항을 찾을 수 없습니다');
+		try {
+			await this.mailerService.sendMail({
+				to: 'rhkdtjd_12@naver.com',
+				subject: 'Test email',
+				text: 'test',
+				html: '<div>회원가입 코드 : 123456</div>',
+			});
+		} catch (error) {
+			console.log(error);
+		}
+
 		return 'hellow2';
 	}
 }
