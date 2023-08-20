06ed9775aa4da41f60fb5d162da0e22fb837d99a
YangGwangSeong <rhkdtjd_12@naver.com>
2023-08-20 04:46:36 -0700

    :sparkles: feat: field 컴포넌트화 (#5)

---
+++ frontend/components/ui/field/field.interface.ts
@@ -0,0 +1,12 @@
+import { InputHTMLAttributes } from 'react';
+import { FieldError } from 'react-hook-form';
+import { IconType } from 'react-icons';
+
+export interface FieldProps {
+	error?: FieldError | undefined;
+	Icon?: IconType;
+}
+
+type TypeInputPropsField = InputHTMLAttributes<HTMLInputElement> & FieldProps;
+
+export interface FieldType extends TypeInputPropsField {}
