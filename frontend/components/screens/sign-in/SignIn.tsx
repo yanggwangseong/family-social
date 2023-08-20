5bf8b1e889b875bccbec2779b32566105271a3e9
YangGwangSeong <rhkdtjd_12@naver.com>
2023-08-20 03:35:19 -0700

    :sparkles: feat: 회원가입 페이지 생성 (#5)

--- frontend/components/screens/sign-in/SignIn.tsx
+++ frontend/components/screens/sign-in/SignIn.tsx
@@ -9,13 +9,13 @@ const SignIn: FC = () => {
 			<div className={styles.container}>
 				<div className={styles.contents_card}>
 					<div className={styles.contents_wrap}>
-						<div className={styles.signin__header_title}>Login</div>
+						<div className={styles.signin__header_title}>로그인</div>
 						<div className={styles.signin__header_subtitle}>
 							이메일과 비밀번호를 이용하여 로그인 할 수 있습니다.
 						</div>
 					</div>
 					<div className={styles.footer_wrap}>
-						회원이 아니신가요?{' '}
+						회원이 아니신가요?
 						<Link href={'/signup'} className={styles.signup}>
 							회원가입
 						</Link>
---
+++ frontend/pages/signup/index.tsx
@@ -0,0 +1,9 @@
+import SignUp from '@/components/screens/sign-up/SignUp';
+import { NextPage } from 'next';
+import React from 'react';
+
+const SignUpPage: NextPage = () => {
+	return <SignUp></SignUp>;
+};
+
+export default SignUpPage;
