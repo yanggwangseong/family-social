11dd53e4946fd55fc21bcdfaa34d7ff72d5d6f6b
YangGwangSeong <rhkdtjd_12@naver.com>
2023-08-28 23:59:33 -0700

    :sparkles: feat: 팸 그룹 관련 entity관계 설정 (#14)

--- backend/src/entities/member-group.entity.ts
+++ backend/src/entities/member-group.entity.ts
@@ -1,8 +1,15 @@
 import { Column, Entity } from 'typeorm';
 import { DefaultEntity } from './common/default.entity';
 
+export type Trole = 'main' | 'user';
+
 @Entity({ name: 'fam_member_group' })
 export class MemberGroupEntity extends DefaultEntity {
-	@Column({ type: 'varchar', length: 30, nullable: false })
-	role!: string;
+	@Column({
+		type: 'varchar',
+		length: 30,
+		nullable: false,
+		default: 'user',
+	})
+	role!: Trole;
 }
