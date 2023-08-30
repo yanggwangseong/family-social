59fb3201a74165570b254d81285899d9d7af8b52
YangGwangSeong <rhkdtjd_12@naver.com>
2023-08-29 21:21:41 -0700

    :sparkles: feat: group 생성 contoller 작성 (#14)

--- backend/src/api/groups/groups.controller.ts
+++ backend/src/api/groups/groups.controller.ts
@@ -1,11 +1,39 @@
-import { Controller, UseInterceptors } from '@nestjs/common';
+import {
+	Body,
+	Controller,
+	Post,
+	UseGuards,
+	UseInterceptors,
+} from '@nestjs/common';
 
 import { LoggingInterceptor } from '@/common/interceptors/logging.interceptor';
 import { TimeoutInterceptor } from '@/common/interceptors/timeout.interceptor';
 import { GroupsService } from './groups.service';
+import { AccessTokenGuard } from '@/common/guards/accessToken.guard';
+import { GroupCreateReqDto } from '@/dto/group/req/group-create-req.dto';
+import { ApiTags } from '@nestjs/swagger';
+import { CreateGroupSwagger } from '@/common/decorators/swagger/swagger-group.decorator';
 
 @UseInterceptors(LoggingInterceptor, TimeoutInterceptor)
+@UseGuards(AccessTokenGuard)
+@ApiTags('groups')
 @Controller('groups')
 export class GroupsController {
 	constructor(private readonly groupsService: GroupsService) {}
+
+	/**
+	 * @summary 유저가 속하는 Group생성
+	 *
+	 * @tag groups
+	 * @param groupName string
+	 * @author YangGwangSeong <soaw83@gmail.com>
+	 * @returns 그룹이름
+	 */
+
+	@CreateGroupSwagger()
+	@Post()
+	async createGroup(@Body() dto: GroupCreateReqDto) {
+		console.log(dto.groupName);
+		return true;
+	}
 }
--- backend/src/api/groups/groups.module.ts
+++ backend/src/api/groups/groups.module.ts
@@ -1,11 +1,14 @@
 import { Module } from '@nestjs/common';
 import { GroupsController } from './groups.controller';
 import { GroupsService } from './groups.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { GroupEntity } from '@/entities/group.entity';
+import { GroupsRepository } from './groups.repository';
 
 @Module({
-	imports: [],
+	imports: [TypeOrmModule.forFeature([GroupEntity])],
 	controllers: [GroupsController],
-	providers: [GroupsService],
+	providers: [GroupsService, GroupsRepository],
 	exports: [],
 })
 export class GroupsModule {}
