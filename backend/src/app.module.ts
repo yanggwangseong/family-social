5d2f8e2c4dfb924f11a10aaab699a29dc2830af3
YangGwangSeong <rhkdtjd_12@naver.com>
2023-09-11 00:08:24 -0700

    fix: conflict git 수정

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -8,6 +8,8 @@ import { MailerModule } from '@nestjs-modules/mailer';
 import { EmailOptions } from './common/config/emailConfig';
 import { MembersModule } from './api/members/members.module';
 import { AuthModule } from './api/auth/auth.module';
+import { GroupsModule } from './api/groups/groups.module';
+import { FamsModule } from './api/fams/fams.module';
 
 @Module({
 	imports: [
@@ -16,11 +18,12 @@ import { AuthModule } from './api/auth/auth.module';
 			envFilePath: [`${__dirname}/../.${process.env.NODE_ENV}.env`],
 			isGlobal: true,
 		}),
-		//TypeOrmModule.forRoot(typeORMConfig),
 		TypeOrmModule.forRootAsync(TypeOrmModuleOptions),
 		UsersModule,
 		MembersModule,
 		AuthModule,
+		GroupsModule,
+		FamsModule,
 	],
 	controllers: [],
 	providers: [],
