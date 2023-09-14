3ba8d8ac8aea3c242ad1e425a7b8bb1e6e6554aa
YangGwangSeong <rhkdtjd_12@naver.com>
2023-09-14 05:23:44 -0700

    :sparkles: feat: [FE]그룹 생성 화면 퍼블리싱 (#14)

--- frontend/components/screens/group/Group.tsx
+++ frontend/components/screens/group/Group.tsx
@@ -1,40 +1,15 @@
 import Format from '@/components/ui/layout/Format';
 import React, { FC } from 'react';
 import styles from './Group.module.scss';
-import Field from '@/components/ui/field/Field';
-import { FaRegBell } from 'react-icons/fa';
-import { AiOutlineHome, AiOutlineMessage } from 'react-icons/ai';
-import Link from 'next/link';
 import GroupSidebar from '@/components/ui/layout/sidebar/group/GroupSidebar';
+import Header from '@/components/ui/header/Header';
 
 const Group: FC = () => {
 	return (
 		<Format title={'groups'}>
 			<div className={styles.container}>
-				<div className={styles.header_container}>
-					<div className={styles.header_wrap}>
-						<div className={styles.header_left_container}>
-							<div className={styles.header_logo}>Fam</div>
-							<Field
-								style={{ marginLeft: '40px' }}
-								fieldClass={'input'}
-							></Field>
-						</div>
-						<div className={styles.right_icons_container}>
-							<Link href={'/feeds'}>
-								<div className={styles.icon_wrap}>
-									<AiOutlineHome size={22}></AiOutlineHome>
-								</div>
-							</Link>
-							<div className={styles.icon_wrap}>
-								<FaRegBell size={22}></FaRegBell>
-							</div>
-							<div className={styles.icon_wrap}>
-								<AiOutlineMessage size={22}></AiOutlineMessage>
-							</div>
-						</div>
-					</div>
-				</div>
+				{/* 헤더 */}
+				<Header />
 				<div className={styles.contents_container}>
 					<GroupSidebar />
 					<div>contensts</div>
--- frontend/components/screens/group/create/GroupCreate.tsx
+++ frontend/components/screens/group/create/GroupCreate.tsx
@@ -1,11 +1,73 @@
 import React, { FC } from 'react';
 import styles from './GroupCreate.module.scss';
 import Format from '@/components/ui/layout/Format';
+import Header from '@/components/ui/header/Header';
+import CustomButton from '@/components/ui/button/custom-button/CustomButton';
+import { AiOutlineArrowLeft } from 'react-icons/ai';
+import Field from '@/components/ui/field/Field';
+import Link from 'next/link';
+import Profile from '@/components/ui/profile/Profile';
 
 const GroupCreate: FC = () => {
 	return (
 		<Format title={'group-create'}>
-			<div className={styles.container}>그룹생성</div>
+			<div className={styles.container}>
+				{/* 헤더 */}
+				<Header />
+				<form className={styles.form}>
+					<div className={styles.contents_card}>
+						<div className={styles.contents_wrap}>
+							<div className="flex">
+								<Link
+									className="p-4 border border-solid border-customDark rounded-full"
+									href={'/groups'}
+								>
+									<AiOutlineArrowLeft size={20}></AiOutlineArrowLeft>
+								</Link>
+
+								<div className="text-2xl font-bold flex justify-center items-center ml-7">
+									Back
+								</div>
+							</div>
+
+							<div className={styles.page_label_container}>
+								<div className={styles.page_label}>그룹 {'>'} 그룹 만들기</div>
+								<div className={styles.page_title}>그룹 만들기</div>
+							</div>
+
+							<div className="mt-12">
+								<Profile />
+							</div>
+
+							<div className="mt-12">
+								<div className={styles.form_label}>그룹명</div>
+								<Field></Field>
+								<div className={styles.form_label}>그룹설명</div>
+								<div>
+									<textarea
+										className="border border-solid border-customDark bg-basic 
+									rounded-2xl w-full py-2 px-4
+									focus:outline-none
+									"
+									></textarea>
+								</div>
+							</div>
+						</div>
+						<div className={styles.footer_wrap}>
+							<CustomButton
+								type="submit"
+								className="mt-8 bg-customOrange text-customDark 
+								font-bold border border-solid border-customDark 
+								rounded-full py-4 px-4
+								w-full hover:bg-orange-500
+								"
+							>
+								만들기
+							</CustomButton>
+						</div>
+					</div>
+				</form>
+			</div>
 		</Format>
 	);
 };
