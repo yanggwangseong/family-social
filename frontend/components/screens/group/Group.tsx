988de71eb64965bed768efdf88f8d62d131e7db4
YangGwangSeong <rhkdtjd_12@naver.com>
2023-09-14 00:02:16 -0700

    :sparkles: feat: [FE]헤더 아이콘 퍼블리싱 (#14)

--- frontend/components/screens/group/Group.tsx
+++ frontend/components/screens/group/Group.tsx
@@ -2,6 +2,8 @@ import Format from '@/components/ui/layout/Format';
 import React, { FC } from 'react';
 import styles from './Group.module.scss';
 import Field from '@/components/ui/field/Field';
+import { FaRegBell } from 'react-icons/fa';
+import { AiOutlineHome, AiOutlineMessage } from 'react-icons/ai';
 
 const Group: FC = () => {
 	return (
@@ -13,7 +15,17 @@ const Group: FC = () => {
 							<div className={styles.header_logo}>Fam</div>
 							<Field style={{ marginLeft: '40px' }}></Field>
 						</div>
-						<div className={styles.right_icons_container}>아이콘</div>
+						<div className={styles.right_icons_container}>
+							<div className={styles.icon_wrap}>
+								<AiOutlineHome size={22}></AiOutlineHome>
+							</div>
+							<div className={styles.icon_wrap}>
+								<FaRegBell size={22}></FaRegBell>
+							</div>
+							<div className={styles.icon_wrap}>
+								<AiOutlineMessage size={22}></AiOutlineMessage>
+							</div>
+						</div>
 					</div>
 				</div>
 				<div>
