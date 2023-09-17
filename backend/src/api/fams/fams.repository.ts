bb376693ab87c26e46064820183317c70d12c499
YangGwangSeong <rhkdtjd_12@naver.com>
2023-09-16 22:05:50 -0700

    :sparkles: feat: [FE] fam-invitations-response type에 member 추가 (#14)

--- backend/src/api/fams/fams.repository.ts
+++ backend/src/api/fams/fams.repository.ts
@@ -60,6 +60,7 @@ export class FamsRepository extends Repository<FamEntity> {
 			},
 			relations: {
 				group: true,
+				member: true,
 			},
 		});
 
--- frontend/shared/interfaces/fam.interface.ts
+++ frontend/shared/interfaces/fam.interface.ts
@@ -1,3 +1,4 @@
+import { AuthResponse } from './auth.interface';
 import { GroupResponse } from './group.interface';
 
 export interface FamInvitationsResponse {
@@ -9,4 +10,5 @@ export interface FamInvitation {
 	id: string;
 	invitationAccepted: false;
 	group: GroupResponse;
+	member: AuthResponse;
 }
