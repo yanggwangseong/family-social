e8eb137d65f9538d2670967c3a0cf9eecbeb4f96
YangGwangSeong <rhkdtjd_12@naver.com>
2023-10-05 23:28:21 -0700

    :sparkles: feat: [BE] 정상적으로 업로드 되는지 테스트 (#25)

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -4,4 +4,17 @@ const nextConfig = {
 	output: 'standalone',
 };
 
-module.exports = nextConfig;
+module.exports = {
+	images: {
+		remotePatterns: [
+			{
+				protocol: 'https',
+				hostname:
+					'elasticbeanstalk-ap-northeast-2-655111786396.s3.ap-northeast-2.amazonaws.com',
+				port: '',
+				pathname: '/feed/developmentbody-media/**',
+			},
+		],
+	},
+	nextConfig,
+};
