d727d561369e9048f0a2612e7c63a305ffa8f41d
YangGwangSeong <rhkdtjd_12@naver.com>
2023-10-11 02:17:54 -0700

    :sparkles: feat: [FE] Splash 페이지 퍼블리싱 (#30)

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -15,6 +15,7 @@ const config: Config = {
 				customGray: '#707070',
 				customOrange: '#e5855d',
 				customGray2427: 'rgba(10, 10, 10, 0.27)',
+				bgSplash: '#111111',
 			},
 			backgroundImage: {
 				'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
