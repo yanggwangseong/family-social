d3c64a7574bbb092b5dece28aa92f98a18c77428
YangGwangSeong <rhkdtjd_12@naver.com>
2023-10-12 22:47:57 -0700

    :sparkles: feat: [FE] 피드 페이지 사이드바 퍼블리싱 (#38)

--- frontend/components/ui/layout/sidebar/main/MainSidebar.tsx
+++ frontend/components/ui/layout/sidebar/main/MainSidebar.tsx
@@ -2,14 +2,16 @@ import React, { FC } from 'react';
 import styles from './MainSidebar.module.scss';
 import CustomButton from '@/components/ui/button/custom-button/CustomButton';
 import Menu from '../menu/Menu';
-import { AiOutlineAudit } from 'react-icons/ai';
+import { AiOutlineAudit, AiOutlineSchedule } from 'react-icons/ai';
+import { MdOutlineManageAccounts } from 'react-icons/md';
 
 const MainSidebar: FC = () => {
 	return (
 		<div className={styles.sidebar_container}>
 			{/* 사이드 메뉴 */}
 			<Menu link="/feeds" Icon={AiOutlineAudit} menu="피드" />
-
+			<Menu link="/schdules" Icon={AiOutlineSchedule} menu="일정작성" />
+			<Menu link="/profile" Icon={MdOutlineManageAccounts} menu="계정" />
 			<div className={styles.sidebar_btn_container}>
 				<CustomButton
 					type="button"
