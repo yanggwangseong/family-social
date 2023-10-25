ad9a3222ff62a3a20875c8447f63e5b1c123afd8
YangGwangSeong <rhkdtjd_12@naver.com>
2023-10-25 00:35:44 -0700

    :sparkles: feat: [BE] 댓글 좋아요 관계 설정 (#44)

--- backend/src/models/entities/fam-like-comment.entity.ts
+++ backend/src/models/entities/fam-like-comment.entity.ts
@@ -1,6 +1,14 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsUUID } from 'class-validator';
-import { CreateDateColumn, Entity, PrimaryColumn } from 'typeorm';
+import {
+	CreateDateColumn,
+	Entity,
+	JoinColumn,
+	ManyToOne,
+	PrimaryColumn,
+} from 'typeorm';
+import { MemberEntity } from './member.entity';
+import { CommentEntity } from './comment.entity';
 
 @Entity({ name: 'fam_like_comment' })
 export class LikeCommentEntity {
@@ -19,6 +27,14 @@ export class LikeCommentEntity {
 	@IsNotEmpty()
 	@IsUUID()
 	public readonly commentId!: string;
+
+	@ManyToOne((type) => MemberEntity, (member) => member.memberLikesComments)
+	@JoinColumn({ name: 'memberId', referencedColumnName: 'id' })
+	member!: MemberEntity;
+
+	@ManyToOne((type) => CommentEntity, (cm) => cm.LikedByComments)
+	@JoinColumn({ name: 'commentId', referencedColumnName: 'id' })
+	comment!: CommentEntity;
 
 	@ApiProperty()
 	@CreateDateColumn({
