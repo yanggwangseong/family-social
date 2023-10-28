e560fa340a122ad11ac5073766a7c6742fc5a03f
YangGwangSeong <rhkdtjd_12@naver.com>
2023-10-28 01:07:27 -0700

    :sparkles: feat(frontend): 아이콘 클릭시 emoji 리스트 표시 (#64)

--- frontend/components/ui/feed/comment/Comments.tsx
+++ frontend/components/ui/feed/comment/Comments.tsx
@@ -1,31 +1,63 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
 import { CommentsProps } from './comments-interface';
 import styles from './Comments.module.scss';
 import CommentItem from './comment-item/CommentItem';
 import Profile from '../../profile/Profile';
 import FieldWithTextarea from '../../field/field-area/FieldArea';
 import { FaRegSmile } from 'react-icons/fa';
+import EmojiPicker from 'emoji-picker-react';
+import CustomButton from '../../button/custom-button/CustomButton';
 
 const Comments: FC<CommentsProps> = ({
 	comments,
 	isToggleCommentWrite,
 	commentTextAreaRef,
 }) => {
+	const [isEmoji, setIsEmoji] = useState<boolean>(false);
+
+	const handleEmojiView = () => {
+		setIsEmoji(!isEmoji);
+	};
 	return (
 		<div className={styles.comment_container}>
 			{isToggleCommentWrite && (
 				<div className="my-2 flex">
 					<Profile />
-					<div className="border border-solid border-customDark rounded-full px-6 py-3 h-12 flex-1 ml-4 flex">
-						<div className="mr-2">
-							<FaRegSmile size={22} />
+					<form className={styles.form}>
+						<div className="border border-solid border-customDark rounded-full px-6 py-3 h-12 flex-1 ml-4 flex">
+							<div className="mr-2 relative">
+								<FaRegSmile
+									className={'cursor-pointer'}
+									size={22}
+									onClick={handleEmojiView}
+								/>
+								{isEmoji && (
+									<div className=" absolute z-10 -top-[420px]">
+										<EmojiPicker
+											height={400}
+											autoFocusSearch={false}
+											searchDisabled={true}
+											skinTonesDisabled={true}
+										/>
+									</div>
+								)}
+							</div>
+							<FieldWithTextarea
+								fieldClass="hidden_border_textarea"
+								placeholder="댓글을 입력 하세요."
+								ref={commentTextAreaRef}
+							></FieldWithTextarea>
+							<div className="flex items-center justify-center">
+								<CustomButton
+									type="submit"
+									className="text-customOrange font-extrabold bg-basic text-sm"
+									shadowNone={true}
+								>
+									POST
+								</CustomButton>
+							</div>
 						</div>
-						<FieldWithTextarea
-							fieldClass="hidden_border_textarea"
-							placeholder="댓글을 입력 하세요."
-							ref={commentTextAreaRef}
-						></FieldWithTextarea>
-					</div>
+					</form>
 				</div>
 			)}
 
