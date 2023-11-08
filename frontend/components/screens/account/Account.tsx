b2da093e4f7c86d86b162000f285fe94c49c8927
YangGwangSeong <rhkdtjd_12@naver.com>
2023-11-08 03:53:03 -0800

    :sparkles: feat(frontend): 프로필 배경과 프로필 이미지 퍼블리싱 (#71)

--- frontend/components/screens/account/Account.tsx
+++ frontend/components/screens/account/Account.tsx
@@ -4,6 +4,8 @@ import Format from '@/components/ui/layout/Format';
 import Header from '@/components/ui/header/Header';
 import MainSidebar from '@/components/ui/layout/sidebar/main/MainSidebar';
 import RightSidebar from '@/components/ui/layout/sidebar/main/rightSidebar/RightSidebar';
+import Image from 'next/image';
+import Profile from '@/components/ui/profile/Profile';
 
 const Account: FC = () => {
 	return (
@@ -15,8 +17,27 @@ const Account: FC = () => {
 					{/* 왼쪽 사이드바 */}
 					<MainSidebar />
 					<div className={styles.detail_container}>
-						<div className="w-full h-[200px]">이미지</div>
-						<div className={styles.main_contents_container}></div>
+						<div className="w-full h-[200px] relative border-b border-solid border-customDark">
+							<Image
+								fill
+								src={'/images/banner/group-base.png'}
+								alt="banner"
+								objectFit="cover"
+							></Image>
+						</div>
+						<div className={styles.main_contents_container}>
+							<div className="flex">
+								<div className="-mt-20 relative z-10 border border-solid border-customDark w-[120px] h-[120px] rounded-full">
+									<Image
+										className="rounded-full"
+										width={120}
+										height={120}
+										src={'/images/profile/profile.png'}
+										alt="img"
+									></Image>
+								</div>
+							</div>
+						</div>
 					</div>
 					{/* 오른쪽 사이드바 */}
 					<RightSidebar />
