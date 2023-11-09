0c6cb11d685006bac5496c8d1de4519d7a348789
YangGwangSeong <rhkdtjd_12@naver.com>
2023-11-08 22:56:05 -0800

    :sparkles: feat(frontend): 프로필 수정 레이어 팝업 프로필 이미지 퍼블리싱 (#71)

--- frontend/components/ui/modal/layer-modal/layer/EditProfile/EditProfile.tsx
+++ frontend/components/ui/modal/layer-modal/layer/EditProfile/EditProfile.tsx
@@ -1,8 +1,25 @@
 import React, { FC } from 'react';
 import styles from './EditProfile.module.scss';
+import Image from 'next/image';
+import { GoPencil } from 'react-icons/go';
 
 const EditProfile: FC = () => {
-	return <div></div>;
+	return (
+		<div className="mt-12">
+			<div className="relative z-10 border border-solid border-customDark w-[120px] h-[120px] rounded-full">
+				<Image
+					className="rounded-full brightness-50"
+					width={120}
+					height={120}
+					src={'/images/profile/profile.png'}
+					alt="img"
+				></Image>
+				<div className={styles.profile_img_icon_container}>
+					<GoPencil size={18} color="#0a0a0a" />
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default EditProfile;
