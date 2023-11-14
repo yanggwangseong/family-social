4e932fc4577c9f6c42da18d38334dcaf64c77f9b
YangGwangSeong <rhkdtjd_12@naver.com>
2023-11-13 22:24:26 -0800

    :sparkles: feat(frontend): 오른쪽 사이드바 탭 메뉴 퍼블리싱 (#78)

--- frontend/components/ui/layout/sidebar/main/rightSidebar/RightSidebar.tsx
+++ frontend/components/ui/layout/sidebar/main/rightSidebar/RightSidebar.tsx
@@ -1,10 +1,28 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
 import styles from './RightSidebar.module.scss';
+import cn from 'classnames';
 
 const RightSidebar: FC = () => {
+	const [isMenu, setIsMenu] = useState<boolean>(true);
 	return (
 		<div className={styles.right_sidebar_container}>
-			<div>rightSidebar</div>
+			<div className={styles.side_top_container}>
+				<div className={styles.top_tab_menu}>
+					<div
+						className={cn(styles.top_tab_menu_item, {
+							[styles.active]: !!isMenu,
+						})}
+					>
+						멤버
+					</div>
+				</div>
+				<div className={styles.top_tab_menu}>
+					<div className={styles.top_tab_menu_item}>그룹</div>
+				</div>
+				<div className={styles.top_tab_menu}>
+					<div className={styles.top_tab_menu_item}>즐겨찾기</div>
+				</div>
+			</div>
 		</div>
 	);
 };
