54c8d654b098b2a42f742262c357185ee8ec5056
YangGwangSeong <rhkdtjd_12@naver.com>
2023-12-05 07:13:49 -0800

    :sparkles: feat(frontend): 여행일정 작성 페이지 생성 (#81)

---
+++ frontend/components/screens/schedule/create/ScheduleCreate.tsx
@@ -0,0 +1,28 @@
+import Header from '@/components/ui/header/Header';
+import Format from '@/components/ui/layout/Format';
+import MainSidebar from '@/components/ui/layout/sidebar/main/MainSidebar';
+import RightSidebar from '@/components/ui/layout/sidebar/main/rightSidebar/RightSidebar';
+import React, { FC } from 'react';
+import styles from './ScheduleCreate.module.scss';
+
+const ScheduleCreate: FC = () => {
+	return (
+		<Format title={'schedule-create'}>
+			<div className={styles.container}>
+				{/* 헤더 */}
+				<Header />
+				<div className={styles.contents_container}>
+					{/* 왼쪽 사이드바 */}
+					<MainSidebar />
+					<div className={styles.detail_container}>
+						<div className={styles.main_contents_container}></div>
+					</div>
+					{/* 오른쪽 사이드바 */}
+					<RightSidebar />
+				</div>
+			</div>
+		</Format>
+	);
+};
+
+export default ScheduleCreate;
