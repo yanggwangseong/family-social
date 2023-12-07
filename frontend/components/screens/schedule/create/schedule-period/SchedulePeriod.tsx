539a556b6f9df1423d63a334a034a07180ab377d
YangGwangSeong <rhkdtjd_12@naver.com>
2023-12-06 18:13:55 -0800

    :sparkles: feat(frontend): date-picker 적용 (#81)

--- frontend/components/screens/schedule/create/schedule-period/SchedulePeriod.tsx
+++ frontend/components/screens/schedule/create/schedule-period/SchedulePeriod.tsx
@@ -1,18 +1,61 @@
-import React, { FC } from 'react';
+import React, { ChangeEvent, FC, MouseEvent, useState } from 'react';
 import styles from './SchedulePeriod.module.scss';
 import CustomButton from '@/components/ui/button/custom-button/CustomButton';
 import { SchedulePeriodProps } from './schedule-period.interface';
 import { Union, schdulePages } from 'types';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
+import { ko } from 'date-fns/esm/locale';
+import { format } from 'date-fns';
 
 const SchedulePeriod: FC<SchedulePeriodProps> = ({ onChangePage }) => {
 	const handleChangePage = (page: Union<typeof schdulePages>) => {
 		onChangePage(page);
 	};
 
+	const [dateRange, setDateRange] = useState([new Date(), new Date()]);
+	const [startDate, endDate] = dateRange;
+
+	const [isOpen, setIsOpen] = useState(false);
+	const handleChange = (e: any) => {
+		setDateRange(e);
+	};
+	const handleClick = (e: MouseEvent<HTMLButtonElement>) => {
+		e.preventDefault();
+		setIsOpen(!isOpen);
+	};
+
+	const selectedDates = () => {
+		setIsOpen(false);
+
+		const formattedDate = format(startDate, 'yyyy-MM-dd (eee)', { locale: ko });
+		console.log(formattedDate);
+	};
+
 	return (
 		<div className={styles.period_container}>
 			<div className={styles.step_title}>STEP 2</div>
 			<div className={styles.title}>여행 기간이 어떻게 되시나요?</div>
+			<button className="example-custom-input" onClick={handleClick}>
+				{`버튼`}
+			</button>
+			{isOpen && (
+				<>
+					<DatePicker
+						locale={ko}
+						selectsRange={true}
+						startDate={startDate}
+						endDate={endDate}
+						onChange={handleChange}
+						inline
+						isClearable={true}
+					/>
+					<button type="button" onClick={selectedDates}>
+						{`선택`}
+					</button>
+				</>
+			)}
+
 			<div className={styles.button_container}>
 				<CustomButton
 					type="button"
@@ -26,10 +69,10 @@ const SchedulePeriod: FC<SchedulePeriodProps> = ({ onChangePage }) => {
 				<CustomButton
 					type="button"
 					className="mt-8 mb-4 bg-customOrange text-customDark 
-            font-bold border border-solid border-customDark 
-            rounded-full p-[10px]
-            w-full hover:bg-orange-500
-            "
+					font-bold border border-solid border-customDark 
+					rounded-full p-[10px]
+					w-full hover:bg-orange-500
+					"
 				>
 					다음
 				</CustomButton>
