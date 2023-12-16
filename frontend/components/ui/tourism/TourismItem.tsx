9c50af37c84a239e2cb0ff893bc335c8778a8be5
YangGwangSeong <rhkdtjd_12@naver.com>
2023-12-15 23:29:38 -0800

    :sparkles: feat(frontend): 지역별 관광 정보 아이템 퍼블리싱 (#81)

--- frontend/components/ui/tourism/TourismItem.tsx
+++ frontend/components/ui/tourism/TourismItem.tsx
@@ -2,12 +2,14 @@ import React, { FC } from 'react';
 import styles from './TourismItem.module.scss';
 import { TourismItemProps } from './tourism-item.interface';
 import Image from 'next/image';
+import { AiFillHeart } from 'react-icons/ai';
+import { AiFillStar } from 'react-icons/ai';
 
 const TourismItem: FC<TourismItemProps> = ({ tour }) => {
 	return (
 		<div className={styles.container}>
 			<div className="flex items-center justify-between">
-				<div className="flex">
+				<div className="flex gap-4">
 					<div className={styles.img_container}>
 						<Image
 							width={120}
@@ -16,6 +18,23 @@ const TourismItem: FC<TourismItemProps> = ({ tour }) => {
 							alt="img"
 							style={{ height: '100px' }}
 						></Image>
+					</div>
+					<div className="flex flex-col justify-between">
+						<div className={styles.tour_title}>{tour.title}</div>
+						<div className="flex gap-6">
+							<div className={styles.tour_content_type_name}>명소</div>
+							<div className={styles.tour_addr}>{tour.addr1}</div>
+						</div>
+						<div className="flex gap-4">
+							<div className="flex items-center gap-2">
+								<AiFillHeart size={14} color="#FB1F42" />
+								<span className="text-sm">0</span>
+							</div>
+							<div className="flex items-center gap-2">
+								<AiFillStar size={14} color="rgb(253, 224, 71)" />
+								<span className="text-sm">0</span>
+							</div>
+						</div>
 					</div>
 				</div>
 				<div className="flex w-[24px] flex-shrink-0">dd</div>
