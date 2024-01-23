cebf431b47a5b7b7294b57d9451cb2be6a0827cd
YangGwangSeong <rhkdtjd_12@naver.com>
2024-01-23 03:44:56 -0800

    :sparkles: feat(frontend): scheduleItem 컴포넌트 퍼블리싱 (#100)

--- frontend/components/ui/schedule/ScheduleItem.tsx
+++ frontend/components/ui/schedule/ScheduleItem.tsx
@@ -1,5 +1,6 @@
 import React, { FC } from 'react';
 import Image from 'next/image';
+import { BsThreeDots } from 'react-icons/bs';
 import styles from './ScheduleItem.module.scss';
 
 const ScheduleItem: FC = () => {
@@ -12,7 +13,12 @@ const ScheduleItem: FC = () => {
 				<div className={styles.contents_top_container}>
 					<div className={styles.d_day}>D-248</div>
 					<div className={styles.title}>2024년 가족 여름휴가</div>
+					<div className={styles.setting_container}>
+						<BsThreeDots size={24} />
+					</div>
 				</div>
+				<div className={styles.schedule_periods}>2024-07-24 ~ 2024-07-28</div>
+				<div className={styles.update_date}>최근 수정일 2024-01-25</div>
 			</div>
 		</div>
 	);
