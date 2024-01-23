98b972970c1c35919d875ab095e15566b204366f
YangGwangSeong <rhkdtjd_12@naver.com>
2024-01-23 02:17:43 -0800

    :sparkles: feat(frontend): 전체일정 가져오는 service 함수 생성 (#100)

--- frontend/services/schedule/schedule.service.ts
+++ frontend/services/schedule/schedule.service.ts
@@ -2,6 +2,7 @@ import { PeriodsType } from '@/atoms/periodAtom';
 import {
 	CreateScheduleRequest,
 	CreateScheduleResponse,
+	GetScheduleListResponse,
 } from '@/shared/interfaces/schedule.interface';
 import { axiosAPI } from 'api/axios';
 
@@ -10,6 +11,14 @@ export const ScheduleService = {
 		const { data } = await axiosAPI.post<CreateScheduleResponse>(
 			`/groups/${groupId}/schedules`,
 			schedules,
+		);
+
+		return data;
+	},
+
+	async getScheduleList(page: number, groupId: string) {
+		const { data } = await axiosAPI.get<GetScheduleListResponse[]>(
+			`/groups/${groupId}/schedules?page=${page}&limit=10`,
 		);
 
 		return data;
