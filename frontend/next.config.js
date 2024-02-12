dc50653f7d159447c845f3698dcf67e6353ecbed
YangGwangSeong <rhkdtjd_12@naver.com>
2024-02-11 23:29:07 -0800

    :lipstick: style(frontend): 여행일정 제목 수정 아이콘에 커서 추가 (#106)

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -9,7 +9,7 @@ const nextConfig = {
 				hostname:
 					'elasticbeanstalk-ap-northeast-2-655111786396.s3.ap-northeast-2.amazonaws.com',
 				port: '',
-				pathname: '/feed/developmentbody-media/**',
+				pathname: '/**',
 			},
 			{
 				protocol: 'http',
