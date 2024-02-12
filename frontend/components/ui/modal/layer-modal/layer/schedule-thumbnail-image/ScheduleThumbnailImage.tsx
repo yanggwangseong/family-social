6fb2246a3054e91f19b86273654de3eda9c9eed4
YangGwangSeong <rhkdtjd_12@naver.com>
2024-02-11 23:53:18 -0800

    :sparkles: feat(frontend): 여행일정 썸네일 수정을 위한 layer-modal 퍼블리싱 (#106)

--- frontend/components/ui/modal/layer-modal/layer/schedule-thumbnail-image/ScheduleThumbnailImage.tsx
+++ frontend/components/ui/modal/layer-modal/layer/schedule-thumbnail-image/ScheduleThumbnailImage.tsx
@@ -1,7 +1,48 @@
 import React, { FC } from 'react';
+import styles from './ScheduleThumbnailImage.module.scss';
+import Image from 'next/image';
+import { GoPencil } from 'react-icons/go';
+import ImageCropper from '@/components/ui/image-cropper/ImageCropper';
+import { useUploadImage } from '@/hooks/useUploadImage';
+import CustomButton from '@/components/ui/button/custom-button/CustomButton';
 
 const ScheduleThumbnailImage: FC = () => {
-	return <div>dd</div>;
+	const { isFiles, handleUploadImage, uploadImage } = useUploadImage();
+
+	return (
+		<div className={styles.container}>
+			<form className={styles.form}>
+				<div className={styles.contents_wrap}>
+					<div className={styles.profile_img_container}>
+						<ImageCropper aspectRatio={1 / 1} onCrop={handleUploadImage}>
+							<div className={styles.image_wrap}>
+								<Image
+									className={styles.profile_img}
+									fill
+									src={'/images/banner/group-base.png'}
+									alt="img"
+								></Image>
+								<div className={styles.profile_img_icon_container}>
+									<GoPencil size={18} color="#0a0a0a" />
+								</div>
+							</div>
+						</ImageCropper>
+					</div>
+				</div>
+
+				<CustomButton
+					type="submit"
+					className="mt-8 bg-customOrange text-customDark 
+					font-bold border border-solid border-customDark 
+					rounded-full p-[10px]
+					w-full hover:bg-orange-500
+					"
+				>
+					수정하기
+				</CustomButton>
+			</form>
+		</div>
+	);
 };
 
 export default ScheduleThumbnailImage;
