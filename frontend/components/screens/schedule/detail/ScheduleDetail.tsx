be2564a6e9e86ae73a2638a8ff70e17167b07f36
YangGwangSeong <rhkdtjd_12@naver.com>
2024-02-13 01:38:34 -0800

    :sparkles: feat(frontend): 일정 detail 페이지 퍼블리싱 (#109)

--- frontend/components/screens/schedule/detail/ScheduleDetail.tsx
+++ frontend/components/screens/schedule/detail/ScheduleDetail.tsx
@@ -1,8 +1,34 @@
 import React, { FC } from 'react';
 import styles from './ScheduleDetail.module.scss';
+import Format from '@/components/ui/layout/Format';
+import Header from '@/components/ui/header/Header';
+import MainSidebar from '@/components/ui/layout/sidebar/main/MainSidebar';
+import ScheduleDetailSelect from '@/components/ui/select/schedule/detail/ScheduleDetailSelect';
 
 const ScheduleDetail: FC = () => {
-	return <div>detail</div>;
+	return (
+		<Format title={'schedule-detail'}>
+			<div className={styles.container}>
+				{/* 헤더 */}
+				<Header />
+
+				<div className={styles.contents_container}>
+					{/* 왼쪽 사이드바 */}
+					<MainSidebar />
+					<div className={styles.detail_container}>
+						<div className={styles.main_contents_container}>
+							<div className="flex">
+								<div>여행9</div>
+								<div>2024.1.21-2024.1.24</div>
+							</div>
+							{/* select박스 */}
+							<ScheduleDetailSelect></ScheduleDetailSelect>
+						</div>
+					</div>
+				</div>
+			</div>
+		</Format>
+	);
 };
 
 export default ScheduleDetail;
