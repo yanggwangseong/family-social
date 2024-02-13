8d69fc7ae972d46bb151897b8fbef4c546a69e68
YangGwangSeong <rhkdtjd_12@naver.com>
2024-02-12 22:28:24 -0800

    :sparkles: feat(frontend): 여행일정 썸네일 업로드 기능 추가 (#106)

--- frontend/components/ui/modal/layer-modal/layer/schedule-thumbnail-image/ScheduleThumbnailImage.tsx
+++ frontend/components/ui/modal/layer-modal/layer/schedule-thumbnail-image/ScheduleThumbnailImage.tsx
@@ -5,13 +5,69 @@ import { GoPencil } from 'react-icons/go';
 import ImageCropper from '@/components/ui/image-cropper/ImageCropper';
 import { useUploadImage } from '@/hooks/useUploadImage';
 import CustomButton from '@/components/ui/button/custom-button/CustomButton';
+import { SubmitHandler, useForm } from 'react-hook-form';
+import axios from 'axios';
+import { Loading } from 'notiflix/build/notiflix-loading-aio';
+import { Report } from 'notiflix/build/notiflix-report-aio';
+import { ScheduleService } from '@/services/schedule/schedule.service';
+import { useMutation } from 'react-query';
+import { modalAtom } from '@/atoms/modalAtom';
+import { useRecoilState } from 'recoil';
 
 const ScheduleThumbnailImage: FC = () => {
 	const { isFiles, handleUploadImage, uploadImage } = useUploadImage();
+	const [, setIsShowing] = useRecoilState<boolean>(modalAtom);
+
+	const {
+		register,
+		formState: { errors, isValid },
+		handleSubmit,
+		reset,
+		getValues,
+		watch,
+	} = useForm({
+		mode: 'onChange',
+	});
+
+	const { mutateAsync: scheduleUploadThumbnailASync } = useMutation(
+		['schedule-thumbnail-image-upload'],
+		async (file: File) =>
+			await ScheduleService.uploadScheduleThumbnailImage(
+				file,
+				'510c756b-c73e-4242-8e98-9535fb35b52a',
+			),
+		{
+			onMutate: variable => {
+				Loading.hourglass();
+			},
+			onSuccess(data) {
+				Loading.remove();
+				Report.success(
+					'성공',
+					`여행일정 썸네일 수정이 성공 하였습니다`,
+					'확인',
+					() => {
+						setIsShowing(false);
+					},
+				);
+			},
+			onError(error) {
+				if (axios.isAxiosError(error)) {
+					Report.warning('실패', `${error.response?.data.message}`, '확인');
+				}
+			},
+		},
+	);
+
+	const onSubmit = async () => {
+		if (isFiles) {
+			const url = await scheduleUploadThumbnailASync(isFiles);
+		}
+	};
 
 	return (
 		<div className={styles.container}>
-			<form className={styles.form}>
+			<form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
 				<div className={styles.contents_wrap}>
 					<div className={styles.profile_img_container}>
 						<ImageCropper aspectRatio={1 / 1} onCrop={handleUploadImage}>
