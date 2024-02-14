8ecfd49c76994d0ee43d3010817ee6f611e0cab8
YangGwangSeong <rhkdtjd_12@naver.com>
2024-02-13 21:04:51 -0800

    :sparkles: feat(frontend): 특정 일정 가져오는 useQuery 추가 (#109)

--- frontend/components/screens/schedule/detail/ScheduleDetail.tsx
+++ frontend/components/screens/schedule/detail/ScheduleDetail.tsx
@@ -1,11 +1,34 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
 import styles from './ScheduleDetail.module.scss';
 import Format from '@/components/ui/layout/Format';
 import Header from '@/components/ui/header/Header';
 import MainSidebar from '@/components/ui/layout/sidebar/main/MainSidebar';
 import ScheduleDetailSelect from '@/components/ui/select/schedule/detail/ScheduleDetailSelect';
+import { useRouter } from 'next/router';
+import { ScheduleService } from '@/services/schedule/schedule.service';
+import { useQuery } from 'react-query';
+import Skeleton from '@/components/ui/skeleton/Skeleton';
 
 const ScheduleDetail: FC = () => {
+	const router = useRouter();
+
+	const [isSelectedPeriod, setIsSelectedPeriod] = useState<string>('ALL');
+
+	const query = router.query as { scheduleId: string };
+
+	const { data, isLoading } = useQuery(
+		['get-scheduleId', query.scheduleId],
+		async () =>
+			await ScheduleService.getScheduleById(
+				'75aca3da-1dac-48ef-84b8-cdf1be8fe37d',
+				query.scheduleId,
+			),
+	);
+
+	const handleSelectedPeriod = (period: string) => {
+		setIsSelectedPeriod(period);
+	};
+
 	return (
 		<Format title={'schedule-detail'}>
 			<div className={styles.container}>
@@ -17,12 +40,24 @@ const ScheduleDetail: FC = () => {
 					<MainSidebar />
 					<div className={styles.detail_container}>
 						<div className={styles.main_contents_container}>
-							<div className={styles.title_container}>
-								<div className={styles.title}>여행9</div>
-								<div className={styles.title_period}>2024.1.21-2024.1.24</div>
-							</div>
+							{isLoading && !data && <Skeleton></Skeleton>}
+
 							{/* select박스 */}
-							<ScheduleDetailSelect></ScheduleDetailSelect>
+							{data && (
+								<>
+									<div className={styles.title_container}>
+										<div className={styles.title}>{data.scheduleName}</div>
+										<div className={styles.title_period}>
+											{`${data.startPeriod.toString()} ~ ${data.endPeriod.toString()}`}
+										</div>
+									</div>
+									<ScheduleDetailSelect
+										schedulePeriods={data.schedulePeriods}
+										onSelectedPeriod={handleSelectedPeriod}
+										isSelectedPeriod={isSelectedPeriod}
+									></ScheduleDetailSelect>
+								</>
+							)}
 						</div>
 					</div>
 				</div>
