4f44e07eb37803d4a1e4c28c61cc1df11f01a57f
YangGwangSeong <rhkdtjd_12@naver.com>
2024-02-13 21:05:17 -0800

    :sparkles: feat(frontend): select option 전체 일정, 1일차, 2일차, 3일차 등 추가 (#109)

--- frontend/components/ui/select/schedule/detail/ScheduleDetailSelect.tsx
+++ frontend/components/ui/select/schedule/detail/ScheduleDetailSelect.tsx
@@ -7,11 +7,21 @@ import {
 import { MdKeyboardArrowDown, MdKeyboardArrowUp } from 'react-icons/md';
 import cn from 'classnames';
 import styles from './ScheduleDetailSelect.module.scss';
+import { TourismPeriodResponse } from '@/shared/interfaces/schedule.interface';
 
-const ScheduleDetailSelect: FC = () => {
+const ScheduleDetailSelect: FC<{
+	schedulePeriods: TourismPeriodResponse[];
+	onSelectedPeriod: (period: string) => void;
+	isSelectedPeriod: string;
+}> = ({ schedulePeriods, onSelectedPeriod, isSelectedPeriod }) => {
 	const [isSelectToggle, setIsSelectToggle] = useState<boolean>(false);
 
 	const handleSelectToggle = () => {
+		setIsSelectToggle(!isSelectToggle);
+	};
+
+	const handleSelectedPeriod = (period: string) => {
+		onSelectedPeriod(period);
 		setIsSelectToggle(!isSelectToggle);
 	};
 
@@ -21,9 +31,21 @@ const ScheduleDetailSelect: FC = () => {
 				<div>
 					<AiOutlineSchedule size={22} />
 				</div>
-				<div className={styles.option_text}>{`1일차`}</div>
+				<div className={styles.option_text}>
+					{isSelectedPeriod === 'ALL'
+						? `전체일정`
+						: `${
+								schedulePeriods.findIndex(
+									item => item.period === isSelectedPeriod,
+								) + 1
+						  }일차 ${isSelectedPeriod}`}
+				</div>
 				<div>
-					<MdKeyboardArrowDown size={22} />
+					{isSelectToggle ? (
+						<MdKeyboardArrowDown size={22} />
+					) : (
+						<MdKeyboardArrowUp size={22} />
+					)}
 				</div>
 			</div>
 			{isSelectToggle && (
@@ -32,19 +54,38 @@ const ScheduleDetailSelect: FC = () => {
 						<div>
 							<AiOutlineEye size={22} />
 						</div>
-						<div className={styles.modal_title}>
-							여행 일자별 관광 순서 및 시간 설정
-						</div>
+						<div className={styles.modal_title}>여행 일정별 계획</div>
 					</div>
-					<div>
-						<div className={styles.select_item}>
-							{`1일차`}
 
+					<div
+						className={cn(styles.select_item, {
+							[styles.active]: isSelectedPeriod === 'ALL',
+						})}
+						onClick={() => handleSelectedPeriod('ALL')}
+					>
+						{`전체일정`}
+						{isSelectedPeriod === 'ALL' && (
 							<div className={styles.icon_container}>
 								<AiOutlineCheck size={14} color="#e5855d" />
 							</div>
-						</div>
+						)}
 					</div>
+					{schedulePeriods.map((period, index) => (
+						<div
+							key={index}
+							className={cn(styles.select_item, {
+								[styles.active]: isSelectedPeriod === period.period,
+							})}
+							onClick={() => handleSelectedPeriod(period.period)}
+						>
+							{`${index + 1}일차 ${period.period}`}
+							{isSelectedPeriod === period.period && (
+								<div className={styles.icon_container}>
+									<AiOutlineCheck size={14} color="#e5855d" />
+								</div>
+							)}
+						</div>
+					))}
 				</div>
 			)}
 		</div>
