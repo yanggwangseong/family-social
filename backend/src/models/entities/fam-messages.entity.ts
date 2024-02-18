4f480b527385312705a42d7ebb10c006affe0d56
YangGwangSeong <rhkdtjd_12@naver.com>
2024-02-17 23:59:45 -0800

    :sparkles: feat(backend): fam-message entity 생성(#112)

--- backend/src/models/entities/fam-messages.entity.ts
+++ backend/src/models/entities/fam-messages.entity.ts
@@ -1,6 +1,5 @@
 import { Entity } from 'typeorm';
-
 import { DefaultEntity } from './common/default.entity';
 
-@Entity({ name: 'fam_chat' })
-export class FamChatsEntity extends DefaultEntity {}
+@Entity({ name: 'fam_message' })
+export class FamMessagesEntity extends DefaultEntity {}
