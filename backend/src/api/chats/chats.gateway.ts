8799c0b905e1388124eb8eaf3f0b85ba089555aa
YangGwangSeong <rhkdtjd_12@naver.com>
2024-02-20 02:39:15 -0800

    :sparkles: chore(backend): socket.io의 사용을 위한 nginx 설정 (#118)

--- backend/src/api/chats/chats.gateway.ts
+++ backend/src/api/chats/chats.gateway.ts
@@ -1,11 +1,27 @@
-import { OnGatewayConnection, WebSocketGateway } from '@nestjs/websockets';
-import { Socket } from 'socket.io';
+import {
+	OnGatewayConnection,
+	OnGatewayDisconnect,
+	OnGatewayInit,
+	WebSocketGateway,
+} from '@nestjs/websockets';
+import { Server, Socket } from 'socket.io';
 
 @WebSocketGateway({
 	// ws://localhost:3000/api/chats
+	transports: ['websocket'],
 	namespace: 'chats',
 })
-export class ChatsGateway implements OnGatewayConnection {
+export class ChatsGateway
+	implements OnGatewayConnection, OnGatewayDisconnect, OnGatewayInit
+{
+	afterInit(server: Server) {
+		console.log('Init', server);
+	}
+
+	handleDisconnect(client: Socket) {
+		console.log('disconnect', client);
+	}
+
 	handleConnection(socket: Socket) {
 		console.log(`connect gateway ${socket}`);
 	}
