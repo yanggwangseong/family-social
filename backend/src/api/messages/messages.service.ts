cd2caf257136f161072a1f8b1483ddc0c20617cf
YangGwangSeong <rhkdtjd_12@naver.com>
2024-02-25 23:32:35 -0800

    :sparkles: feat(backend): message 생성 기능 추가 (#122)

--- backend/src/api/messages/messages.service.ts
+++ backend/src/api/messages/messages.service.ts
@@ -1,8 +1,13 @@
 import { Injectable } from '@nestjs/common';
 
 import { MessagesRepository } from '@/models/repositories/messages.repository';
+import { ICreateMessageArgs } from '@/types/args/message';
 
 @Injectable()
 export class MessagesService {
 	constructor(private readonly messagesRepository: MessagesRepository) {}
+
+	async createMessage(createMessageArgs: ICreateMessageArgs) {
+		return this.messagesRepository.createMessage(createMessageArgs);
+	}
 }
--- backend/src/models/repositories/messages.repository.ts
+++ backend/src/models/repositories/messages.repository.ts
@@ -3,6 +3,8 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { v4 as uuidv4 } from 'uuid';
 
+import { ICreateMessageArgs } from '@/types/args/message';
+
 import { MessageEntity } from '../entities/message.entity';
 
 @Injectable()
@@ -12,5 +14,32 @@ export class MessagesRepository extends Repository<MessageEntity> {
 		private readonly repository: Repository<MessageEntity>,
 	) {
 		super(repository.target, repository.manager, repository.queryRunner);
+	}
+
+	async createMessage(createMessageArgs: ICreateMessageArgs) {
+		const message = await this.repository.insert({
+			id: uuidv4(),
+			...createMessageArgs,
+		});
+
+		return await this.repository
+			.findOneOrFail({
+				select: {
+					chat: {
+						id: true,
+					},
+				},
+				where: {
+					id: message.identifiers[0].id,
+				},
+				relations: {
+					chat: true,
+				},
+			})
+			.then((data) => {
+				return {
+					chatId: data.chat.id,
+				};
+			});
 	}
 }
