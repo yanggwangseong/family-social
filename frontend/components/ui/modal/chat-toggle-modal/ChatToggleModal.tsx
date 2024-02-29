bad067f3269fd5f9b8d2631c873e83f19fc0d0af
YangGwangSeong <rhkdtjd_12@naver.com>
2024-02-28 22:10:57 -0800

    :sparkles: add(frontend): chat-toggle-modal 컴포넌트 생성 (#135)

---
+++ frontend/components/ui/modal/chat-toggle-modal/ChatToggleModal.tsx
@@ -0,0 +1,8 @@
+import React, { FC } from 'react';
+import styles from './ChatToggleModal.module.scss';
+
+const ChatToggleModal: FC = () => {
+	return <div></div>;
+};
+
+export default ChatToggleModal;
