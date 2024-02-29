a702672206cb0358460623d58cde92f7a9800537
YangGwangSeong <rhkdtjd_12@naver.com>
2024-02-28 21:57:59 -0800

    :sparkles: feat(frontend): useModal 을 이용하여 메세지 modal toggle 기능 추가 (#135)

--- frontend/components/ui/header/Header.tsx
+++ frontend/components/ui/header/Header.tsx
@@ -1,11 +1,18 @@
-import React, { FC } from 'react';
+import React, { FC, useRef } from 'react';
 import styles from './Header.module.scss';
 import Field from '../field/Field';
 import Link from 'next/link';
 import { FaRegBell } from 'react-icons/fa';
 import { AiOutlineHome, AiOutlineMessage } from 'react-icons/ai';
+import { useModal } from '@/hooks/useModal';
 
 const Header: FC = () => {
+	const messageModalWrapperRef = useRef<HTMLDivElement>(null);
+	const {
+		isShowing: isOpenMessage,
+		handleToggleModal: handleCloseMessageModal,
+	} = useModal(messageModalWrapperRef);
+
 	return (
 		<div className={styles.header_container}>
 			<div className={styles.header_wrap}>
@@ -22,8 +29,21 @@ const Header: FC = () => {
 					<div className={styles.icon_wrap}>
 						<FaRegBell size={22}></FaRegBell>
 					</div>
-					<div className={styles.icon_wrap}>
+					<div
+						className={styles.icon_wrap}
+						ref={messageModalWrapperRef}
+						onClick={handleCloseMessageModal}
+					>
 						<AiOutlineMessage size={22}></AiOutlineMessage>
+						{isOpenMessage && (
+							<div
+								className="absolute top-14 right-0 border
+								border-solid border-customDark bg-white w-[360px] p-4
+								shadow-2xl z-50"
+							>
+								message
+							</div>
+						)}
 					</div>
 				</div>
 			</div>
