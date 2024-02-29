b10d9d20a109d92662b892092940843f7090a283
YangGwangSeong <rhkdtjd_12@naver.com>
2024-02-28 22:08:09 -0800

    :sparkles: add(frontend): message-toggle-modal 컴포넌트 생성 (#135)

---
+++ frontend/components/ui/modal/message-toggle-modal/MessageToggleModal.tsx
@@ -0,0 +1,8 @@
+import React, { FC } from 'react';
+import styles from './MessageToggleModal.module.scss';
+
+const MessageToggleModal: FC = () => {
+	return <div></div>;
+};
+
+export default MessageToggleModal;
