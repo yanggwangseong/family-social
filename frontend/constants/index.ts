f7cce5f567409318a34ff2829a1d5136c262823c
YangGwangSeong <rhkdtjd_12@naver.com>
2024-02-28 21:31:55 -0800

    :sparkles: feat(frontend): socket.io-client를 이용한 socket 통신 연동 테스트 (#134)

--- frontend/constants/index.ts
+++ frontend/constants/index.ts
@@ -1,3 +1,4 @@
 export const ACCESS_TOKEN_KEY = process.env.NEXT_PUBLIC_COOKIE_KEY;
 export const API_URL = process.env.NEXT_PUBLIC_API_URL;
 export const SSR_API_URL = process.env.NEXT_PUBLIC_SSR_API_URL;
+export const SOCKET_URL = process.env.NEXT_PUBLIC_SOCKET_URL;
