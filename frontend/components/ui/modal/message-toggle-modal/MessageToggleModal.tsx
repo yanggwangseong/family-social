f858ca0bb17668b16e8df2cbc4a50ba4a6c29d72
YangGwangSeong <rhkdtjd_12@naver.com>
2024-03-02 19:30:28 -0800

    :sparkles: feat(frontend): 채팅 메세지 작성 폼 추가 (#135)

--- frontend/components/ui/modal/message-toggle-modal/MessageToggleModal.tsx
+++ frontend/components/ui/modal/message-toggle-modal/MessageToggleModal.tsx
@@ -1,9 +1,38 @@
 import React, { FC } from 'react';
 import styles from './MessageToggleModal.module.scss';
 import Profile from '../../profile/Profile';
-import { IoCloseSharp } from 'react-icons/io5';
+import { IoCloseSharp, IoSend } from 'react-icons/io5';
+import { FaRegSmile } from 'react-icons/fa';
+import { useEmoji } from '@/hooks/useEmoji';
+import EmojiPicker, { EmojiClickData } from 'emoji-picker-react';
+import { SubmitHandler, useForm } from 'react-hook-form';
+import FieldWithTextarea from '../../field/field-area/FieldArea';
+import CustomButton from '../../button/custom-button/CustomButton';
 
 const MessageToggleModal: FC = () => {
+	const {
+		register,
+		formState: { errors, isValid, isDirty },
+		setValue,
+		handleSubmit,
+		reset,
+		getValues,
+		watch,
+		getFieldState,
+	} = useForm<{ message: string }>({
+		mode: 'onChange',
+	});
+
+	const { isEmoji, handleEmojiView, handlesetValueAddEmoji } = useEmoji<{
+		message: string;
+	}>(getValues, setValue);
+
+	const handleAddEmojiValue = (emojiData: EmojiClickData) => {
+		handlesetValueAddEmoji(emojiData, 'message');
+	};
+
+	const onSubmit: SubmitHandler<{ message: string }> = data => {};
+
 	return (
 		<div className={styles.container}>
 			<div className={styles.top_container}>
@@ -23,9 +52,7 @@ const MessageToggleModal: FC = () => {
 						<Profile />
 						<div className={styles.description_container}>
 							<div className={styles.description_wrapper}>
-								<div className={styles.description}>
-									Hell2323Hell2323Hell2323Hell2323Hell2323Hell2323
-								</div>
+								<div className={styles.description}>hi</div>
 							</div>
 						</div>
 					</div>
@@ -33,9 +60,7 @@ const MessageToggleModal: FC = () => {
 						<Profile />
 						<div className={styles.description_container}>
 							<div className={styles.description_wrapper}>
-								<div className={styles.description}>
-									Hell2323Hell2323Hell2323Hell2323Hell2323Hell2323
-								</div>
+								<div className={styles.description}>hello</div>
 							</div>
 						</div>
 					</div>
@@ -43,16 +68,57 @@ const MessageToggleModal: FC = () => {
 						<Profile />
 						<div className={styles.description_container}>
 							<div className={styles.description_wrapper}>
-								<div className={styles.description}>
-									Hell2323Hell2323Hell2323Hell2323Hell2323Hell2323
-								</div>
+								<div className={styles.description}>zzzz</div>
 							</div>
 						</div>
 					</div>
 				</div>
 
 				<div className={styles.bottom_container}>
-					<div>message</div>
+					<form
+						className={styles.bottom_form}
+						onSubmit={handleSubmit(onSubmit)}
+					>
+						<div className={styles.comment_form_container}>
+							<div className={styles.comment_emoji_container}>
+								<FaRegSmile
+									className={'cursor-pointer'}
+									size={22}
+									onClick={handleEmojiView}
+								/>
+								{isEmoji && (
+									<div className={styles.emoji_view_container}>
+										<EmojiPicker
+											height={400}
+											autoFocusSearch={false}
+											searchDisabled={true}
+											skinTonesDisabled={true}
+											onEmojiClick={handleAddEmojiValue}
+										/>
+									</div>
+								)}
+							</div>
+							<FieldWithTextarea
+								{...register('message', {
+									maxLength: {
+										value: 2000,
+										message: '최대 2000자까지 가능합니다',
+									},
+								})}
+								fieldClass="hidden_border_textarea"
+								placeholder="댓글을 입력 하세요."
+							></FieldWithTextarea>
+							<div className={styles.comment_btn_container}>
+								<CustomButton
+									type="submit"
+									className="text-customOrange font-extrabold bg-basic text-sm"
+									shadowNone={true}
+								>
+									<IoSend />
+								</CustomButton>
+							</div>
+						</div>
+					</form>
 				</div>
 			</div>
 		</div>
