14fb89dcc4aafb23969b29df60d4edc0a21f7648
YangGwangSeong <rhkdtjd_12@naver.com>
2024-03-04 21:48:45 -0800

    :lipstick: feat(frontend): 채팅 상대 검색하는 input 스타일 추가 (#146)

--- frontend/components/ui/modal/create-message-modal/CreateMessageModal.tsx
+++ frontend/components/ui/modal/create-message-modal/CreateMessageModal.tsx
@@ -1,7 +1,9 @@
 import React, { FC } from 'react';
 import styles from './CreateMessageModal.module.scss';
+import { IoCloseSharp } from 'react-icons/io5';
 import { useRecoilState } from 'recoil';
 import { createMessageModalAtom } from '@/atoms/createMessageModalAtom';
+import Field from '@/components/ui/field/Field';
 
 const CreateMessageModal: FC = () => {
 	const [createLayer, setCreateLayer] = useRecoilState<boolean>(
@@ -12,7 +14,22 @@ const CreateMessageModal: FC = () => {
 		<>
 			{createLayer && (
 				<div className={styles.container}>
-					<div>dfdfdfdsfsdf</div>
+					<div className={styles.top_container}>
+						<div className={styles.top_wrap}>
+							<div className={styles.title}>새 메세지</div>
+							<div className={styles.close_btn}>
+								<IoCloseSharp size={24} />
+							</div>
+						</div>
+					</div>
+
+					<div>
+						<Field
+							fieldClass="small_inline_input"
+							labelText={'채팅 상대'}
+							placeholder="채팅 상대를 검색 해주세요."
+						></Field>
+					</div>
 				</div>
 			)}
 		</>
