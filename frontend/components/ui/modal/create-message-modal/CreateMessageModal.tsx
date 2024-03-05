0dbb63fc385298f7fc5eed54a3c161c3980ec783
YangGwangSeong <rhkdtjd_12@naver.com>
2024-03-04 21:49:11 -0800

    :lipstick: feat(frontend): create-message-modal 퍼블리싱 (#146)

--- frontend/components/ui/modal/create-message-modal/CreateMessageModal.tsx
+++ frontend/components/ui/modal/create-message-modal/CreateMessageModal.tsx
@@ -1,9 +1,12 @@
 import React, { FC } from 'react';
 import styles from './CreateMessageModal.module.scss';
 import { IoCloseSharp } from 'react-icons/io5';
+import { MdGroups } from 'react-icons/md';
 import { useRecoilState } from 'recoil';
 import { createMessageModalAtom } from '@/atoms/createMessageModalAtom';
 import Field from '@/components/ui/field/Field';
+import Line from '../../line/Line';
+import Profile from '../../profile/Profile';
 
 const CreateMessageModal: FC = () => {
 	const [createLayer, setCreateLayer] = useRecoilState<boolean>(
@@ -29,6 +32,19 @@ const CreateMessageModal: FC = () => {
 							labelText={'채팅 상대'}
 							placeholder="채팅 상대를 검색 해주세요."
 						></Field>
+					</div>
+					<Line />
+					{/* [TODO] 검색 키워드가 없을때만 view 보이기 */}
+					<div className={styles.group_chat_container}>
+						<div className={styles.group_chat_icon_container}>
+							<MdGroups size={22} />
+						</div>
+						<div className={styles.group_chat_text}>그룹 채팅 만들기</div>
+					</div>
+					<Line />
+					<div className={styles.search_lst_container}>
+						<Profile username="양우성"></Profile>
+						<Profile username="양우성"></Profile>
 					</div>
 				</div>
 			)}
