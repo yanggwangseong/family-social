ec0efc9a4ffa92644e5c947d75a26ca2485d9fc2
YangGwangSeong <rhkdtjd_12@naver.com>
2024-03-08 23:45:11 -0800

    :sparkles: feat(backend): mail관련 env key string const로 생성 (#154)

--- backend/src/common/config/emailConfig.ts
+++ backend/src/common/config/emailConfig.ts
@@ -1,17 +1,24 @@
 import { ConfigModule, ConfigService } from '@nestjs/config';
 
+import {
+	ENV_MAIL_HOST,
+	ENV_MAIL_PORT,
+	ENV_MAIL_PWD,
+	ENV_MAIL_USER,
+} from '@/constants/env-keys.const';
+
 export const EmailOptions = {
 	imports: [ConfigModule],
 	inject: [ConfigService],
 	useFactory: async (configService: ConfigService) => {
 		const option = {
 			transport: {
-				host: configService.get<string>('MAIL_HOST'),
-				port: Number(configService.get<number>('MAIL_PORT')),
+				host: configService.get<string>(ENV_MAIL_HOST),
+				port: Number(configService.get<number>(ENV_MAIL_PORT)),
 				secure: false,
 				auth: {
-					user: configService.get<string>('MAIL_USER'),
-					pass: configService.get<string>('MAIL_PWD'),
+					user: configService.get<string>(ENV_MAIL_USER),
+					pass: configService.get<string>(ENV_MAIL_PWD),
 				},
 			},
 		};
--- backend/src/constants/env-keys.const.ts
+++ backend/src/constants/env-keys.const.ts
@@ -6,3 +6,9 @@ export const ENV_DB_USERNAME = 'DB_USERNAME' as const;
 export const ENV_DB_PASSWORD = 'DB_PASSWORD' as const;
 export const ENV_DB_DATABASE = 'DB_DATABASE' as const;
 export const ENV_DB_SYNCHRONIZE = 'DB_SYNCHRONIZE' as const;
+
+// mail
+export const ENV_MAIL_HOST = 'MAIL_HOST' as const;
+export const ENV_MAIL_PORT = 'MAIL_PORT' as const;
+export const ENV_MAIL_USER = 'MAIL_USER' as const;
+export const ENV_MAIL_PWD = 'MAIL_PWD' as const;
