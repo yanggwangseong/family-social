97fa5073755532aa278be6529ae70076f535d324
YangGwangSeong <rhkdtjd_12@naver.com>
2024-03-08 23:42:08 -0800

    :sparkles: feat(backend): db관련 env key string const로 생성 (#154)

---
+++ backend/src/constants/env-keys.const.ts
@@ -0,0 +1,8 @@
+// database
+export const ENV_DB_TYPE = 'DB_TYPE' as const;
+export const ENV_DB_HOST = 'DB_HOST' as const;
+export const ENV_DB_PORT = 'DB_PORT' as const;
+export const ENV_DB_USERNAME = 'DB_USERNAME' as const;
+export const ENV_DB_PASSWORD = 'DB_PASSWORD' as const;
+export const ENV_DB_DATABASE = 'DB_DATABASE' as const;
+export const ENV_DB_SYNCHRONIZE = 'DB_SYNCHRONIZE' as const;
