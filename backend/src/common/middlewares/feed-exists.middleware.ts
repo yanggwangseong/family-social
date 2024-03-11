71cee7efff54d5769e3204a65e5b3161b95db4aa
YangGwangSeong <rhkdtjd_12@naver.com>
2024-03-11 00:09:06 -0700

    :heavy_plus_sign: add(backend): 정확한 validation 체크를 위한 joi 라이브러리 설치 (#160)

--- backend/src/common/middlewares/feed-exists.middleware.ts
+++ backend/src/common/middlewares/feed-exists.middleware.ts
@@ -1,10 +1,28 @@
 import { Injectable, NestMiddleware } from '@nestjs/common';
 import { NextFunction, Request, Response } from 'express';
 
+import { ERROR_FEED_NOT_FOUND } from '@/constants/business-error';
+import { FeedsRepository } from '@/models/repositories/feeds.repository';
+
+import { EntityNotFoundException } from '../exception/service.exception';
+
 @Injectable()
 export class FeedExistsMiddleware implements NestMiddleware {
-	use(req: Request, res: Response, next: NextFunction) {
-		console.log('******params=*******', req.params);
+	constructor(private readonly feedsRepository: FeedsRepository) {}
+	async use(req: Request, res: Response, next: NextFunction) {
+		const feedId = req.params.feedId;
+		console.log('****=', feedId);
+
+		if (!feedId) {
+			throw EntityNotFoundException(ERROR_FEED_NOT_FOUND);
+		}
+
+		const feed = await this.feedsRepository.findFeedById(feedId);
+
+		if (!feed) {
+			throw EntityNotFoundException(ERROR_FEED_NOT_FOUND);
+		}
+
 		next();
 	}
 }
