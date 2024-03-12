243f76dc2505a5b46cb7a5ad9c2130863e9e8163
YangGwangSeong <rhkdtjd_12@naver.com>
2024-03-11 23:46:38 -0700

    :sparkles: feat(backend): groupdId exists 체크가 필요한 api에 middleware 적용 (#160)

--- backend/src/api/groups/groups.module.ts
+++ backend/src/api/groups/groups.module.ts
@@ -1,6 +1,12 @@
-import { Module } from '@nestjs/common';
+import {
+	MiddlewareConsumer,
+	Module,
+	NestModule,
+	RequestMethod,
+} from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
+import { GroupExistsMiddleware } from '@/common/middlewares/group-exists.middleware';
 import { FamEntity } from '@/models/entities/fam.entity';
 import { GroupEntity } from '@/models/entities/group.entity';
 import { FamsRepository } from '@/models/repositories/fams.repository';
@@ -23,4 +29,14 @@ import { SchedulesModule } from '../schedules/schedules.module';
 	providers: [GroupsService, GroupsRepository, FamsRepository],
 	exports: [],
 })
-export class GroupsModule {}
+export class GroupsModule implements NestModule {
+	configure(consumer: MiddlewareConsumer) {
+		consumer
+			.apply(GroupExistsMiddleware)
+			.exclude(
+				{ path: 'groups', method: RequestMethod.GET },
+				{ path: 'groups', method: RequestMethod.POST },
+			)
+			.forRoutes(GroupsController);
+	}
+}
