960df3204e759b971a63460e8ca837c16a3efcdb
YangGwangSeong <rhkdtjd_12@naver.com>
2024-03-26 23:05:58 -0700

    :lipstick: style(frontend): 일정 일자를 선택하는 table 모바일 퍼블리싱 (#215)

--- frontend/components/ui/table/Table.tsx
+++ frontend/components/ui/table/Table.tsx
@@ -12,14 +12,16 @@ const Table: FC<PropsWithChildren<TableProps>> = ({
 				{headerColumns.map((tr, index) => (
 					<tr key={index}>
 						{tr.column.map((data, index) => (
-							<th
-								key={index}
-								className={styles.table_header_column}
-								colSpan={data.colspan}
-								rowSpan={data.rowspan}
-							>
-								{data.item}
-							</th>
+							<>
+								<th
+									key={index}
+									className={styles.table_header_column}
+									colSpan={data.colspan}
+									rowSpan={data.rowspan}
+								>
+									{data.item}
+								</th>
+							</>
 						))}
 					</tr>
 				))}
