76e17888e27a6f156b40c81318b8d861817c3e40
YangGwangSeong <rhkdtjd_12@naver.com>
2024-03-29 23:23:00 -0700

    :sparkles: add(frontend): 멤버 프로필과 그룹 프로필을 합친 컴포넌트 퍼블리싱 (#213)

--- frontend/components/ui/profile/group-and-member-profile/GroupAndMemberProfile.tsx
+++ frontend/components/ui/profile/group-and-member-profile/GroupAndMemberProfile.tsx
@@ -5,20 +5,26 @@ import Image from 'next/image';
 const GroupAndMemberProfile: FC = () => {
 	return (
 		<div className={styles.container}>
-			<div className={styles.group_profile_container}>
-				<Image
-					fill
-					src={'/images/banner/sm/group-base-sm.png'}
-					alt="img"
-				></Image>
-				<div className={styles.profile_container}>
+			<div>
+				<div className={styles.group_profile_container}>
 					<Image
-						className="rounded-full"
 						fill
-						src={'/images/profile/profile.png'}
+						src={'/images/banner/sm/group-base-sm.png'}
 						alt="img"
 					></Image>
+					<div className={styles.profile_container}>
+						<Image
+							className="rounded-full"
+							fill
+							src={'/images/profile/profile.png'}
+							alt="img"
+						></Image>
+					</div>
 				</div>
+			</div>
+			<div className={styles.description_container}>
+				<div className={styles.group_name}>토트넘응원그룹</div>
+				<div className={styles.member_name}>양광성</div>
 			</div>
 		</div>
 	);
