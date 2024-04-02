d2dd3b656080f224646b3a04f0ff942b14becf59
YangGwangSeong <rhkdtjd_12@naver.com>
2024-04-01 22:25:44 -0700

    :sparkles:  fix(frontend): 인터페이스의 유니온을 사용하기 위한 타입 수정 (#219)

--- frontend/components/ui/swiper/swiper-container.interface.ts
+++ frontend/components/ui/swiper/swiper-container.interface.ts
@@ -1,7 +1,21 @@
 import { MediaInfo } from '@/shared/interfaces/media.interface';
 import { SwiperOptions } from 'swiper/types';
 
-export interface SwiperContainerProps {
+// export interface SwiperContainerProps {
+// 	list: MediaInfo[] | any[];
+// 	overrideSwiperOptions?: SwiperOptions;
+// }
+
+export interface SwiperContainerMediaInfo {
 	list: MediaInfo[];
 	overrideSwiperOptions?: SwiperOptions;
 }
+
+export interface SwiperContainerImagesGallary {
+	list: any[];
+	overrideSwiperOptions?: SwiperOptions;
+}
+
+export type SwiperContainerProps =
+	| SwiperContainerMediaInfo
+	| SwiperContainerImagesGallary;
