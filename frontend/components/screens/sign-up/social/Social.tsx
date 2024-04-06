5bd9a5f270940dc7384f0b14406e514ece533bb6
YangGwangSeong <rhkdtjd_12@naver.com>
2024-04-06 04:52:09 -0700

    :lipstick: style(frontend): 소셜회원 회원가입 페이지 퍼블리싱 (#217)

--- frontend/components/screens/sign-up/social/Social.tsx
+++ frontend/components/screens/sign-up/social/Social.tsx
@@ -1,8 +1,77 @@
 import React, { FC } from 'react';
-import styels from './Social.module.scss';
+import styles from './Social.module.scss';
+import Image from 'next/image';
+import Format from '@/components/ui/layout/Format';
+import Field from '@/components/ui/field/Field';
+import Line from '@/components/ui/line/Line';
+import CustomButton from '@/components/ui/button/custom-button/CustomButton';
 
 const Social: FC = () => {
-	return <div>소셜회원가입</div>;
+	return (
+		<Format title="social-signup">
+			<div className={styles.container}>
+				<div className={styles.contents_card}>
+					<div className={styles.contents_wrap}>
+						<div className={styles.signin__header_title}>
+							GOOGLE 계정을 이용한 회원가입
+						</div>
+						<div className={styles.signin__header_subtitle}>
+							이메일과 비밀번호를 이용하여 로그인 할 수 있습니다.
+						</div>
+
+						<div className={styles.profile_img_selected_container}>
+							<div className={styles.profile_img_title}>프로필 이미지 선택</div>
+							<div className={styles.img_container}>
+								<div className={styles.profile_img_container}>
+									<Image
+										className="rounded-full"
+										width={60}
+										height={60}
+										src={'/images/profile/profile.png'}
+										alt="img"
+									></Image>
+									<div>gaengdev</div>
+								</div>
+								<div className={styles.profile_img_container}>
+									<Image
+										className="rounded-full"
+										width={60}
+										height={60}
+										src={'/images/profile/profile.png'}
+										alt="img"
+									></Image>
+									<div>gaengdev</div>
+								</div>
+							</div>
+						</div>
+						{/* 라인 */}
+						<Line />
+
+						<div className={styles.section_subtitle}>추가 정보 입력</div>
+
+						<form className={styles.form}>
+							<div className={styles.form_label}>이름</div>
+							<Field placeholder="이름을 입력 해주세요!" />
+							<div className={styles.form_label}>전화번호</div>
+							<Field
+								maxLength={11}
+								placeholder="휴대폰 번호를 '-'를 제외하고 입력 해주세요."
+							/>
+						</form>
+					</div>
+					{/* 하단 버튼 컨테이너 */}
+					<div className={styles.footer_wrap}>
+						<CustomButton
+							type="submit"
+							className="bg-customOrange text-customDark font-bold border border-solid border-customDark rounded-full p-[10px] w-full"
+						>
+							회원가입
+						</CustomButton>
+					</div>
+				</div>
+			</div>
+		</Format>
+	);
 };
 
 export default Social;
