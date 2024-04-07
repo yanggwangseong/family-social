26880d044f6deaea1b2e856c471847a0d5a1b0ea
YangGwangSeong <rhkdtjd_12@naver.com>
2024-04-06 19:02:04 -0700

    :sparkles: fix(frontend): 버튼 form 이벤트를 위한 form 태그 위치 변경 (#217)

--- frontend/components/screens/sign-up/social/Social.tsx
+++ frontend/components/screens/sign-up/social/Social.tsx
@@ -47,48 +47,48 @@ const Social: FC = () => {
 			<div className={styles.container}>
 				{data && (
 					<div className={styles.contents_card}>
-						<div className={styles.contents_wrap}>
-							<div className={styles.signin__header_title}>
-								GOOGLE 계정을 이용한 회원가입
-							</div>
-							<div className={styles.signin__header_subtitle}>
-								이메일과 비밀번호를 이용하여 로그인 할 수 있습니다.
-							</div>
-
-							<div className={styles.profile_img_selected_container}>
-								<div className={styles.profile_img_title}>
-									프로필 이미지 선택
+						<form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
+							<div className={styles.contents_wrap}>
+								<div className={styles.signin__header_title}>
+									GOOGLE 계정을 이용한 회원가입
 								</div>
-								<div className={styles.img_container}>
-									<div className={styles.profile_img_container}>
-										<Image
-											className="rounded-full"
-											width={60}
-											height={60}
-											src={data.profileImage}
-											alt="img"
-										></Image>
-										<div>{data.username}</div>
+								<div className={styles.signin__header_subtitle}>
+									이메일과 비밀번호를 이용하여 로그인 할 수 있습니다.
+								</div>
+
+								<div className={styles.profile_img_selected_container}>
+									<div className={styles.profile_img_title}>
+										프로필 이미지 선택
 									</div>
-									<div className={styles.profile_img_container}>
-										<Image
-											className="rounded-full"
-											width={60}
-											height={60}
-											src={'/images/profile/profile.png'}
-											alt="img"
-										></Image>
-										<div>{data.username}</div>
+									<div className={styles.img_container}>
+										<div className={styles.profile_img_container}>
+											<Image
+												className="rounded-full"
+												width={60}
+												height={60}
+												src={data.profileImage}
+												alt="img"
+											></Image>
+											<div>{data.username}</div>
+										</div>
+										<div className={styles.profile_img_container}>
+											<Image
+												className="rounded-full"
+												width={60}
+												height={60}
+												src={'/images/profile/profile.png'}
+												alt="img"
+											></Image>
+											<div>{data.username}</div>
+										</div>
 									</div>
 								</div>
-							</div>
 
-							{/* 라인 */}
-							<Line />
+								{/* 라인 */}
+								<Line />
 
-							<div className={styles.section_subtitle}>추가 정보 입력</div>
+								<div className={styles.section_subtitle}>추가 정보 입력</div>
 
-							<form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
 								<div className={styles.form_label}>이름</div>
 								<Field
 									{...register('username', {
@@ -100,6 +100,7 @@ const Social: FC = () => {
 									})}
 									placeholder="이름을 입력 해주세요!"
 									error={errors.username}
+									defaultValue={data.username}
 								/>
 								<div className={styles.form_label}>전화번호</div>
 								<Field
@@ -118,18 +119,18 @@ const Social: FC = () => {
 									placeholder="휴대폰 번호를 '-'를 제외하고 입력 해주세요."
 									error={errors.phoneNumber}
 								/>
-							</form>
-						</div>
-						{/* 하단 버튼 컨테이너 */}
-						<div className={styles.footer_wrap}>
-							<CustomButton
-								type="submit"
-								className="bg-customOrange text-customDark font-bold border border-solid border-customDark rounded-full p-[10px] w-full"
-								disabled={!isValid}
-							>
-								회원가입
-							</CustomButton>
-						</div>
+							</div>
+							{/* 하단 버튼 컨테이너 */}
+							<div className={styles.footer_wrap}>
+								<CustomButton
+									type="submit"
+									className="bg-customOrange text-customDark font-bold border border-solid border-customDark rounded-full p-[10px] w-full"
+									disabled={!isValid}
+								>
+									회원가입
+								</CustomButton>
+							</div>
+						</form>
 					</div>
 				)}
 			</div>
