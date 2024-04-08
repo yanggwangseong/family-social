54ede07baf410545ef407c04f0c22089ce7ad719
YangGwangSeong <rhkdtjd_12@naver.com>
2024-04-08 03:48:08 -0700

    :sparkles: feat(backend): NotificationEntity 프로퍼티 설정하기 (#232)

--- backend/src/models/entities/notification.entity.ts
+++ backend/src/models/entities/notification.entity.ts
@@ -1,6 +1,110 @@
-import { Entity } from 'typeorm';
+import { ApiProperty } from '@nestjs/swagger';
+import {
+	IsBoolean,
+	IsNotEmpty,
+	IsString,
+	IsUUID,
+	MaxLength,
+} from 'class-validator';
+import { Column, Entity, PrimaryColumn } from 'typeorm';
+
+import { booleanValidationMessage } from '@/common/validation-message/boolean-validation-message';
+import { maxLengthValidationMessage } from '@/common/validation-message/max-length-validation-message';
+import { notEmptyValidationMessage } from '@/common/validation-message/not-empty-validation-message';
+import { stringValidationMessage } from '@/common/validation-message/string-validation-message';
+import { uuidValidationMessage } from '@/common/validation-message/uuid-validation-message';
 
 import { DefaultEntity } from './common/default.entity';
 
 @Entity({ name: 'fam_notification' })
-export class NotificationEntity extends DefaultEntity {}
+export class NotificationEntity extends DefaultEntity {
+	@PrimaryColumn('uuid')
+	@ApiProperty({
+		nullable: false,
+	})
+	@IsNotEmpty({
+		message: notEmptyValidationMessage,
+	})
+	@IsUUID(4, { message: uuidValidationMessage })
+	public readonly notificationTypeId!: string;
+
+	@PrimaryColumn('uuid')
+	@ApiProperty({
+		nullable: false,
+	})
+	@IsNotEmpty({
+		message: notEmptyValidationMessage,
+	})
+	@IsUUID(4, { message: uuidValidationMessage })
+	public readonly recipientId!: string;
+
+	@PrimaryColumn('uuid')
+	@ApiProperty({
+		nullable: false,
+	})
+	@IsNotEmpty({
+		message: notEmptyValidationMessage,
+	})
+	@IsUUID(4, { message: uuidValidationMessage })
+	public readonly senderId!: string;
+
+	@Column({ type: 'varchar', length: 60, nullable: false })
+	@ApiProperty({
+		maxLength: 60,
+		nullable: false,
+	})
+	@IsNotEmpty({
+		message: notEmptyValidationMessage,
+	})
+	@IsString({
+		message: stringValidationMessage,
+	})
+	@MaxLength(60, { message: maxLengthValidationMessage })
+	notificationTitle!: string;
+
+	@Column({ type: 'varchar', length: 100, nullable: false })
+	@ApiProperty({
+		maxLength: 60,
+		nullable: false,
+	})
+	@IsNotEmpty({
+		message: notEmptyValidationMessage,
+	})
+	@IsString({
+		message: stringValidationMessage,
+	})
+	@MaxLength(60, { message: maxLengthValidationMessage })
+	notificationDescription!: string;
+
+	@Column({ type: 'uuid', nullable: true })
+	@ApiProperty({
+		nullable: true,
+	})
+	@IsNotEmpty({
+		message: notEmptyValidationMessage,
+	})
+	@IsUUID(4, { message: uuidValidationMessage })
+	notificationFeedId?: string;
+
+	@Column({ type: 'uuid', nullable: true })
+	@ApiProperty({
+		nullable: true,
+	})
+	@IsNotEmpty({
+		message: notEmptyValidationMessage,
+	})
+	@IsUUID(4, { message: uuidValidationMessage })
+	notificationCommentId?: string;
+
+	@Column({ type: 'boolean', nullable: false, default: false })
+	@ApiProperty({
+		nullable: false,
+	})
+	@IsNotEmpty({
+		message: notEmptyValidationMessage,
+	})
+	@IsBoolean({
+		message: booleanValidationMessage,
+	})
+	isRead!: boolean;
+}
