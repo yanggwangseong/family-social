7de00778c3773293c59e1a3666525dc0db6ee7b3
YangGwangSeong <rhkdtjd_12@naver.com>
2024-04-08 23:33:09 -0700

    :sparkles: feat(backend): 내가 작성한 피드에 누군가 댓글을 달았을때 notification 생성 기능 (#234)

--- backend/src/api/notifications/notifications.module.ts
+++ backend/src/api/notifications/notifications.module.ts
@@ -1,16 +1,24 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
+import { NotificationTypeEntity } from '@/models/entities/notification-type.entity';
 import { NotificationEntity } from '@/models/entities/notification.entity';
+import { NotificationTypeRepository } from '@/models/repositories/notification-type.repository';
 import { NotificationsRepository } from '@/models/repositories/notifications.repository';
 
 import { NotificationsController } from './notifications.controller';
 import { NotificationsService } from './notifications.service';
 
 @Module({
-	imports: [TypeOrmModule.forFeature([NotificationEntity])],
+	imports: [
+		TypeOrmModule.forFeature([NotificationEntity, NotificationTypeEntity]),
+	],
 	controllers: [NotificationsController],
-	providers: [NotificationsService, NotificationsRepository],
+	providers: [
+		NotificationsService,
+		NotificationsRepository,
+		NotificationTypeRepository,
+	],
 	exports: [NotificationsService],
 })
 export class NotificationsModule {}
--- backend/src/api/notifications/notifications.service.ts
+++ backend/src/api/notifications/notifications.service.ts
@@ -1,10 +1,29 @@
 import { Injectable } from '@nestjs/common';
 
+import { NotificationTypeRepository } from '@/models/repositories/notification-type.repository';
 import { NotificationsRepository } from '@/models/repositories/notifications.repository';
+import { ICreateCommentOnMyPostNotificationArgs } from '@/types/args/notification';
 
 @Injectable()
 export class NotificationsService {
 	constructor(
 		private readonly notificationsRepository: NotificationsRepository,
+		private readonly notificationTypeRepository: NotificationTypeRepository,
 	) {}
+
+	async createCommentOnMyPostNotification(
+		notificationArgs: ICreateCommentOnMyPostNotificationArgs,
+	) {
+		const { notificationType, ...rest } = notificationArgs;
+
+		const { notificationTypeId } =
+			await this.notificationTypeRepository.findNotificationTypeId(
+				notificationType,
+			);
+
+		await this.notificationsRepository.createNotification({
+			notificationTypeId,
+			...rest,
+		});
+	}
 }
