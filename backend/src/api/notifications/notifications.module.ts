f84dd2bcc9f29957f05c5941bb82210f6d690f58
YangGwangSeong <rhkdtjd_12@naver.com>
2024-04-08 19:38:09 -0700

    :sparkles: add(backend): Notification module 생성 (#234)

---
+++ backend/src/api/notifications/notifications.module.ts
@@ -0,0 +1,16 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { NotificationEntity } from '@/models/entities/notification.entity';
+import { NotificationsRepository } from '@/models/repositories/notifications.repository';
+
+import { NotificationsController } from './notifications.controller';
+import { NotificationsService } from './notifications.service';
+
+@Module({
+	imports: [TypeOrmModule.forFeature([NotificationEntity])],
+	controllers: [NotificationsController],
+	providers: [NotificationsService, NotificationsRepository],
+	exports: [],
+})
+export class NotificationsModule {}
