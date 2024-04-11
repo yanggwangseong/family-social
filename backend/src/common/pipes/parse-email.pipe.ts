b593b96521bb4ea1fad450d30258da5acd875f77
YangGwangSeong <rhkdtjd_12@naver.com>
2024-04-10 23:37:20 -0700

    :sparkles: feat(backend): parse-email-pipe에 email 에러 const 메세지 추가

--- backend/src/common/pipes/parse-email.pipe.ts
+++ backend/src/common/pipes/parse-email.pipe.ts
@@ -1,6 +1,8 @@
 import { Injectable, PipeTransform } from '@nestjs/common';
 import { z } from 'zod';
 
+import { ERROR_EMAIL_PIPE_MESSAGE } from '@/constants/business-error';
+
 import { BadRequestServiceException } from '../exception/service.exception';
 
 @Injectable()
@@ -11,7 +13,7 @@ export class ParseEmailPipe implements PipeTransform {
 		const validationResult = schema.safeParse(value);
 
 		if (validationResult.success === false) {
-			throw BadRequestServiceException('이메일 형식이 올바르지 않습니다');
+			throw BadRequestServiceException(ERROR_EMAIL_PIPE_MESSAGE);
 		}
 
 		return value.toString();
