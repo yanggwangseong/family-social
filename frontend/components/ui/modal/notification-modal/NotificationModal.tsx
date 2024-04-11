e33815ab8a78565bbf20a79b189f92b35b24fb36
YangGwangSeong <rhkdtjd_12@naver.com>
2024-04-10 20:46:39 -0700

    :sparkles: feat(frontend): 알림 리스트 가져오기 (#233)

--- frontend/components/ui/modal/notification-modal/NotificationModal.tsx
+++ frontend/components/ui/modal/notification-modal/NotificationModal.tsx
@@ -1,7 +1,15 @@
 import React, { FC } from 'react';
 import styles from './NotificationModal.module.scss';
+import { useQuery } from 'react-query';
+import { NotificationService } from '@/services/notification/notification.service';
+import Skeleton from '../../skeleton/Skeleton';
 
 const NotificationModal: FC = () => {
+	const { data, isLoading } = useQuery(
+		['get-notifications'],
+		async () => await NotificationService.getNotifications(),
+	);
+
 	return (
 		<div className={styles.container}>
 			<div className={styles.top_wrap}>
@@ -10,10 +18,17 @@ const NotificationModal: FC = () => {
 					<div className={styles.right_text}>전체보기</div>
 				</div>
 			</div>
+			{isLoading && <Skeleton />}
 
-			<div className={styles.item_container}>
-				<div>items</div>
-			</div>
+			{data && (
+				<div className={styles.item_container}>
+					{data.list.length === 0 ? (
+						<div className={styles.not_found_text}>알람이 없습니다.</div>
+					) : (
+						<div>items</div>
+					)}
+				</div>
+			)}
 		</div>
 	);
 };
