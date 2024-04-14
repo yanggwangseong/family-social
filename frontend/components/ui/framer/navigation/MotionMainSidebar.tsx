698daf52b432b21d89b06ed4f3b693d681670b35
YangGwangSeong <rhkdtjd_12@naver.com>
2024-04-13 23:46:50 -0700

    :sparkles: feat(frontend): 메인 랜딩 페이지 애니메이션 효과 (#244)

---
+++ frontend/components/ui/framer/navigation/MotionMainSidebar.tsx
@@ -0,0 +1,13 @@
+import React, { FC } from 'react';
+import { motion } from 'framer-motion';
+
+const variants = {
+	open: { opacity: 1, x: 0 },
+	closed: { opacity: 0, x: '-100%' },
+};
+
+const MotionMainSidebar: FC = () => {
+	return <div></div>;
+};
+
+export default MotionMainSidebar;
