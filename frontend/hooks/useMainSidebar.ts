a7b6fbc3daf9a438e6798b7821093bead4a84f2f
YangGwangSeong <rhkdtjd_12@naver.com>
2024-04-13 22:20:34 -0700

    :sparkles: feat(frontend): useEffect와 window.resize 이벤트를 이용하여 모바일 상태에 따른 mainSidebar 상태값 설정 (#249)

--- frontend/hooks/useMainSidebar.ts
+++ frontend/hooks/useMainSidebar.ts
@@ -1,14 +1,38 @@
 import { mainSidebarAtom } from '@/atoms/mainSidebarAtom';
-import { useState } from 'react';
+import { useEffect, useLayoutEffect, useState } from 'react';
 import { useRecoilState } from 'recoil';
 
 export const useMainSidebar = () => {
 	const [isLeftSidebarShowing, setIsLeftSidebarShowing] =
 		useRecoilState(mainSidebarAtom);
 
+	const [isMobile, setIsMobile] = useState(false);
+
 	const handleCloseMainSidebar = () => {
 		isLeftSidebarShowing && setIsLeftSidebarShowing(false);
 	};
+
+	useEffect(() => {
+		const handleResize = () => {
+			setIsMobile(window.innerWidth <= 768); // 예시: 모바일 화면 너비가 768px 이하인 경우를 기준으로 설정
+		};
+
+		handleResize(); // 처음 렌더링시 사이즈에 따라 모바일 여부 설정
+		window.addEventListener('resize', handleResize);
+
+		return () => {
+			window.removeEventListener('resize', handleResize);
+		};
+	}, []);
+
+	// // PC 환경에서는 항상 사이드바를 보여주기 위해 isMobile 상태에 따라 값 설정
+	useEffect(() => {
+		if (!isMobile) {
+			setIsLeftSidebarShowing(true);
+		} else {
+			setIsLeftSidebarShowing(false);
+		}
+	}, [isMobile, setIsLeftSidebarShowing]);
 
 	return {
 		isLeftSidebarShowing,
