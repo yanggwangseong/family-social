db9ace8a220e45683c728cfd1a632a9822254baa
YangGwangSeong <rhkdtjd_12@naver.com>
2024-04-19 23:34:31 -0700

    :sparkles: feat(frontend): 모든 layer-modal에 적용되는 toggle-animation 추가 (#244)

--- frontend/components/ui/modal/layer-modal/LayerModal.tsx
+++ frontend/components/ui/modal/layer-modal/LayerModal.tsx
@@ -3,6 +3,9 @@ import React, { FC } from 'react';
 import { useRecoilState } from 'recoil';
 import styles from './LayerModal.module.scss';
 import LayerModalControll from './LayerModalControll';
+import { motion } from 'framer-motion';
+import { toggleWrapperVariant } from '@/utils/animation/toggle-variant';
+import cn from 'classnames';
 
 const LayerModal: FC = () => {
 	const [isShowing, setIsShowing] = useRecoilState<boolean>(modalAtom);
@@ -10,24 +13,29 @@ const LayerModal: FC = () => {
 
 	return (
 		<>
-			{isShowing && (
-				<div className={styles.modal_mask}>
-					<div className={styles.modal_container}>
-						<div className={styles.modal_top_container}>
-							<div className={styles.modal_title_wrap}>
-								{isLayer.modal_title}
-							</div>
-							<div
-								className={styles.modal_close_wrap}
-								onClick={() => setIsShowing(false)}
-							>
-								x
-							</div>
+			<motion.div
+				className={cn(styles.container, {
+					[styles.modal_mask]: !!isShowing,
+				})}
+				initial={false}
+				animate={isShowing ? 'open' : 'closed'}
+			>
+				<motion.div
+					className={styles.modal_container}
+					variants={toggleWrapperVariant}
+				>
+					<div className={styles.modal_top_container}>
+						<div className={styles.modal_title_wrap}>{isLayer.modal_title}</div>
+						<div
+							className={styles.modal_close_wrap}
+							onClick={() => setIsShowing(false)}
+						>
+							x
 						</div>
-						<LayerModalControll status={isLayer.layer}></LayerModalControll>
 					</div>
-				</div>
-			)}
+					<LayerModalControll status={isLayer.layer}></LayerModalControll>
+				</motion.div>
+			</motion.div>
 		</>
 	);
 };
