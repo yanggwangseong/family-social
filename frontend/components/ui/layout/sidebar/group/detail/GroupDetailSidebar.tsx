13d149a0a8d99b7ac2f21b0cc3f3f1e31e567bcb
YangGwangSeong <rhkdtjd_12@naver.com>
2024-04-20 21:03:03 -0700

    :sparkles: feat(frontend): 그룹 디테일 왼쪽 사이드바 애니메이션 적용 (#244)

--- frontend/components/ui/layout/sidebar/group/detail/GroupDetailSidebar.tsx
+++ frontend/components/ui/layout/sidebar/group/detail/GroupDetailSidebar.tsx
@@ -27,6 +27,7 @@ import { useModal } from '@/hooks/useModal';
 import { useMainSidebar } from '@/hooks/useMainSidebar';
 import { motion } from 'framer-motion';
 import { BUTTONGESTURE, INLINEBUTTONGESTURE } from '@/utils/animation/gestures';
+import { useMenuAnimation } from '@/hooks/useMenuAnimation';
 
 const GroupDetailSidebar: FC<{ groupId: string }> = ({ groupId }) => {
 	const [isToggleSetting, setToggleSetting] = useState<boolean>(true);
@@ -49,122 +50,132 @@ const GroupDetailSidebar: FC<{ groupId: string }> = ({ groupId }) => {
 		handleCloseMainSidebar,
 	} = useMainSidebar();
 
+	const { sidebarScope } = useMenuAnimation(isLeftSidebarShowing);
+
 	return (
 		<>
-			{isLeftSidebarShowing && (
-				<div className={styles.sidebar_container}>
-					<GroupProfile
-						group={{
-							id: 'sdfsdf',
-							groupDescription: '한국을 좋아하는 그룹입니다',
-							groupName: 'korea',
-						}}
-					/>
+			<motion.div
+				className={styles.sidebar_container}
+				ref={sidebarScope}
+				initial={{ transform: 'translateX(-100%)' }} // 초기 상태: 왼쪽으로 이동하여 숨김
+			>
+				<motion.div
+					className={styles.mobile_close_btn}
+					onClick={handleCloseMainSidebar}
+				>
+					x
+				</motion.div>
+				<GroupProfile
+					group={{
+						id: 'sdfsdf',
+						groupDescription: '한국을 좋아하는 그룹입니다',
+						groupName: 'korea',
+					}}
+				/>
 
-					{/* 라인 */}
-					<Line />
+				{/* 라인 */}
+				<Line />
 
-					<div className={styles.sidebar_member_container}>
-						<div className={styles.icon_container}>
-							<PiUserDuotone size={24} color="#0a0a0a" />
-						</div>
-						<div className={styles.member_text}>1명</div>
+				<div className={styles.sidebar_member_container}>
+					<div className={styles.icon_container}>
+						<PiUserDuotone size={24} color="#0a0a0a" />
 					</div>
+					<div className={styles.member_text}>1명</div>
+				</div>
 
-					{/* 라인 */}
-					<Line />
+				{/* 라인 */}
+				<Line />
 
-					<div className={styles.sidebar_btn_container}>
-						<div
-							className={styles.toggle_menu_btn_container}
-							ref={invitationModalWrapperRef}
-						>
-							<CustomButton
-								type="button"
-								className="bg-customOrange text-customDark 
+				<div className={styles.sidebar_btn_container}>
+					<div
+						className={styles.toggle_menu_btn_container}
+						ref={invitationModalWrapperRef}
+					>
+						<CustomButton
+							type="button"
+							className="bg-customOrange text-customDark 
 					font-bold border border-solid border-customDark 
 					rounded-full p-[10px] w-full
 					hover:bg-orange-500
 					"
-								onClick={handleCloseInvitationModal}
-							>
-								+ 초대하기
-							</CustomButton>
-							{isOpenInvitation && (
-								<ToggleModal
-									list={InviteMenu}
-									onClose={handleCloseInvitationModal}
-								/>
-							)}
-						</div>
-						<div
-							className={styles.toggle_menu_icon_container}
-							ref={settingModalWrapperRef}
+							onClick={handleCloseInvitationModal}
 						>
-							<motion.div {...BUTTONGESTURE}>
-								<BsThreeDots size={22} onClick={handleCloseSettingModal} />
-							</motion.div>
-							{isOpenSetting && (
-								<ToggleModal
-									list={GroupSettingMenu}
-									onClose={handleCloseSettingModal}
-								/>
-							)}
-						</div>
+							+ 초대하기
+						</CustomButton>
+						{isOpenInvitation && (
+							<ToggleModal
+								list={InviteMenu}
+								onClose={handleCloseInvitationModal}
+							/>
+						)}
 					</div>
-					<motion.div
-						className={styles.sidebar_home_btn_container}
-						onClick={handleCloseMainSidebar}
-						{...INLINEBUTTONGESTURE}
+					<div
+						className={styles.toggle_menu_icon_container}
+						ref={settingModalWrapperRef}
 					>
-						<div className={styles.home_menu_container}>
-							<div className={styles.icon_container}>
-								<PiHouseDuotone size={22} />
-							</div>
-							<div className={styles.home_menu_text}>
-								<div>커뮤니티 홈</div>
-							</div>
-						</div>
-					</motion.div>
-
-					<Line />
-
-					<div className={styles.management_menu_container}>
-						<div
-							className={styles.management_toggle_menu_container}
-							onClick={() => setToggleSetting(!isToggleSetting)}
-						>
-							<div className={styles.menu_text}>관리자 도구</div>
-							<div className={styles.toggle_icon}>
-								{isToggleSetting ? (
-									<IoIosArrowUp size={22} />
-								) : (
-									<IoIosArrowDown size={22} />
-								)}
-							</div>
-						</div>
-						{isToggleSetting && (
-							<motion.div {...INLINEBUTTONGESTURE}>
-								<Link
-									className={styles.menu_item_container}
-									href={`/groups/${groupId}/edit`}
-									onClick={handleCloseMainSidebar}
-								>
-									<div className={styles.icon_container}>
-										<PiGearSixDuotone size={22} />
-									</div>
-									<div className={styles.menu_container}>
-										<div>그룹 설정</div>
-										<div className={styles.description}>
-											그룹 정보 수정 등 관리
-										</div>
-									</div>
-								</Link>
-							</motion.div>
+						<motion.div {...BUTTONGESTURE}>
+							<BsThreeDots size={22} onClick={handleCloseSettingModal} />
+						</motion.div>
+						{isOpenSetting && (
+							<ToggleModal
+								list={GroupSettingMenu}
+								onClose={handleCloseSettingModal}
+							/>
 						)}
 					</div>
 				</div>
-			)}
+				<motion.div
+					className={styles.sidebar_home_btn_container}
+					onClick={handleCloseMainSidebar}
+					{...INLINEBUTTONGESTURE}
+				>
+					<div className={styles.home_menu_container}>
+						<div className={styles.icon_container}>
+							<PiHouseDuotone size={22} />
+						</div>
+						<div className={styles.home_menu_text}>
+							<div>커뮤니티 홈</div>
+						</div>
+					</div>
+				</motion.div>
+
+				<Line />
+
+				<div className={styles.management_menu_container}>
+					<div
+						className={styles.management_toggle_menu_container}
+						onClick={() => setToggleSetting(!isToggleSetting)}
+					>
+						<div className={styles.menu_text}>관리자 도구</div>
+						<div className={styles.toggle_icon}>
+							{isToggleSetting ? (
+								<IoIosArrowUp size={22} />
+							) : (
+								<IoIosArrowDown size={22} />
+							)}
+						</div>
+					</div>
+					{isToggleSetting && (
+						<motion.div {...INLINEBUTTONGESTURE}>
+							<Link
+								className={styles.menu_item_container}
+								href={`/groups/${groupId}/edit`}
+								onClick={handleCloseMainSidebar}
+							>
+								<div className={styles.icon_container}>
+									<PiGearSixDuotone size={22} />
+								</div>
+								<div className={styles.menu_container}>
+									<div>그룹 설정</div>
+									<div className={styles.description}>
+										그룹 정보 수정 등 관리
+									</div>
+								</div>
+							</Link>
+						</motion.div>
+					)}
+				</div>
+			</motion.div>
 		</>
 	);
 };
