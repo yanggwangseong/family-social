54175536964fd18e1257e3587d17b30722ba8449
YangGwangSeong <rhkdtjd_12@naver.com>
2024-04-23 20:30:04 -0700

    :sparkles: feat(frontend): ErrorTemplateContainer 컴포넌트 생성 (#246)

---
+++ frontend/components/ui/error-container/ErrorContainer.tsx
@@ -0,0 +1,31 @@
+import React, { FC } from 'react';
+import { ErrorContainerProps } from './error-container.interface';
+
+const ErrorContainer: FC<ErrorContainerProps> = ({ title, handleGoMain }) => {
+	return (
+		<div>
+			<h1>{title}</h1>
+			<h3>서비스 에러가 발생 하였습니다.</h3>
+			<p>
+				<br />
+				죄송합니다.
+				<br />
+				기술적인 문제로
+				<br />
+				일시적으로 서비스를 사용할 수 없습니다.
+				<br />
+				관리자에게 요청하여
+				<br />
+				빠른 시간 안에 해결 하겠습니다.
+			</p>
+
+			<div>
+				<button type="button" onClick={handleGoMain}>
+					메인 사이트로 돌아가기
+				</button>
+			</div>
+		</div>
+	);
+};
+
+export default ErrorContainer;
--- frontend/pages/ErrorBoundary.tsx
+++ frontend/pages/ErrorBoundary.tsx
@@ -1,3 +1,4 @@
+import ErrorContainer from '@/components/ui/error-container/ErrorContainer';
 import React, { ErrorInfo, ReactNode } from 'react';
 
 interface ErrorBoundaryProps {
@@ -40,33 +41,14 @@ class ErrorBoundary extends React.Component<
 	render() {
 		if (this.state.hasError) {
 			return (
-				<div>
-					<h1>{this.props.title}</h1>
-					<h3>서비스 에러가 발생 하였습니다.</h3>
-					<p>
-						<br />
-						죄송합니다.
-						<br />
-						기술적인 문제로
-						<br />
-						일시적으로 서비스를 사용할 수 없습니다.
-						<br />
-						관리자에게 요청하여
-						<br />
-						빠른 시간 안에 해결 하겠습니다.
-					</p>
-
-					<div>
-						<button type="button" onClick={this.handleGoMain}>
-							메인 사이트로 돌아가기
-						</button>
-					</div>
-				</div>
+				<ErrorContainer
+					title={this.props.title}
+					handleGoMain={this.handleGoMain}
+				/>
 			);
 		}
 
 		// Return children components in case of no error
-
 		return this.props.children;
 	}
 }
