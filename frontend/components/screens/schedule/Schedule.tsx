cf416073c5d4427146df75f1a6357676954a63df
YangGwangSeong <rhkdtjd_12@naver.com>
2024-04-25 00:16:30 -0700

    :sparkles: feat(frontend): 여행일정에 IntersectionObserver훅 적용 (#110)

--- frontend/components/screens/schedule/Schedule.tsx
+++ frontend/components/screens/schedule/Schedule.tsx
@@ -14,74 +14,78 @@ import { PiPencilDuotone } from 'react-icons/pi';
 import { AnimatePresence, motion } from 'framer-motion';
 import Link from 'next/link';
 import { BUTTONGESTURE } from '@/utils/animation/gestures';
+import { useScheduleIntersectionObserver } from '@/hooks/useScheduleIntersectionObserver';
+import { useInfiniteSelect } from '@/hooks/useInfiniteSelect';
 
 const Schedule: FC = () => {
-	const {
-		data,
-		fetchNextPage,
-		hasNextPage,
-		isLoading,
-		isError,
-		isRefetching,
-		refetch,
-	} = useInfiniteQuery(
-		['schedules'],
-		async ({ pageParam = 1 }) =>
-			await ScheduleService.getScheduleList(
-				pageParam,
-				'75aca3da-1dac-48ef-84b8-cdf1be8fe37d',
-				4,
-			),
-		{
-			getNextPageParam: (lastPage, allPosts) => {
-				return lastPage.page !== allPosts[0].totalPage
-					? lastPage.page + 1
-					: undefined;
-			},
-		},
-	);
+	// const {
+	// 	data,
+	// 	fetchNextPage,
+	// 	hasNextPage,
+	// 	isLoading,
+	// 	isError,
+	// 	isRefetching,
+	// 	refetch,
+	// } = useInfiniteQuery(
+	// 	['schedules'],
+	// 	async ({ pageParam = 1 }) =>
+	// 		await ScheduleService.getScheduleList(
+	// 			pageParam,
+	// 			'75aca3da-1dac-48ef-84b8-cdf1be8fe37d',
+	// 			4,
+	// 		),
+	// 	{
+	// 		getNextPageParam: (lastPage, allPosts) => {
+	// 			return lastPage.page !== allPosts[0].totalPage
+	// 				? lastPage.page + 1
+	// 				: undefined;
+	// 		},
+	// 	},
+	// );
 
-	const [observedPost, setObservedPost] = useState('');
+	// const [observedPost, setObservedPost] = useState('');
 
-	useEffect(() => {
-		const observeElement = (element: HTMLElement | null) => {
-			if (!element) return;
-			// 브라우저 viewport와 설정한 요소(Element)와 교차점을 관찰
-			const observer = new IntersectionObserver(
-				// entries는 IntersectionObserverEntry 인스턴스의 배열
-				entries => {
-					console.log('entries', entries);
-					// isIntersecting: 관찰 대상의 교차 상태(Boolean)
-					if (entries[0].isIntersecting === true) {
-						fetchNextPage();
-						observer.unobserve(element); //이전에 observe 하고 있던걸 없애준다.
-					}
-				},
-				{ threshold: 1 },
-			);
-			// 대상 요소의 관찰을 시작
-			observer.observe(element);
-		};
+	// useEffect(() => {
+	// 	const observeElement = (element: HTMLElement | null) => {
+	// 		if (!element) return;
+	// 		// 브라우저 viewport와 설정한 요소(Element)와 교차점을 관찰
+	// 		const observer = new IntersectionObserver(
+	// 			// entries는 IntersectionObserverEntry 인스턴스의 배열
+	// 			entries => {
+	// 				console.log('entries', entries);
+	// 				// isIntersecting: 관찰 대상의 교차 상태(Boolean)
+	// 				if (entries[0].isIntersecting === true) {
+	// 					fetchNextPage();
+	// 					observer.unobserve(element); //이전에 observe 하고 있던걸 없애준다.
+	// 				}
+	// 			},
+	// 			{ threshold: 1 },
+	// 		);
+	// 		// 대상 요소의 관찰을 시작
+	// 		observer.observe(element);
+	// 	};
 
-		//포스트가 없다면 return
-		if (
-			!data?.pages[data?.pages.length - 1].list ||
-			data?.pages[data?.pages.length - 1].list.length === 0
-		)
-			return;
-		//posts 배열안에 마지막 post에 id를 가져옵니다.
-		const id =
-			data?.pages[data?.pages.length - 1].list[
-				data?.pages[data?.pages.length - 1].list.length - 1
-			]['id'];
-		//posts 배열에 post가 추가되서 마지막 post가 바뀌었다면
-		// 바뀐 post중 마지막post를 observedPost로
-		if (id !== observedPost) {
-			setObservedPost(id);
-			observeElement(document.getElementById(id));
-		}
-		return () => {};
-	}, [data, fetchNextPage, observedPost]);
+	// 	//포스트가 없다면 return
+	// 	if (
+	// 		!data?.pages[data?.pages.length - 1].list ||
+	// 		data?.pages[data?.pages.length - 1].list.length === 0
+	// 	)
+	// 		return;
+	// 	//posts 배열안에 마지막 post에 id를 가져옵니다.
+	// 	const id =
+	// 		data?.pages[data?.pages.length - 1].list[
+	// 			data?.pages[data?.pages.length - 1].list.length - 1
+	// 		]['id'];
+	// 	//posts 배열에 post가 추가되서 마지막 post가 바뀌었다면
+	// 	// 바뀐 post중 마지막post를 observedPost로
+	// 	if (id !== observedPost) {
+	// 		setObservedPost(id);
+	// 		observeElement(document.getElementById(id));
+	// 	}
+	// 	return () => {};
+	// }, [data, fetchNextPage, observedPost]);
+
+	const { data, isLoading, isRefetching } = useScheduleIntersectionObserver();
 
 	return (
 		<Format title={'schedule'}>
