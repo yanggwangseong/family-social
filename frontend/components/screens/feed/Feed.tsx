6c98323b510d68265baff95d6828170f29f9697c
YangGwangSeong <rhkdtjd_12@naver.com>
2024-04-25 00:41:24 -0700

    :sparkles: feat(frontend): useFeedIntersectionObserver훅 피드 페이지에 적용 (#110)

--- frontend/components/screens/feed/Feed.tsx
+++ frontend/components/screens/feed/Feed.tsx
@@ -27,6 +27,7 @@ import { PiPencilDuotone } from 'react-icons/pi';
 import { useCreateFeed } from '@/hooks/useCreateFeed';
 import { AnimatePresence, motion } from 'framer-motion';
 import { BUTTONGESTURE } from '@/utils/animation/gestures';
+import { useFeedIntersectionObserver } from '@/hooks/useFeedIntersectionObserver';
 
 const Feed: FC = () => {
 	const router = useRouter();
@@ -35,6 +36,20 @@ const Feed: FC = () => {
 	const { handleIsLottie, lottieRef, handleLottieComplete } = useLottieLike();
 
 	const { handleCreateFeed } = useCreateFeed();
+
+	const {
+		data,
+		fetchNextPage,
+		hasNextPage,
+		isLoading,
+		isError,
+		isRefetching,
+		refetch,
+	} = useFeedIntersectionObserver(
+		['feeds'],
+		async ({ pageParam = 1 }) =>
+			await FeedService.getFeeds(pageParam, query.options),
+	);
 
 	// const handleLike = () => {
 	// 	if (lottieRef.current) {
@@ -120,28 +135,28 @@ const Feed: FC = () => {
 	// 	},
 	// );
 
-	const {
-		data,
-		fetchNextPage,
-		hasNextPage,
-		isLoading,
-		isError,
-		isRefetching,
-		refetch,
-	} = useInfiniteQuery(
-		['feeds'],
-		async ({ pageParam = 1 }) =>
-			await FeedService.getFeeds(pageParam, query.options),
-		{
-			getNextPageParam: (lastPage, allPosts) => {
-				return lastPage.page !== allPosts[0].totalPage
-					? lastPage.page + 1
-					: undefined;
-			},
-		},
-	);
+	// const {
+	// 	data,
+	// 	fetchNextPage,
+	// 	hasNextPage,
+	// 	isLoading,
+	// 	isError,
+	// 	isRefetching,
+	// 	refetch,
+	// } = useInfiniteQuery(
+	// 	['feeds'],
+	// 	async ({ pageParam = 1 }) =>
+	// 		await FeedService.getFeeds(pageParam, query.options),
+	// 	{
+	// 		getNextPageParam: (lastPage, allPosts) => {
+	// 			return lastPage.page !== allPosts[0].totalPage
+	// 				? lastPage.page + 1
+	// 				: undefined;
+	// 		},
+	// 	},
+	// );
 
-	const [observedPost, setObservedPost] = useState('');
+	//const [observedPost, setObservedPost] = useState('');
 
 	// const handleUpdateLike = (feedId: string, page: number) => {
 	// 	feedLikeSync({ feedId, page });
@@ -158,46 +173,46 @@ const Feed: FC = () => {
 		handleIsLottie,
 	});
 
-	useEffect(() => {
-		const observeElement = (element: HTMLElement | null) => {
-			if (!element) return;
-			// 브라우저 viewport와 설정한 요소(Element)와 교차점을 관찰
-			const observer = new IntersectionObserver(
-				// entries는 IntersectionObserverEntry 인스턴스의 배열
-				entries => {
-					console.log('entries', entries);
-					// isIntersecting: 관찰 대상의 교차 상태(Boolean)
-					if (entries[0].isIntersecting === true) {
-						console.log('마지막 포스트에 왔습니다');
-						fetchNextPage();
-						observer.unobserve(element); //이전에 observe 하고 있던걸 없애준다.
-					}
-				},
-				{ threshold: 1 },
-			);
-			// 대상 요소의 관찰을 시작
-			observer.observe(element);
-		};
+	// useEffect(() => {
+	// 	const observeElement = (element: HTMLElement | null) => {
+	// 		if (!element) return;
+	// 		// 브라우저 viewport와 설정한 요소(Element)와 교차점을 관찰
+	// 		const observer = new IntersectionObserver(
+	// 			// entries는 IntersectionObserverEntry 인스턴스의 배열
+	// 			entries => {
+	// 				console.log('entries', entries);
+	// 				// isIntersecting: 관찰 대상의 교차 상태(Boolean)
+	// 				if (entries[0].isIntersecting === true) {
+	// 					console.log('마지막 포스트에 왔습니다');
+	// 					fetchNextPage();
+	// 					observer.unobserve(element); //이전에 observe 하고 있던걸 없애준다.
+	// 				}
+	// 			},
+	// 			{ threshold: 1 },
+	// 		);
+	// 		// 대상 요소의 관찰을 시작
+	// 		observer.observe(element);
+	// 	};
 
-		//포스트가 없다면 return
-		if (
-			!data?.pages[data?.pages.length - 1].list ||
-			data?.pages[data?.pages.length - 1].list.length === 0
-		)
-			return;
-		//posts 배열안에 마지막 post에 id를 가져옵니다.
-		const id =
-			data?.pages[data?.pages.length - 1].list[
-				data?.pages[data?.pages.length - 1].list.length - 1
-			].feedId;
-		//posts 배열에 post가 추가되서 마지막 post가 바뀌었다면
-		// 바뀐 post중 마지막post를 observedPost로
-		if (id !== observedPost) {
-			setObservedPost(id);
-			observeElement(document.getElementById(id));
-		}
-		return () => {};
-	}, [data, fetchNextPage, observedPost]);
+	// 	//포스트가 없다면 return
+	// 	if (
+	// 		!data?.pages[data?.pages.length - 1].list ||
+	// 		data?.pages[data?.pages.length - 1].list.length === 0
+	// 	)
+	// 		return;
+	// 	//posts 배열안에 마지막 post에 id를 가져옵니다.
+	// 	const id =
+	// 		data?.pages[data?.pages.length - 1].list[
+	// 			data?.pages[data?.pages.length - 1].list.length - 1
+	// 		].feedId;
+	// 	//posts 배열에 post가 추가되서 마지막 post가 바뀌었다면
+	// 	// 바뀐 post중 마지막post를 observedPost로
+	// 	if (id !== observedPost) {
+	// 		setObservedPost(id);
+	// 		observeElement(document.getElementById(id));
+	// 	}
+	// 	return () => {};
+	// }, [data, fetchNextPage, observedPost]);
 
 	return (
 		<Format title={'feed'}>
