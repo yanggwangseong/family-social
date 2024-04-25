f14e98e96d9439d111e3157798312fec4a19f8a4
YangGwangSeong <rhkdtjd_12@naver.com>
2024-04-24 20:10:29 -0700

    :sparkles: feat(frontend): 해당 그룹에 해당하는 피드 글 가져오기 (#208)

--- frontend/components/screens/group/detail/GroupDetail.tsx
+++ frontend/components/screens/group/detail/GroupDetail.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useRef, useState } from 'react';
+import React, { FC, useEffect, useRef, useState } from 'react';
 import styles from './GroupDetail.module.scss';
 import Format from '@/components/ui/layout/Format';
 import { useRouter } from 'next/router';
@@ -11,17 +11,35 @@ import ToggleModal from '@/components/ui/modal/ToggleModal';
 import { InviteMenu } from '@/components/ui/modal/toggle-menu.constants';
 import { useModal } from '@/hooks/useModal';
 import { PiPencilDuotone } from 'react-icons/pi';
-import { motion } from 'framer-motion';
+import { AnimatePresence, motion } from 'framer-motion';
 import TabMenu from '@/components/ui/tab-menu/TabMenu';
 import { groupTabMenus } from '@/components/ui/tab-menu/tab-menu.constants';
 import { BUTTONGESTURE } from '@/utils/animation/gestures';
 import { useCreateFeed } from '@/hooks/useCreateFeed';
+import LottieLike from '@/components/ui/lottie/LottieLike';
+import { useLottieLike } from '@/hooks/useLottieLike';
+import { useFeedLike } from '@/hooks/useFeedLike';
+import { useCommentLike } from '@/hooks/useCommentLike';
+import { useInfiniteQuery } from 'react-query';
+import { FeedService } from '@/services/feed/feed.service';
+import { GroupService } from '@/services/group/group.service';
+import Skeleton from '@/components/ui/skeleton/Skeleton';
+import FeedItem from '@/components/ui/feed/FeedItem';
 
 const GroupDetail: FC = () => {
 	const router = useRouter();
+
+	const query = router.query as {
+		options: 'GROUPFEED' | 'GROUPMEMBER' | 'GROUPEVENT';
+	};
+
+	const [observedPost, setObservedPost] = useState('');
+
 	const { groupId } = router.query as { groupId: string };
 
 	const { handleCreateFeed } = useCreateFeed();
+
+	const { handleIsLottie, lottieRef, handleLottieComplete } = useLottieLike();
 
 	const invitationModalWrapperRef = useRef<HTMLDivElement>(null);
 	const {
@@ -29,12 +47,96 @@ const GroupDetail: FC = () => {
 		handleToggleModal: handleCloseInvitationModal,
 	} = useModal(invitationModalWrapperRef);
 
+	const {
+		data,
+		fetchNextPage,
+		hasNextPage,
+		isLoading,
+		isError,
+		isRefetching,
+		refetch,
+	} = useInfiniteQuery(
+		['feeds'],
+		async ({ pageParam = 1 }) =>
+			await GroupService.getFeedsOfGroup(
+				pageParam,
+				query.options ?? 'GROUPFEED',
+				groupId,
+			),
+		{
+			getNextPageParam: (lastPage, allPosts) => {
+				return lastPage.page !== allPosts[0].totalPage
+					? lastPage.page + 1
+					: undefined;
+			},
+			enabled: !!groupId,
+		},
+	);
+
+	const handleRefetch = (pageValue: number) => {
+		refetch({ refetchPage: (page, index) => index === pageValue - 1 });
+	};
+
+	const { handleUpdateLike } = useFeedLike({ handleRefetch, handleIsLottie });
+
+	const { handleLikeComment } = useCommentLike({
+		handleRefetch,
+		handleIsLottie,
+	});
+
+	useEffect(() => {
+		const observeElement = (element: HTMLElement | null) => {
+			if (!element) return;
+			// 브라우저 viewport와 설정한 요소(Element)와 교차점을 관찰
+			const observer = new IntersectionObserver(
+				// entries는 IntersectionObserverEntry 인스턴스의 배열
+				entries => {
+					console.log('entries', entries);
+					// isIntersecting: 관찰 대상의 교차 상태(Boolean)
+					if (entries[0].isIntersecting === true) {
+						console.log('마지막 포스트에 왔습니다');
+						fetchNextPage();
+						observer.unobserve(element); //이전에 observe 하고 있던걸 없애준다.
+					}
+				},
+				{ threshold: 1 },
+			);
+			// 대상 요소의 관찰을 시작
+			observer.observe(element);
+		};
+
+		//포스트가 없다면 return
+		if (
+			!data?.pages[data?.pages.length - 1].list ||
+			data?.pages[data?.pages.length - 1].list.length === 0
+		)
+			return;
+		//posts 배열안에 마지막 post에 id를 가져옵니다.
+		const id =
+			data?.pages[data?.pages.length - 1].list[
+				data?.pages[data?.pages.length - 1].list.length - 1
+			].feedId;
+		//posts 배열에 post가 추가되서 마지막 post가 바뀌었다면
+		// 바뀐 post중 마지막post를 observedPost로
+		if (id !== observedPost) {
+			setObservedPost(id);
+			observeElement(document.getElementById(id));
+		}
+		return () => {};
+	}, [data, fetchNextPage, observedPost]);
+
 	return (
 		<Format title={'group-detail'}>
 			<div className={styles.container}>
 				{/* 헤더 */}
 				<Header />
 				<div className={styles.contents_container}>
+					{/* lottie-like */}
+					<LottieLike
+						lottieRef={lottieRef}
+						onLottieComplete={handleLottieComplete}
+					/>
+
 					<GroupDetailSidebar groupId={groupId} />
 					<div className={styles.detail_container}>
 						<div className={styles.main}>
@@ -96,7 +198,33 @@ const GroupDetail: FC = () => {
 								<div className={styles.tap_menu_container}>
 									<TabMenu list={groupTabMenus} options={'GROUPFEED'} />
 								</div>
-								<div>피드</div>
+								<div className={styles.feed_container}>
+									{isLoading && <Skeleton />}
+
+									{data?.pages.map((page, pageIndex) => (
+										<AnimatePresence key={pageIndex}>
+											{page.list.map((feed, index) => (
+												<FeedItem
+													key={feed.feedId}
+													feed={feed}
+													index={index}
+													onLike={handleUpdateLike}
+													page={page.page}
+													onRefetch={handleRefetch}
+													onLikeComment={handleLikeComment}
+												/>
+											))}
+										</AnimatePresence>
+									))}
+
+									{isRefetching && (
+										<React.Fragment>
+											<Skeleton />
+											<Skeleton />
+											<Skeleton />
+										</React.Fragment>
+									)}
+								</div>
 							</div>
 						</div>
 						<div
