6d54d688ae7e171e66975dbb5ccc67d3efcf66fa
YangGwangSeong <rhkdtjd_12@naver.com>
2024-04-28 03:54:40 -0700

    :sparkles: feat(backend): mention의 typeId를 가져오는 findMentionTypeId 메서드 추가 (#248)

--- backend/src/models/repositories/mention-type.repository.ts
+++ backend/src/models/repositories/mention-type.repository.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { QueryRunner, Repository } from 'typeorm';
 
+import { MentionType, Union } from '@/types';
+
 import { MentionTypeEntity } from '../entities/mention-type.entity';
 
 @Injectable()
@@ -17,5 +19,23 @@ export class MentionTypeRepository extends Repository<MentionTypeEntity> {
 		return qr
 			? qr.manager.getRepository<MentionTypeEntity>(MentionTypeEntity)
 			: this.repository;
+	}
+
+	async findMentionTypeId(mentionType: Union<typeof MentionType>) {
+		return await this.repository
+			.findOneOrFail({
+				select: {
+					id: true,
+					mentionType: true,
+				},
+				where: {
+					mentionType,
+				},
+			})
+			.then((item) => {
+				return {
+					mentionTypId: item.id,
+				};
+			});
 	}
 }
