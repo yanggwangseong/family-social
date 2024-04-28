a8b2810b2581ddbf95775656671e74dfd516aeff
YangGwangSeong <rhkdtjd_12@naver.com>
2024-04-28 00:48:39 -0700

    :sparkles: feat(backend): mentions-type-repository 생성 (#248)

--- backend/src/models/repositories/mention-type.repository.ts
+++ backend/src/models/repositories/mention-type.repository.ts
@@ -2,20 +2,20 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { QueryRunner, Repository } from 'typeorm';
 
-import { MentionEntity } from '../entities/mention.entity';
+import { MentionTypeEntity } from '../entities/mention-type.entity';
 
 @Injectable()
-export class MentionsRepository extends Repository<MentionEntity> {
+export class MentionTypeRepository extends Repository<MentionTypeEntity> {
 	constructor(
-		@InjectRepository(MentionEntity)
-		private readonly repository: Repository<MentionEntity>,
+		@InjectRepository(MentionTypeEntity)
+		private readonly repository: Repository<MentionTypeEntity>,
 	) {
 		super(repository.target, repository.manager, repository.queryRunner);
 	}
 
 	getScheduleRepository(qr?: QueryRunner) {
 		return qr
-			? qr.manager.getRepository<MentionEntity>(MentionEntity)
+			? qr.manager.getRepository<MentionTypeEntity>(MentionTypeEntity)
 			: this.repository;
 	}
 }
