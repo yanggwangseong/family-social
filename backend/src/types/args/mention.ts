8eb0e0137f14fca6fd90f5014ce91f71330e6066
YangGwangSeong <rhkdtjd_12@naver.com>
2024-04-28 19:05:11 -0700

    :sparkles: feat(backend): ICreateMentionArgs 타입 추가 (#248)

---
+++ backend/src/types/args/mention.ts
@@ -0,0 +1,13 @@
+import { QueryRunner } from 'typeorm';
+
+import { MentionCreateReqDto } from '@/models/dto/mention/req/mention-create-req.dto';
+import { MentionEntity } from '@/models/entities/mention.entity';
+
+import { MentionType, Union } from '../index';
+
+export interface ICreateMentionArgs
+	extends Pick<MentionEntity, 'mentionSenderId' | 'mentionFeedId'> {
+	mentionType: Union<typeof MentionType>;
+	mentions: MentionCreateReqDto[];
+	qr?: QueryRunner;
+}
