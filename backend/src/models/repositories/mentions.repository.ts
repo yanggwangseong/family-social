573fd5c514d47af08d0b289adb45cbc68a8d9952
YangGwangSeong <rhkdtjd_12@naver.com>
2024-04-28 19:06:59 -0700

    :sparkles: feat(backend): repository createMentions 메서드 추가 (#248)

--- backend/src/models/repositories/mentions.repository.ts
+++ backend/src/models/repositories/mentions.repository.ts
@@ -1,7 +1,10 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { QueryRunner, Repository } from 'typeorm';
+import { QueryDeepPartialEntity } from 'typeorm/query-builder/QueryPartialEntity';
+import { v4 as uuidv4 } from 'uuid';
 
+import { MentionCreateReqDto } from '../dto/mention/req/mention-create-req.dto';
 import { MentionEntity } from '../entities/mention.entity';
 
 @Injectable()
@@ -13,9 +16,28 @@ export class MentionsRepository extends Repository<MentionEntity> {
 		super(repository.target, repository.manager, repository.queryRunner);
 	}
 
-	getScheduleRepository(qr?: QueryRunner) {
+	getMentionRepository(qr?: QueryRunner) {
 		return qr
 			? qr.manager.getRepository<MentionEntity>(MentionEntity)
 			: this.repository;
+	}
+
+	async createMentions(
+		mentions: MentionCreateReqDto[],
+		overrideInsertFeilds: Omit<
+			QueryDeepPartialEntity<MentionEntity>,
+			'id' | 'mentionRecipientId'
+		>,
+		qr?: QueryRunner,
+	) {
+		const mentionsRepository = this.getMentionRepository(qr);
+
+		mentions.map(async (mention: MentionCreateReqDto) => {
+			await mentionsRepository.insert({
+				id: uuidv4(),
+				mentionRecipientId: mention.mentionMemberId,
+				...overrideInsertFeilds,
+			});
+		});
 	}
 }
