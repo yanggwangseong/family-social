11f3ef50f8e1118f35f355218510566244f65bc0
YangGwangSeong <rhkdtjd_12@naver.com>
2024-05-09 00:33:19 -0700

    :sparkles: feat(frontend): mention-match-pattern const 파일 생성 (#277)

---
+++ frontend/constants/mention-match-pattern.const.ts
@@ -0,0 +1,1 @@
+export const MENTION_MATCH_PATTERN = /@\[([^\]]+)\]\(([^)]+)\)/g;
--- frontend/utils/extract-mention.ts
+++ frontend/utils/extract-mention.ts
@@ -1,12 +1,12 @@
 import { CreateMentionRequest } from '@/components/ui/form/comment-form.interface';
+import { MENTION_MATCH_PATTERN } from '@/constants/mention-match-pattern.const';
 
 export const extractMention = (contents: string) => {
-	const regex = /@\[([^\]]+)\]\(([^)]+)\)/g;
 	const matches: CreateMentionRequest[] = [];
 
 	let match;
 	let i = 0;
-	while ((match = regex.exec(contents)) !== null) {
+	while ((match = MENTION_MATCH_PATTERN.exec(contents)) !== null) {
 		matches.push({
 			mentionMemberId: match[2],
 			mentionPosition: i++,
