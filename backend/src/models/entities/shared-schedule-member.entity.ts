fed37dbd322a9277e5402641b5065b8b62731ec2
YangGwangSeong <rhkdtjd_12@naver.com>
2024-05-13 22:04:41 -0700

    :sparkles: feat(backend): 여행일정 공유 테이블 관계 설정 (#101)

--- backend/src/models/entities/shared-schedule-member.entity.ts
+++ backend/src/models/entities/shared-schedule-member.entity.ts
@@ -1,9 +1,12 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsUUID } from 'class-validator';
-import { Entity, PrimaryColumn } from 'typeorm';
+import { Entity, JoinColumn, ManyToOne, PrimaryColumn } from 'typeorm';
 
 import { notEmptyValidationMessage } from '@/common/validation-message/not-empty-validation-message';
 import { uuidValidationMessage } from '@/common/validation-message/uuid-validation-message';
+
+import { FamEntity } from './fam.entity';
+import { ScheduleEntity } from './schedule.entity';
 
 @Entity({ name: 'fam_shared_schedule_member' })
 export class SharedScheduleMember {
@@ -26,4 +29,12 @@ export class SharedScheduleMember {
 	})
 	@IsUUID(4, { message: uuidValidationMessage })
 	public readonly sharedFamId!: string;
+
+	@ManyToOne(() => ScheduleEntity, (sc) => sc.sharedMembers)
+	@JoinColumn({ name: 'sharedScheduleId', referencedColumnName: 'id' })
+	sharedSchedule!: ScheduleEntity;
+
+	@ManyToOne(() => FamEntity, (fm) => fm.sharedMembers)
+	@JoinColumn({ name: 'sharedFamId', referencedColumnName: 'id' })
+	sharedMember!: FamEntity;
 }
