0e33a55d784f2c0f14f3357c62cd8bdae5eea242
YangGwangSeong <rhkdtjd_12@naver.com>
2024-05-23 04:24:44 -0700

    :sparkles: feat(frontend): 위 아래로 크기를 resize 조절 할 수 있는 오른쪽 사이드바 생성 (#291)

--- frontend/components/ui/layout/sidebar/schedule/ScheduleSidebar.tsx
+++ frontend/components/ui/layout/sidebar/schedule/ScheduleSidebar.tsx
@@ -1,10 +1,11 @@
-import React, { FC, useEffect, useState } from 'react';
+import React, { FC, useCallback, useEffect, useRef, useState } from 'react';
 import styles from './ScheduleSidebar.module.scss';
 
 import { ScheduleSidebarProps } from './schedule-sidebar.interface';
 import cn from 'classnames';
-
+import { PiEqualsBold } from 'react-icons/pi';
 import ScheduleSidebarController from './ScheduleSidebarController';
+import { useResizeVertical } from '@/hooks/useResizeVertical';
 
 const ScheduleSidebar: FC<ScheduleSidebarProps> = ({
 	isSelecteGroup,
@@ -12,6 +13,14 @@ const ScheduleSidebar: FC<ScheduleSidebarProps> = ({
 	isStartEndPeriod,
 	isPage,
 }) => {
+	const maxSidebarHeight = window.innerHeight - 80;
+	const minSidebarHeight = 330;
+
+	const { handleTouchStart, handleMouseDown, sidebarRef } = useResizeVertical(
+		maxSidebarHeight,
+		minSidebarHeight,
+	);
+
 	if (isPage === 'selectGroupPage') return null;
 
 	return (
@@ -19,7 +28,15 @@ const ScheduleSidebar: FC<ScheduleSidebarProps> = ({
 			className={cn(styles.right_sidebar_container, {
 				[styles.mobile_toruism_sidebar]: isPage === 'tourismPage',
 			})}
+			ref={sidebarRef}
 		>
+			<div
+				className={styles.resize_panel_btn}
+				onMouseDown={handleMouseDown}
+				onTouchStart={handleTouchStart}
+			>
+				<PiEqualsBold size={24} />
+			</div>
 			<ScheduleSidebarController
 				isSelecteGroup={isSelecteGroup}
 				isScheduleName={isScheduleName}
