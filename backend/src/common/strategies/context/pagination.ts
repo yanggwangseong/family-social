2efbf3ef7c678cd2fc37456408e044d7e15a3a69
YangGwangSeong <rhkdtjd_12@naver.com>
2024-05-28 01:53:30 -0700

    :sparkles: feat(backend): pagination-strategy 패턴 적용을 위한 context 클래스 생성 (#254)

---
+++ backend/src/common/strategies/context/pagination.ts
@@ -0,0 +1,29 @@
+import { FindManyOptions, ObjectLiteral, Repository } from 'typeorm';
+
+import { DefaultPaginationReqDto } from '@/models/dto/pagination/req/default-pagination-req.dto';
+
+export interface PaginationStrategy<T extends ObjectLiteral> {
+	paginate(
+		dto: DefaultPaginationReqDto,
+		repository: Repository<T>,
+		overrideFindOptions: FindManyOptions<T>,
+		path?: string,
+	): Promise<any>;
+}
+
+export class Pagination<T extends ObjectLiteral> {
+	private strategy!: PaginationStrategy<T>;
+
+	setStrategy(strategy: PaginationStrategy<T>) {
+		this.strategy = strategy;
+	}
+
+	paginate(
+		dto: DefaultPaginationReqDto,
+		repository: Repository<T>,
+		overrideFindOptions: FindManyOptions<T> = {},
+		path?: string,
+	) {
+		return this.strategy.paginate(dto, repository, overrideFindOptions, path);
+	}
+}
