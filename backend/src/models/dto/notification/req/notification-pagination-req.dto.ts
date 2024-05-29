66bbc290111d880130cb67cb8beace30fc55f4b3
YangGwangSeong <rhkdtjd_12@naver.com>
2024-05-28 22:32:23 -0700

    :sparkles: feat(backend): IsIn validation 데코레이터 커스텀 메세지 생성 (#254)

--- backend/src/models/dto/notification/req/notification-pagination-req.dto.ts
+++ backend/src/models/dto/notification/req/notification-pagination-req.dto.ts
@@ -1,10 +1,11 @@
 import { IsIn } from 'class-validator';
 
+import { isInValidationMessage } from '@/common/validation-message/is-in-validation-message';
 import { Union, isReadOptions } from '@/types';
 
 import { DefaultPaginationReqDto } from '../../pagination/req/default-pagination-req.dto';
 
 export class NotificationPaginationReqDto extends DefaultPaginationReqDto {
-	@IsIn(isReadOptions)
+	@IsIn(isReadOptions, { message: isInValidationMessage })
 	is_read_options!: Union<typeof isReadOptions>;
 }
