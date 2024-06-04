cd2a83648a01ea50e1a472cd6c02125fafb66479
YangGwangSeong <rhkdtjd_12@naver.com>
2024-06-03 22:57:58 -0700

    :sparkles: feat(backend): notification-paginate-res-dto를 basic-pagination-res-dto로 변경 (#305)

--- frontend/services/notification/notification.service.ts
+++ frontend/services/notification/notification.service.ts
@@ -5,7 +5,7 @@ import { Union, isReadOptions } from 'types';
 export const NotificationService = {
 	async getNotifications(readOptions: Union<typeof isReadOptions>) {
 		const { data } = await axiosAPI.get<NotificationResponse>(
-			`/notifications?is_read_options=${readOptions}`,
+			`/notifications?is_read_options=${readOptions}&limit=10`,
 		);
 
 		return data;
