6485b0706d38dd3ba207428c4b18ae6f075bd50e
YangGwangSeong <rhkdtjd_12@naver.com>
2024-06-08 23:43:53 -0700

    :sparkles: feat(backend): mail-send-log entity 작성 (#274)

--- backend/src/models/entities/mail-send-log.entity.ts
+++ backend/src/models/entities/mail-send-log.entity.ts
@@ -1,6 +1,47 @@
-import { Entity } from 'typeorm';
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsUUID } from 'class-validator';
+import {
+	Column,
+	CreateDateColumn,
+	Entity,
+	Index,
+	PrimaryColumn,
+} from 'typeorm';
 
-import { DefaultEntity } from './common/default.entity';
+import { notEmptyValidationMessage } from '@/common/validation-message/not-empty-validation-message';
+import { uuidValidationMessage } from '@/common/validation-message/uuid-validation-message';
 
 @Entity({ name: 'fam_mail_send_log' })
-export class MailSendLogEntity extends DefaultEntity {}
+@Index(['createdAt'])
+export class MailSendLogEntity {
+	@ApiProperty()
+	@PrimaryColumn('uuid')
+	@IsUUID(4, { each: true, message: uuidValidationMessage })
+	@IsNotEmpty({
+		message: notEmptyValidationMessage,
+	})
+	id!: string;
+
+	@Column({ type: 'varchar', length: 60, nullable: false })
+	toEmail!: string;
+
+	@Column({ type: 'varchar', length: 100, nullable: false })
+	mailSubject!: string;
+
+	@Column({
+		type: 'boolean',
+		nullable: false,
+	})
+	sendStatus!: boolean;
+
+	@Column({ type: 'varchar', length: 120, nullable: true })
+	reasonMessage!: string;
+
+	@ApiProperty()
+	@CreateDateColumn({
+		type: 'timestamp',
+		precision: 3,
+		default: () => 'CURRENT_TIMESTAMP',
+	})
+	createdAt!: Date;
+}
