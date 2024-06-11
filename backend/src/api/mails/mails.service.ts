ee9eda248ceae42d4e338696966bfca1ce62cc34
YangGwangSeong <rhkdtjd_12@naver.com>
2024-06-10 22:41:40 -0700

    :sparkles: feat(backend): mail-service에서 메일 전송시 mail-log 테이블에 저장 (#274)

--- backend/src/api/mails/mails.service.ts
+++ backend/src/api/mails/mails.service.ts
@@ -64,7 +64,8 @@ export class MailsService {
 		);
 
 		const newMailLog = this.createMailSendLogs(sendResult);
-		console.log(newMailLog);
+
+		await this.mailSendLogRepository.createMailSendLog(newMailLog);
 	}
 
 	async sendSignUpEmailVerify(
@@ -72,47 +73,48 @@ export class MailsService {
 		signupVerifyToken: string,
 		qr?: QueryRunner,
 	): Promise<void> {
-		const sendResult = await Promise.allSettled(
-			await this.mailerService.sendMail({
-				to: email,
-				subject: '이메일 인증',
-				text: '이메일 인증',
-				html: `<!DOCTYPE html>
-				<html lang="ko">
-				<head>
-					<meta charset="UTF-8">
-					<meta name="viewport" content="width=device-width, initial-scale=1.0">
-					<title>이메일 인증</title>
-				</head>
-				<body
-					style='font-family: Arial, sans-serif;
-						   margin: 0;
-						   padding: 0;
-						   background-color: #f4f4f4;
-						   text-align: center;'
-					>
-					<div
-						style='padding: 20px;
-							   background-color: #ffffff;
-							   border-radius: 5px;
-							   box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);'
-						>
-						<h1>이메일 인증</h1>
-						<p style='margin-bottom:30px;'>가입해 주셔서 감사합니다! 아래의 인증 코드를 사용하세요:</p>
-						<p
-							style='font-size: 24px;
-								   font-weight: bold;
-								   color: #007bff;'
-							>${signupVerifyToken}</p>
-						<p style='margin-top:30px;'>이 코드를 인증 페이지에서 입력하여 가입을 완료하세요.</p>
-						<p>만약 이 서비스에 가입하지 않으셨다면 이 이메일을 무시하셔도 됩니다.</p>
-						<p>감사합니다.</p>
-					</div>
-				</body>
-				</html>
-				`,
-			}),
-		);
+		const subject = `이메일 인증`;
+		const htmlContent = `<!DOCTYPE html>
+		<html lang="ko">
+		<head>
+			<meta charset="UTF-8">
+			<meta name="viewport" content="width=device-width, initial-scale=1.0">
+			<title>이메일 인증</title>
+		</head>
+		<body
+			style='font-family: Arial, sans-serif;
+				   margin: 0;
+				   padding: 0;
+				   background-color: #f4f4f4;
+				   text-align: center;'
+			>
+			<div
+				style='padding: 20px;
+					   background-color: #ffffff;
+					   border-radius: 5px;
+					   box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);'
+				>
+				<h1>이메일 인증</h1>
+				<p style='margin-bottom:30px;'>가입해 주셔서 감사합니다! 아래의 인증 코드를 사용하세요:</p>
+				<p
+					style='font-size: 24px;
+						   font-weight: bold;
+						   color: #007bff;'
+					>${signupVerifyToken}</p>
+				<p style='margin-top:30px;'>이 코드를 인증 페이지에서 입력하여 가입을 완료하세요.</p>
+				<p>만약 이 서비스에 가입하지 않으셨다면 이 이메일을 무시하셔도 됩니다.</p>
+				<p>감사합니다.</p>
+			</div>
+		</body>
+		</html>
+		`;
+		const sendResult = await Promise.allSettled([
+			await this.sendEmail(email, subject, subject, htmlContent),
+		]);
+
+		const newMailLog = this.createMailSendLogs(sendResult);
+
+		await this.mailSendLogRepository.createMailSendLog(newMailLog, qr);
 	}
 
 	private async sendEmail(
