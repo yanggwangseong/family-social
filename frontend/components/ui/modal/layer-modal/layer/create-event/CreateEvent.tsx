be5b53cb42d1e72751874795962bb5d29e9b827a
YangGwangSeong <rhkdtjd_12@naver.com>
2024-06-25 23:57:08 -0700

    :sparkles: feat(frontend): 이벤트 생성 form 추가 (#320)

--- frontend/components/ui/modal/layer-modal/layer/create-event/CreateEvent.tsx
+++ frontend/components/ui/modal/layer-modal/layer/create-event/CreateEvent.tsx
@@ -11,10 +11,25 @@ import { Report } from 'notiflix/build/notiflix-report-aio';
 import axios from 'axios';
 import { useMutation } from 'react-query';
 import { MediaService } from '@/services/media/media.service';
+import { SubmitHandler, useForm } from 'react-hook-form';
+import Field from '@/components/ui/field/Field';
+import FieldWithTextarea from '@/components/ui/field/field-area/FieldArea';
 
 const CreateEvent: FC = () => {
 	const [isEventImage, setIsEventImage] = useState<string>();
 	const FileInput = useRef<HTMLInputElement | null>(null);
+
+	const {
+		register,
+		formState: { errors, isValid },
+		handleSubmit,
+		reset,
+		getValues,
+		watch,
+		control,
+	} = useForm<{ eventName: string; eventDescription: string }>({
+		mode: 'onChange',
+	});
 
 	const { mutateAsync } = useMutation(
 		['group-event-image-upload'],
@@ -53,47 +68,88 @@ const CreateEvent: FC = () => {
 		FileInput.current!.click();
 	};
 
+	const onSubmit: SubmitHandler<{
+		eventName: string;
+		eventDescription: string;
+	}> = data => {
+		console.log('submit?');
+		//updateGroupSync(data);
+	};
+
 	return (
 		<LayerModalVariantWrapper className={styles.create_event_container}>
-			<div className={styles.contents_container}>
-				<div className={styles.top_container}>
-					<div className={styles.banner_img_container}>
-						<Image
-							className={styles.banner_img}
-							fill
-							src={'/images/banner/group-base.png'}
-							alt="banner"
-						></Image>
-						<div className={styles.banner_edit_btn}>
-							<PiPencilDuotone size={22} />
-							<button className={styles.btn_text} onClick={handleClick}>
-								수정
-							</button>
+			<form className={styles.create_form} onSubmit={handleSubmit(onSubmit)}>
+				<div className={styles.contents_container}>
+					<div className={styles.top_container}>
+						<div className={styles.banner_img_container}>
+							<Image
+								className={styles.banner_img}
+								fill
+								src={'/images/banner/group-base.png'}
+								alt="banner"
+							></Image>
+							<div className={styles.banner_edit_btn}>
+								<PiPencilDuotone size={22} />
+								<button className={styles.btn_text} onClick={handleClick}>
+									수정
+								</button>
 
-							<input
-								type="file"
-								id="fileUpload"
-								style={{ display: 'none' }}
-								onChange={handleGroupCoverImageUpload}
-								ref={FileInput}
-							/>
+								<input
+									type="file"
+									id="fileUpload"
+									style={{ display: 'none' }}
+									onChange={handleGroupCoverImageUpload}
+									ref={FileInput}
+								/>
+							</div>
+						</div>
+
+						{/* 프로필 */}
+						<Profile username="양광성" />
+
+						<div className={styles.field_container}>
+							<Field
+								fieldClass={'inline_input'}
+								labelText={'이벤트 이름'}
+								{...register('eventName', {
+									required: '이벤트 이름은 필수입니다!',
+									minLength: {
+										value: 2,
+										message: '최소 이름은 2자 이상입니다.',
+									},
+								})}
+								placeholder="이벤트 이름을 입력 해주세요!"
+								error={errors.eventName}
+							></Field>
+							<FieldWithTextarea
+								fieldClass={'inline_textarea'}
+								labelText={'이벤트 설명'}
+								{...register('eventDescription', {
+									maxLength: {
+										value: 1000,
+										message: '최대 1000자까지 가능합니다',
+									},
+								})}
+								placeholder="상세 정보를 추가하세요"
+								error={errors.eventDescription}
+							></FieldWithTextarea>
 						</div>
 					</div>
-					<Profile username="양광성" />
 				</div>
-			</div>
-			<div className={styles.button_container}>
-				<CustomButton
-					type="submit"
-					className="ml-auto bg-customOrange text-customDark 
+				<div className={styles.button_container}>
+					<CustomButton
+						type="submit"
+						className="ml-auto bg-customOrange text-customDark 
 								font-bold border border-solid border-customDark 
 								rounded-full p-[10px]
 								w-full hover:bg-orange-500
 								"
-				>
-					이벤트 만들기
-				</CustomButton>
-			</div>
+						disabled={!isValid}
+					>
+						이벤트 만들기
+					</CustomButton>
+				</div>
+			</form>
 		</LayerModalVariantWrapper>
 	);
 };
