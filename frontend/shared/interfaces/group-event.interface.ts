2275fe49ce5e45d4be965a9a529ae4a40745f281
YangGwangSeong <rhkdtjd_12@naver.com>
2024-06-26 23:59:15 -0700

    :sparkles: feat(frontend): 이벤트 리스트 가져오기위한 타입 정의 (#320)

--- frontend/shared/interfaces/group-event.interface.ts
+++ frontend/shared/interfaces/group-event.interface.ts
@@ -1,4 +1,6 @@
 import { Union, eventOptionsLists } from 'types';
+import { GroupProfileResponse } from './group.interface';
+import { SearchMemberResponse } from './member.interface';
 
 export interface CreateGroupEventRequest {
 	eventType: Union<typeof eventOptionsLists>;
@@ -9,4 +11,21 @@ export interface CreateGroupEventRequest {
 	eventStartTime: string;
 	eventEndDate?: string;
 	eventEndTime?: string;
+}
+
+export interface GroupEventItemResponse {
+	id: string;
+	eventType: Union<typeof eventOptionsLists>;
+	eventGroupId: string;
+	eventOrganizerId: string;
+	eventCoverImage: string;
+	eventName: string;
+	eventDescription: string;
+	eventStartDate: string;
+	eventStartTime: string;
+	eventEndDate?: string;
+	eventEndTime?: string;
+	createdAt: Date;
+	eventGroup: GroupProfileResponse;
+	eventOrganizer: SearchMemberResponse;
 }
