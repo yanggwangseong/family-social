7545563e00f09bc007f12614ba20ed03c155b698
YangGwangSeong <rhkdtjd_12@naver.com>
2024-07-06 21:16:28 -0700

    :sparkles: add(backend): updated-at-decorator 추가 (#272)

---
+++ backend/src/common/decorators/entity/updated-at.decorator.ts
@@ -0,0 +1,19 @@
+import { applyDecorators } from '@nestjs/common';
+import { ApiProperty } from '@nestjs/swagger';
+import { Transform } from 'class-transformer';
+import { UpdateDateColumn } from 'typeorm';
+
+export const UpdatedAtDecorator = () => {
+	return applyDecorators(
+		ApiProperty(),
+		Transform(({ value }: { value: Date }) => value.toISOString(), {
+			toPlainOnly: true,
+		}),
+		UpdateDateColumn({
+			type: 'timestamptz',
+			precision: 3,
+			default: () => 'CURRENT_TIMESTAMP',
+			// onUpdate: 'CURRENT_TIMESTAMP', mysql에서만 작동
+		}),
+	);
+};
