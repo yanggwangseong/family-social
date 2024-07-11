94c0531b59aa8706af8b42828a8f764364ff3bd2
YangGwangSeong <rhkdtjd_12@naver.com>
2024-07-10 23:57:16 -0700

    :sparkles: feat(backend): object-deepCopy 하는 mergeObjects 유틸 함수 추가 (#62)

---
+++ backend/src/utils/mergeTwoObj.ts
@@ -0,0 +1,37 @@
+export const mergeObjects = <T extends object = object>(
+	target: T,
+	...sources: T[]
+): T => {
+	if (!sources.length) {
+		return target;
+	}
+	const source = sources.shift();
+	if (source === undefined) {
+		return target;
+	}
+
+	if (isMergebleObject(target) && isMergebleObject(source)) {
+		Object.keys(source).forEach(function (key: string) {
+			const sourceTarget = (source as Record<string, any>)[key];
+			let targetItem = (target as Record<string, any>)[key];
+			if (isMergebleObject(sourceTarget)) {
+				if (!targetItem) {
+					targetItem = {};
+				}
+				mergeObjects(targetItem, sourceTarget);
+			} else {
+				targetItem = sourceTarget;
+			}
+		});
+	}
+
+	return mergeObjects(target, ...sources);
+};
+
+const isObject = (item: any): boolean => {
+	return item !== null && typeof item === 'object';
+};
+
+const isMergebleObject = (item: any): boolean => {
+	return isObject(item) && !Array.isArray(item);
+};
