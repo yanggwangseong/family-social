2522ae7258bae4f9d8241b2235bc672c0a1ecd0c
YangGwangSeong <rhkdtjd_12@naver.com>
2024-07-21 22:41:04 -0700

    :sparkles: feat(frontend): 문화시설 조회 detail-layer-modal 소개정보, 추가정보 추가 (#330)

--- frontend/components/ui/tour/cultural/Cultural.tsx
+++ frontend/components/ui/tour/cultural/Cultural.tsx
@@ -1,23 +1,69 @@
-import { useTourAdditionalExplanation } from '@/hooks/useTourAdditionalExplanation';
 import { TourIntroductionUnionType } from '@/shared/interfaces/tour.interface';
-import { isAdditionalCommon, isCultural } from '@/utils/type-guard';
+import { isCultural } from '@/utils/type-guard';
 import React, { FC } from 'react';
+import AdditionalCommon from '../additional-common/AdditionalCommon';
+import styles from './Cultural.module.scss';
+import {
+	IoChevronDownOutline,
+	IoChevronUp,
+	IoInformationCircle,
+} from 'react-icons/io5';
+import { useToggleState } from '@/hooks/useToggleState';
 
 // 문화시설 (14)
 const TourCultural: FC<{ list: TourIntroductionUnionType }> = ({ list }) => {
-	const { data } = useTourAdditionalExplanation();
+	const { isToggle, setIsToggle } = useToggleState();
 
 	return (
 		<>
-			{isCultural(list) &&
-				list.list.map((item, index) => (
-					<div key={index}>{item.chkcreditcardculture}</div>
-				))}
+			{isCultural(list) && (
+				<div className={styles.container}>
+					<div className={styles.top_container}>
+						<div>
+							<IoInformationCircle size={18} color="#0a0a0a" />
+						</div>
+						<div className={styles.title}>소개 정보:</div>
+						<div className={styles.btn_container} onClick={setIsToggle}>
+							{isToggle ? (
+								<IoChevronUp size={18} color="#0a0a0a" />
+							) : (
+								<IoChevronDownOutline size={18} color="#0a0a0a" />
+							)}
+						</div>
+					</div>
+					{isToggle && (
+						<>
+							{list.list.map((item, index) => (
+								<>
+									<div className={styles.introduction_container} key={index}>
+										<div className={styles.item_wrap}>
+											<div className={styles.item_title}>이용 요금</div>
+											<div className={styles.item_value}>{item.usefee}</div>
+											<div className={styles.item_title}>이용 시간</div>
+											<div className={styles.item_value}>
+												{item.usetimeculture}
+											</div>
+										</div>
+										<div className={styles.item_wrap}>
+											<div className={styles.item_title}>쉬는날</div>
+											<div className={styles.item_value}>
+												{item.restdateculture}
+											</div>
+											<div className={styles.item_title}>문의 및 안내</div>
+											<div className={styles.item_value}>
+												{item.infocenterculture}
+											</div>
+										</div>
+									</div>
+								</>
+							))}
+						</>
+					)}
+				</div>
+			)}
 
-			{data &&
-				isAdditionalCommon(data) &&
-				data.list.length > 0 &&
-				data.list.map((item, index) => <div key={index}>{item.infoname}</div>)}
+			{/* additional */}
+			<AdditionalCommon />
 		</>
 	);
 };
