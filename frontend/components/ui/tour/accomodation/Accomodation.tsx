da0b925f0f47dc054f5cda6a0e6481137ea53680
YangGwangSeong <rhkdtjd_12@naver.com>
2024-07-21 22:56:13 -0700

    :sparkles: feat(frontend): 숙박 조회 detail-layer-modal 소개정보, 추가정보 추가 (#330)

--- frontend/components/ui/tour/accomodation/Accomodation.tsx
+++ frontend/components/ui/tour/accomodation/Accomodation.tsx
@@ -1,25 +1,87 @@
-import { useTourAdditionalExplanation } from '@/hooks/useTourAdditionalExplanation';
+import { useToggleState } from '@/hooks/useToggleState';
 import { TourIntroductionUnionType } from '@/shared/interfaces/tour.interface';
-import { isAccomodation, isAdditionalAccomodation } from '@/utils/type-guard';
+import { isAccomodation } from '@/utils/type-guard';
 import React, { FC } from 'react';
+import AdditionalAccomodation from '../additional-accomodation/AdditionalAccomodation';
+import styles from './Accomodation.module.scss';
+import {
+	IoChevronDownOutline,
+	IoChevronUp,
+	IoInformationCircle,
+} from 'react-icons/io5';
 
 // 숙박 (32)
 const TourAccomodation: FC<{
 	list: TourIntroductionUnionType;
 }> = ({ list }) => {
-	const { data } = useTourAdditionalExplanation();
+	const { isToggle, setIsToggle } = useToggleState();
 
 	return (
 		<>
-			{isAccomodation(list) &&
-				list.list.map((item, index) => (
-					<div key={index}>{item.accomcountlodging}</div>
-				))}
+			{isAccomodation(list) && (
+				<div className={styles.container}>
+					<div className={styles.top_container}>
+						<div>
+							<IoInformationCircle size={18} color="#0a0a0a" />
+						</div>
+						<div className={styles.title}>소개 정보:</div>
+						<div className={styles.btn_container} onClick={setIsToggle}>
+							{isToggle ? (
+								<IoChevronUp size={18} color="#0a0a0a" />
+							) : (
+								<IoChevronDownOutline size={18} color="#0a0a0a" />
+							)}
+						</div>
+					</div>
+					{isToggle && (
+						<>
+							{list.list.map((item, index) => (
+								<>
+									<div className={styles.introduction_container} key={index}>
+										<div className={styles.item_wrap}>
+											<div className={styles.item_title}>입실시간</div>
+											<div className={styles.item_value}>
+												{item.checkintime}
+											</div>
+											<div className={styles.item_title}>퇴실시간</div>
+											<div className={styles.item_value}>
+												{item.checkouttime}
+											</div>
+										</div>
+										<div className={styles.item_wrap}>
+											<div className={styles.item_title}>객실내취사여부</div>
+											<div className={styles.item_value}>{item.chkcooking}</div>
+											<div className={styles.item_title}>베니키아여부</div>
+											<div className={styles.item_value}>{item.benikia}</div>
+										</div>
+										<div className={styles.item_wrap}>
+											<div className={styles.item_title}>바비큐장여부</div>
+											<div className={styles.item_value}>{item.barbecue}</div>
+											<div className={styles.item_title}>주차시설</div>
+											<div className={styles.item_value}>
+												{item.parkinglodging}
+											</div>
+										</div>
+										<div className={styles.item_wrap}>
+											<div className={styles.item_title}>예약안내홈페이지</div>
+											<div className={styles.item_value}>
+												{item.reservationurl}
+											</div>
+											<div className={styles.item_title}>문의 및 안내</div>
+											<div className={styles.item_value}>
+												{item.infocenterlodging}
+											</div>
+										</div>
+									</div>
+								</>
+							))}
+						</>
+					)}
+				</div>
+			)}
 
-			{data &&
-				isAdditionalAccomodation(data) &&
-				data.list.length > 0 &&
-				data.list.map((item, index) => <div key={index}>{item.roomcount}</div>)}
+			{/* additional */}
+			<AdditionalAccomodation />
 		</>
 	);
 };
