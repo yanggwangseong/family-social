ff3f289dc8408e02e4cb9c4774ff2b2ce71eac1a
YangGwangSeong <rhkdtjd_12@naver.com>
2024-07-21 21:44:15 -0700

    :sparkles: feat(frontend): 여행코스 조회 detail-layer-modal 소개정보, 추가정보 추가 (#330)

--- frontend/components/ui/tour/tour-course/TourCourse.tsx
+++ frontend/components/ui/tour/tour-course/TourCourse.tsx
@@ -1,21 +1,101 @@
+import { useIntroductionAndAdditional } from '@/hooks/useIntroductionAndAdditional';
 import { useTourAdditionalExplanation } from '@/hooks/useTourAdditionalExplanation';
 import { TourIntroductionUnionType } from '@/shared/interfaces/tour.interface';
 import { isAdditionalTourCourse, isTourCourse } from '@/utils/type-guard';
 import React, { FC } from 'react';
+import {
+	IoChevronDownOutline,
+	IoChevronUp,
+	IoInformationCircle,
+} from 'react-icons/io5';
+import styles from './TourCourse.module.scss';
 
 // 여행코스 (25)
 const TourCourse: FC<{ list: TourIntroductionUnionType }> = ({ list }) => {
 	const { data } = useTourAdditionalExplanation();
 
+	const { isIntroduction, setIsIntroduction, isAdditional, setIsAdditional } =
+		useIntroductionAndAdditional();
+
 	return (
 		<>
-			{isTourCourse(list) &&
-				list.list.map((item, index) => <div key={index}>{item.schedule}</div>)}
+			{isTourCourse(list) && (
+				<div className={styles.container}>
+					<div className={styles.top_container}>
+						<div>
+							<IoInformationCircle size={18} color="#0a0a0a" />
+						</div>
+						<div className={styles.title}>소개 정보:</div>
+						<div className={styles.btn_container} onClick={setIsIntroduction}>
+							{isIntroduction ? (
+								<IoChevronUp size={18} color="#0a0a0a" />
+							) : (
+								<IoChevronDownOutline size={18} color="#0a0a0a" />
+							)}
+						</div>
+					</div>
+					{isIntroduction && (
+						<>
+							{list.list.map((item, index) => (
+								<>
+									<div className={styles.introduction_container} key={index}>
+										<div className={styles.item_wrap}>
+											<div className={styles.item_title}>코스 테마</div>
+											<div className={styles.item_value}>{item.theme}</div>
+											<div className={styles.item_title}>코스총소요시간</div>
+											<div className={styles.item_value}>{item.taketime}</div>
+										</div>
+										<div className={styles.item_wrap}>
+											<div className={styles.item_title}>코스총거리</div>
+											<div className={styles.item_value}>{item.distance}</div>
+											<div className={styles.item_title}>문의 및 안내</div>
+											<div className={styles.item_value}>
+												{item.infocentertourcourse}
+											</div>
+										</div>
+									</div>
+								</>
+							))}
+						</>
+					)}
+				</div>
+			)}
 
-			{data &&
-				isAdditionalTourCourse(data) &&
-				data.list.length > 0 &&
-				data.list.map((item, index) => <div key={index}>{item.subname}</div>)}
+			{data && isAdditionalTourCourse(data) && data.list.length > 0 && (
+				<div className={styles.additional_container}>
+					<div className={styles.title_container}>
+						<div>
+							<IoInformationCircle size={18} color="#0a0a0a" />
+						</div>
+						<div className={styles.title}>추가 정보:</div>
+						<div className={styles.btn_container} onClick={setIsAdditional}>
+							{isAdditional ? (
+								<IoChevronUp size={18} color="#0a0a0a" />
+							) : (
+								<IoChevronDownOutline size={18} color="#0a0a0a" />
+							)}
+						</div>
+					</div>
+					{isAdditional && (
+						<>
+							{data.list.map((data, index) => (
+								<>
+									<div className={styles.item_wrap} key={index}>
+										<div className={styles.item_title}>코스명</div>
+										<div className={styles.item_value}>{data.subname}</div>
+									</div>
+									<div className={styles.item_wrap} key={index}>
+										<div className={styles.item_title}>코스개요</div>
+										<div className={styles.item_value}>
+											{data.subdetailoverview}
+										</div>
+									</div>
+								</>
+							))}
+						</>
+					)}
+				</div>
+			)}
 		</>
 	);
 };
--- frontend/components/ui/tour/tourist/Tourist.tsx
+++ frontend/components/ui/tour/tourist/Tourist.tsx
@@ -8,55 +8,60 @@ import {
 	IoInformationCircle,
 } from 'react-icons/io5';
 import { useTourAdditionalExplanation } from '@/hooks/useTourAdditionalExplanation';
+import { useIntroductionAndAdditional } from '@/hooks/useIntroductionAndAdditional';
 
 // 관광지조회 (12)
 const Tourist: FC<{ list: TourIntroductionUnionType }> = ({ list }) => {
 	const { data } = useTourAdditionalExplanation();
 
-	const [isIntroduction, setIsIntroduction] = useReducer(state => {
-		return !state;
-	}, false);
-
-	const [isAdditional, setIsAdditional] = useReducer(state => {
-		return !state;
-	}, false);
+	const { isIntroduction, setIsIntroduction, isAdditional, setIsAdditional } =
+		useIntroductionAndAdditional();
 
 	return (
 		<>
-			{isTourist(list) &&
-				list.list.map((item, index) => (
-					<div className={styles.tourist_container} key={index}>
-						<div className={styles.tourist_top_container}>
-							<div>
-								<IoInformationCircle size={18} color="#0a0a0a" />
-							</div>
-							<div className={styles.title}>소개 정보:</div>
-							<div className={styles.btn_container} onClick={setIsIntroduction}>
-								{isIntroduction ? (
-									<IoChevronUp size={18} color="#0a0a0a" />
-								) : (
-									<IoChevronDownOutline size={18} color="#0a0a0a" />
-								)}
-							</div>
+			{isTourist(list) && (
+				<div className={styles.tourist_container}>
+					<div className={styles.tourist_top_container}>
+						<div>
+							<IoInformationCircle size={18} color="#0a0a0a" />
 						</div>
-						{isIntroduction && (
-							<div className={styles.tourist_introduction_container}>
-								<div className={styles.item_wrap}>
-									<div className={styles.item_title}>이용시간</div>
-									<div className={styles.item_value}>{item.usetime}</div>
-									<div className={styles.item_title}>쉬는날</div>
-									<div className={styles.item_value}>{item.restdate}</div>
-								</div>
-								<div className={styles.item_wrap}>
-									<div className={styles.item_title}>개장일</div>
-									<div className={styles.item_value}>{item.opendate}</div>
-									<div className={styles.item_title}>문의 및 안내</div>
-									<div className={styles.item_value}>{item.infocenter}</div>
-								</div>
-							</div>
-						)}
+						<div className={styles.title}>소개 정보:</div>
+						<div className={styles.btn_container} onClick={setIsIntroduction}>
+							{isIntroduction ? (
+								<IoChevronUp size={18} color="#0a0a0a" />
+							) : (
+								<IoChevronDownOutline size={18} color="#0a0a0a" />
+							)}
+						</div>
 					</div>
-				))}
+					{isIntroduction && (
+						<>
+							{list.list.map((item, index) => (
+								<>
+									<div
+										className={styles.tourist_introduction_container}
+										key={index}
+									>
+										<div className={styles.item_wrap}>
+											<div className={styles.item_title}>이용시간</div>
+											<div className={styles.item_value}>{item.usetime}</div>
+											<div className={styles.item_title}>쉬는날</div>
+											<div className={styles.item_value}>{item.restdate}</div>
+										</div>
+										<div className={styles.item_wrap}>
+											<div className={styles.item_title}>개장일</div>
+											<div className={styles.item_value}>{item.opendate}</div>
+											<div className={styles.item_title}>문의 및 안내</div>
+											<div className={styles.item_value}>{item.infocenter}</div>
+										</div>
+									</div>
+								</>
+							))}
+						</>
+					)}
+				</div>
+			)}
+
 			{data && isAdditionalCommon(data) && data.list.length > 0 && (
 				<div className={styles.tourist_additional_container}>
 					<div className={styles.title_container}>
