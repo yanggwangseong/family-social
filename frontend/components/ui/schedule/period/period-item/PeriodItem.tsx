d953a6ea28f5fceb478f6682e49f587a1cbe53bb
YangGwangSeong <rhkdtjd_12@naver.com>
2024-07-29 23:07:31 -0700

    :sparkles: feat(frontend): 시작시간과 종료시간 변경시 periodAtom 전역 변수 변경 (#318)

--- frontend/components/ui/schedule/period/period-item/PeriodItem.tsx
+++ frontend/components/ui/schedule/period/period-item/PeriodItem.tsx
@@ -1,18 +1,22 @@
-import { PeriodsType } from '@/atoms/periodAtom';
+import { periodAtom, PeriodsType } from '@/atoms/periodAtom';
 import Field from '@/components/ui/field/Field';
 import { FormatDateToString } from '@/utils/formatDateToString';
 import { TranslateDateFormat } from '@/utils/translate-date-format';
 import { Report } from 'notiflix/build/notiflix-report-aio';
-import React, { ChangeEvent, FC, useState } from 'react';
+import React, { ChangeEvent, FC, useEffect, useState } from 'react';
 import styles from './PeriodItem.module.scss';
 import TimePicker from '@/components/ui/time-picker/TimePicker';
 import { useForm } from 'react-hook-form';
 import { isAfter, isBefore } from 'date-fns';
 import { stringToTime } from '@/utils/string-to-time';
+import { useRecoilState } from 'recoil';
 
 const PeriodItem: FC<{ period: PeriodsType }> = ({ period }) => {
 	//const [startTime, setStartTime] = useState(period.startTime || '10:00');
 	//const [endTime, setEndTime] = useState(period.endTime || '22:00');
+
+	const [isPeriods, setIsPeriods] = useRecoilState(periodAtom);
+
 	const date = FormatDateToString(period.period);
 
 	const {
@@ -56,6 +60,33 @@ const PeriodItem: FC<{ period: PeriodsType }> = ({ period }) => {
 		}
 
 		setValue(name, time);
+		handleChangePeriod();
+	};
+
+	const handleChangePeriod = () => {
+		const startTime = getValues('startTime');
+		const endTime = getValues('endTime');
+
+		const newPeriod = isPeriods.map(item => {
+			if (item.period === period.period) {
+				// tourism이 있다면 stayTime을 재조정
+
+				return {
+					...item,
+					startTime: TranslateDateFormat(
+						startTime ? startTime : new Date('10:00'),
+						'HH:mm',
+					),
+					endTime: TranslateDateFormat(
+						endTime ? endTime : new Date('22:00'),
+						'HH:mm',
+					),
+				};
+			}
+			return item;
+		});
+
+		setIsPeriods(newPeriod);
 	};
 
 	return (
