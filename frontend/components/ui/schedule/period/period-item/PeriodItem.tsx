90f424a92f047abc74461b13d77aaf8c10ee2d61
YangGwangSeong <rhkdtjd_12@naver.com>
2024-07-30 01:09:09 -0700

    :sparkles: feat(frontend): tourism-item이 존재 한다면 stayTime 재조정 (#318)

--- frontend/components/ui/schedule/period/period-item/PeriodItem.tsx
+++ frontend/components/ui/schedule/period/period-item/PeriodItem.tsx
@@ -3,11 +3,19 @@ import Field from '@/components/ui/field/Field';
 import { FormatDateToString } from '@/utils/formatDateToString';
 import { TranslateDateFormat } from '@/utils/translate-date-format';
 import { Report } from 'notiflix/build/notiflix-report-aio';
+import { Confirm } from 'notiflix/build/notiflix-confirm-aio';
 import React, { ChangeEvent, FC, useEffect, useState } from 'react';
 import styles from './PeriodItem.module.scss';
 import TimePicker from '@/components/ui/time-picker/TimePicker';
 import { useForm } from 'react-hook-form';
-import { isAfter, isBefore } from 'date-fns';
+import {
+	addMinutes,
+	differenceInMinutes,
+	format,
+	isAfter,
+	isBefore,
+	isEqual,
+} from 'date-fns';
 import { stringToTime } from '@/utils/string-to-time';
 import { useRecoilState } from 'recoil';
 
@@ -36,10 +44,14 @@ const PeriodItem: FC<{ period: PeriodsType }> = ({ period }) => {
 		},
 	});
 
+	const calculateTimeDifference = (startTime: Date, endTime: Date): number => {
+		return differenceInMinutes(endTime, startTime);
+	};
+
 	const hanldeChangeTime = (name: 'startTime' | 'endTime', time: Date) => {
 		if (name === 'endTime') {
 			const startTime = getValues('startTime');
-			if (isBefore(time, startTime)) {
+			if (isBefore(time, startTime) || isEqual(time, startTime)) {
 				Report.info(
 					`${TranslateDateFormat(date, 'MM/dd')} 종료시간`,
 					'종료 시간은 시작 시간 이후여야 합니다.',
@@ -49,7 +61,7 @@ const PeriodItem: FC<{ period: PeriodsType }> = ({ period }) => {
 			}
 		} else if (name === 'startTime') {
 			const endTime = getValues('endTime');
-			if (isAfter(time, endTime)) {
+			if (isAfter(time, endTime) || isEqual(time, endTime)) {
 				Report.info(
 					`${TranslateDateFormat(date, 'MM/dd')} 시작시간`,
 					'종료 시간은 시작 시간 이후여야 합니다.',
@@ -59,8 +71,29 @@ const PeriodItem: FC<{ period: PeriodsType }> = ({ period }) => {
 			}
 		}
 
-		setValue(name, time);
-		handleChangePeriod();
+		const updateTime = () => {
+			setValue(name, time);
+			handleChangePeriod();
+		};
+
+		if (period.tourisms && period.tourisms.length > 0) {
+			Confirm.show(
+				'관광 아이템 머무르는 시간',
+				`${TranslateDateFormat(
+					date,
+					'MM/dd',
+				)}일에 이미 설정한 관광 아이템의 머무르는 시간이 초기화 되어도 괜찮습니까?`,
+				'네',
+				'아니요',
+				updateTime,
+				() => {
+					return false;
+				},
+				{},
+			);
+		} else {
+			updateTime();
+		}
 	};
 
 	const handleChangePeriod = () => {
@@ -69,7 +102,36 @@ const PeriodItem: FC<{ period: PeriodsType }> = ({ period }) => {
 
 		const newPeriod = isPeriods.map(item => {
 			if (item.period === period.period) {
+				let updatedTourisms = item.tourisms;
 				// tourism이 있다면 stayTime을 재조정
+
+				if (item.tourisms && item.tourisms.length > 0) {
+					const totalTime = calculateTimeDifference(startTime, endTime);
+
+					const commonDivider = Math.floor(totalTime / item.tourisms.length);
+
+					const remainder = totalTime % item.tourisms.length;
+
+					updatedTourisms = [...item.tourisms]
+						.sort((a, b) => a.position - b.position)
+						.map((tourism, index) => {
+							const stayTimeInMinutes =
+								index === item.tourisms!.length - 1
+									? commonDivider + remainder
+									: commonDivider;
+
+							const hours = Math.floor(stayTimeInMinutes / 60);
+							const minutes = stayTimeInMinutes % 60;
+							const stayTime = `${String(hours).padStart(2, '0')}:${String(
+								minutes,
+							).padStart(2, '0')}`;
+
+							return {
+								...tourism,
+								stayTime,
+							};
+						});
+				}
 
 				return {
 					...item,
@@ -81,6 +143,7 @@ const PeriodItem: FC<{ period: PeriodsType }> = ({ period }) => {
 						endTime ? endTime : new Date('22:00'),
 						'HH:mm',
 					),
+					tourisms: updatedTourisms,
 				};
 			}
 			return item;
