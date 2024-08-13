262bb881cb602f008573d492661397e16a1c8138
YangGwangSeong <rhkdtjd_12@naver.com>
2024-08-12 23:51:51 -0700

    :sparkles: feat(backend): all-exception-filter에 winston logger 적용 (#253)

--- backend/src/common/filter/all-exception.filter.ts
+++ backend/src/common/filter/all-exception.filter.ts
@@ -1,6 +1,13 @@
-import { ArgumentsHost, Catch, ExceptionFilter } from '@nestjs/common';
+import {
+	ArgumentsHost,
+	Catch,
+	ExceptionFilter,
+	HttpException,
+} from '@nestjs/common';
+import { Request, Response } from 'express';
 
 import { ServiceException } from '../exception/service.exception';
+import { winstonLogger } from '../logger/winston';
 
 @Catch()
 export class AllExceptionFilter implements ExceptionFilter {
@@ -8,12 +15,31 @@ export class AllExceptionFilter implements ExceptionFilter {
 		if (exception instanceof ServiceException) {
 			// service-http-exception에서 처리
 			throw exception;
-		} else {
-			console.log(
-				'**************** exception을 ServiceException에서 catch 할 수 없는 에러들 ***********',
-				exception,
-			);
-			console.log('**************** host ***********', host);
 		}
+		/**
+		 * validation, guard, serviceException 등 개발자가 정의 하지 않은 에러
+		 * TypeOrm 에러나 예측하지 못한 에러들
+		 * 1. winston Log 파일에 기록
+		 * 2. slack 웹훅을 통한 실시간 알림
+		 */
+		const ctx = host.switchToHttp();
+		const request = ctx.getRequest<Request>();
+		const response = ctx.getResponse<Response>();
+		const { ip, method, originalUrl } = request;
+		const userAgent = request.get('user-agent');
+		const { statusCode } = response;
+
+		if (statusCode >= 400 && statusCode < 500)
+			winstonLogger.warn(
+				`[${method}]${originalUrl}(${statusCode}) ${ip} ${userAgent} ${
+					(exception as HttpException).message
+				}`,
+			);
+		else if (statusCode >= 500)
+			winstonLogger.error(
+				`[${method}]${originalUrl}(${statusCode}) ${ip} ${userAgent} ${
+					(exception as HttpException).message
+				}`,
+			);
 	}
 }
--- backend/src/common/middlewares/logger.middleware.ts
+++ backend/src/common/middlewares/logger.middleware.ts
@@ -1,10 +1,11 @@
 import { Injectable, NestMiddleware } from '@nestjs/common';
-import { NextFunction } from 'express';
+import { Request, Response, NextFunction } from 'express';
 
 @Injectable()
 export class LoggerMiddleware implements NestMiddleware {
 	use(req: Request & { now: number }, res: Response, next: NextFunction) {
 		req.now = Date.now();
+
 		next();
 	}
 }
