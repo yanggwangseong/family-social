6f31dfd5c34741ce7ce354f91f1baee387b4fa72
YangGwangSeong <rhkdtjd_12@naver.com>
2024-08-13 23:59:51 -0700

    :sparkles: feat(backend): all-exception-filter에서 TypeORM에러와 구분하기 (#253)

--- backend/src/common/filter/all-exception.filter.ts
+++ backend/src/common/filter/all-exception.filter.ts
@@ -4,18 +4,33 @@ import {
 	ExceptionFilter,
 	HttpException,
 } from '@nestjs/common';
-import { Request, Response } from 'express';
+import { Request } from 'express';
+import { QueryFailedError } from 'typeorm';
 
-import { ServiceException } from '../exception/service.exception';
+import { ERROR_INTERNAL_SERVER_ERROR } from '@/constants/business-error';
+
+import {
+	InternalServerErrorException,
+	ServiceException,
+} from '../exception/service.exception';
 import { winstonLogger } from '../logger/winston';
+
+export enum ErrorTypeEnum {
+	TYPEORM = 'TYPEORM',
+	OTHER = 'OTHER',
+}
 
 @Catch()
 export class AllExceptionFilter implements ExceptionFilter {
 	catch(exception: unknown, host: ArgumentsHost) {
+		/**
+		 * 사용자 정의 exception 일때 throw
+		 */
 		if (exception instanceof ServiceException) {
 			// service-http-exception에서 처리
 			throw exception;
 		}
+
 		/**
 		 * validation, guard, serviceException 등 개발자가 정의 하지 않은 에러
 		 * TypeOrm 에러나 예측하지 못한 에러들
@@ -24,22 +39,39 @@ export class AllExceptionFilter implements ExceptionFilter {
 		 */
 		const ctx = host.switchToHttp();
 		const request = ctx.getRequest<Request>();
-		const response = ctx.getResponse<Response>();
+
 		const { ip, method, originalUrl } = request;
 		const userAgent = request.get('user-agent');
-		const { statusCode } = response;
+
+		if (exception instanceof QueryFailedError) {
+			winstonLogger.error(
+				`[${ErrorTypeEnum.TYPEORM}][${method}]${originalUrl} ${ip} ${userAgent} - ${exception.message}`,
+			);
+
+			throw InternalServerErrorException(ERROR_INTERNAL_SERVER_ERROR);
+		}
+
+		const statusCode =
+			exception instanceof HttpException ? exception.getStatus() : 500;
 
 		if (statusCode >= 400 && statusCode < 500)
 			winstonLogger.warn(
-				`[${method}]${originalUrl}(${statusCode}) ${ip} ${userAgent} ${
+				`[${
+					ErrorTypeEnum.OTHER
+				}][${method}]${originalUrl}(${statusCode}) ${ip} ${userAgent} - ${
 					(exception as HttpException).message
 				}`,
 			);
-		else if (statusCode >= 500)
+		else if (statusCode >= 500) {
 			winstonLogger.error(
-				`[${method}]${originalUrl}(${statusCode}) ${ip} ${userAgent} ${
+				`[${
+					ErrorTypeEnum.OTHER
+				}][${method}]${originalUrl}(${statusCode}) ${ip} ${userAgent} - ${
 					(exception as HttpException).message
 				}`,
 			);
+		}
+
+		throw InternalServerErrorException(ERROR_INTERNAL_SERVER_ERROR);
 	}
 }
