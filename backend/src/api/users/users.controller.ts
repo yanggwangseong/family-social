ee41562251a95d1ed85c3bb525f88f8d86f39b72
YangGwangSeong <rhkdtjd_12@naver.com>
2024-08-14 23:09:31 -0700

    :heavy_plus_sign: add(backend): 모니터링을 위한 sentry 라이브러리 설치 (#253)

--- backend/src/api/users/users.controller.ts
+++ backend/src/api/users/users.controller.ts
@@ -3,6 +3,7 @@ import { plainToInstance } from 'class-transformer';
 
 import { LoggingInterceptor } from '@/common/interceptors/logging.interceptor';
 import { TimeoutInterceptor } from '@/common/interceptors/timeout.interceptor';
+import { winstonLogger } from '@/common/logger/winston';
 
 import { UsersResDto } from './users-res.dto';
 import { UsersService } from './users.service';
@@ -15,6 +16,9 @@ export class UsersController {
 	@Get()
 	async test() {
 		const user = await this.usersService.test();
+
+		winstonLogger.error('error level');
+		winstonLogger.warn('warn level');
 
 		return plainToInstance(UsersResDto, {
 			createdAt: user,
