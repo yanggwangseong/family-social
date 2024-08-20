9305fb5c4edd08fe8cdf27845c802d38e50d02bf
YangGwangSeong <rhkdtjd_12@naver.com>
2024-08-20 02:28:12 -0700

    :sparkles: feat(backend): seed 사용을 script 추가 (#340)

--- backend/data-source.ts
+++ backend/data-source.ts
@@ -1,15 +1,36 @@
-import { config } from 'dotenv';
+import path from 'path';
+
+import dotenv from 'dotenv';
 import { DataSource } from 'typeorm';
 
-config();
+import {
+	ENV_DB_DATABASE,
+	ENV_DB_PASSWORD,
+	ENV_DB_PORT,
+	ENV_DB_SOURCE_HOST,
+	ENV_DB_USERNAME,
+} from '@/constants/env-keys.const';
+
+dotenv.config({
+	path: path.resolve(
+		process.env.NODE_ENV === 'production'
+			? '.production.env'
+			: process.env.NODE_ENV === 'stage'
+			? '.stage.env'
+			: '.development.env',
+	),
+});
+
+if (process.env.NODE_ENV === 'development')
+	process.env.DB_HOST = process.env[ENV_DB_SOURCE_HOST];
 
 export default new DataSource({
 	type: 'postgres',
-	host: process.env.DB_HOST,
-	port: Number(process.env.DB_PORT),
-	username: process.env.DB_USERNAME,
-	password: process.env.DB_PASSWORD,
-	database: process.env.DB_DATABASE,
+	host: process.env.ENV_DB_HOST,
+	port: Number(process.env[ENV_DB_PORT]),
+	username: process.env[ENV_DB_USERNAME],
+	password: process.env[ENV_DB_PASSWORD],
+	database: process.env[ENV_DB_DATABASE],
 	synchronize: false,
 	entities: ['src/**/*.entity.ts'],
 	migrations: ['src/database/migrations/*.ts'],
