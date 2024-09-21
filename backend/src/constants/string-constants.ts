2aa0c1e4cecbb4c73f8333f80eae53200fe5788b
YangGwangSeong <rhkdtjd_12@naver.com>
2024-09-20 23:34:46 -0700

    :art: refactor:(backend): feeds-service 코드 리팩토링 (#369)

--- backend/src/constants/string-constants.ts
+++ backend/src/constants/string-constants.ts
@@ -1,5 +1,6 @@
 import { roleType } from '@/models/entities/fam.entity';
 
 export const MENTION_ON_COMMENT = 'mention_on_comment' as const;
+export const MENTION_ON_FEED = 'mention_on_feed' as const;
 
 export const FAM_ROLE_USER = 'user' as roleType;
