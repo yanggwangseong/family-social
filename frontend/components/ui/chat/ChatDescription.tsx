a22c965537bd4ef46025540a909b914c186c367a
YangGwangSeong <rhkdtjd_12@naver.com>
2024-09-29 23:00:30 -0700

    :sparkles: feat(frontend): 채팅 메시지 전송 시간 추가 (#375)

--- frontend/components/ui/chat/ChatDescription.tsx
+++ frontend/components/ui/chat/ChatDescription.tsx
@@ -1,8 +1,7 @@
 import React, { FC } from 'react';
 import styles from './ChatDescription.module.scss';
-import { format, parseISO } from 'date-fns';
-import { ko } from 'date-fns/locale';
 import { ChatListResponse } from '@/shared/interfaces/chat.interface';
+import ChatCreateDate from './chat-create-date/ChatCreateDate';
 
 const ChatDescription: FC<{ chat: ChatListResponse }> = ({ chat }) => {
 	return (
@@ -15,11 +14,7 @@ const ChatDescription: FC<{ chat: ChatListResponse }> = ({ chat }) => {
 					<div className={styles.chat_join_member_count}>
 						{chat.joinMemberCount > 2 && chat.joinMemberCount}
 					</div>
-					<div className={styles.chat_date}>
-						{format(parseISO(chat.recentMessage.createdAt), 'a h:mm', {
-							locale: ko,
-						})}
-					</div>
+					<ChatCreateDate createdAt={chat.recentMessage.createdAt} />
 				</div>
 				<div className={styles.chat_message}>{chat.recentMessage.message}</div>
 			</div>
