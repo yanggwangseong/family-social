afb69102eca5bf2b9cb9be488cc0f1bbc1bb02c7
YangGwangSeong <rhkdtjd_12@naver.com>
2024-10-12 03:01:24 -0700

    :sparkles: feat(backend): 그룹 팔로우 식별자 관계로 변경 (#301)

--- backend/src/models/entities/group.follow.entity.ts
+++ backend/src/models/entities/group.follow.entity.ts
@@ -1,18 +1,25 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsUUID } from 'class-validator';
-import { Column, Entity, Index, JoinColumn, ManyToOne, Unique } from 'typeorm';
+import {
+	Entity,
+	Index,
+	JoinColumn,
+	ManyToOne,
+	PrimaryColumn,
+	Unique,
+} from 'typeorm';
 
 import { notEmptyValidationMessage } from '@/common/validation-message/not-empty-validation-message';
 import { uuidValidationMessage } from '@/common/validation-message/uuid-validation-message';
 
-import { DefaultEntity } from './common/default.entity';
+import { CreatedUpdatedAtEntity } from './common/created-updated-at.entity';
 import { GroupEntity } from './group.entity';
 
 @Entity({ name: 'fam_group_follow' })
 @Unique(['followedGroupId', 'followingGroupId'])
 @Index(['followedGroupId'])
 @Index(['followingGroupId'])
-export class GroupFollowEntity extends DefaultEntity {
+export class GroupFollowEntity extends CreatedUpdatedAtEntity {
 	@ManyToOne(() => GroupEntity, (group) => group.following, {
 		onDelete: 'CASCADE',
 	})
@@ -24,7 +31,7 @@ export class GroupFollowEntity extends DefaultEntity {
 		message: notEmptyValidationMessage,
 	})
 	@IsUUID(4, { each: true, message: uuidValidationMessage })
-	@Column({ type: 'uuid', nullable: false })
+	@PrimaryColumn({ type: 'uuid', nullable: false })
 	followingGroupId!: string;
 
 	@ManyToOne(() => GroupEntity, (group) => group.followers, {
@@ -33,6 +40,6 @@ export class GroupFollowEntity extends DefaultEntity {
 	@JoinColumn({ name: 'followedGroupId' })
 	followedGroup!: GroupEntity;
 
-	@Column({ type: 'uuid', nullable: false })
+	@PrimaryColumn({ type: 'uuid', nullable: false })
 	followedGroupId!: string;
 }
