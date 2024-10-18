573c3867edb1307664e82322651eb4171066e0de
YangGwangSeong <rhkdtjd_12@naver.com>
2024-10-18 04:44:07 -0700

    :sparkles: feat(frontend):  intersectionObserver를 통해 무한 스크롤 이벤트 가져오기 (#322)

--- frontend/components/screens/group/event/GroupEvent.tsx
+++ frontend/components/screens/group/event/GroupEvent.tsx
@@ -1,7 +1,84 @@
+import Format from '@/components/ui/layout/Format';
+import styles from './GroupEvent.module.scss';
 import React, { FC } from 'react';
+import Header from '@/components/ui/header/Header';
+import GroupSidebar from '@/components/ui/layout/sidebar/group/GroupSidebar';
+import { useGroupEventItemIntersectionObserver } from '@/hooks/useGroupEventItemIntersectionObserver';
+import { GroupEventService } from '@/services/group-event/group-event.service';
+import { TranslateDateFormat } from '@/utils/translate-date-format';
+import Skeleton from '@/components/ui/skeleton/Skeleton';
+import { AnimatePresence } from 'framer-motion';
+import GroupEventItem from '@/components/ui/group/event-item/GroupEventItem';
+import NotFoundSearch from '@/components/ui/not-found/search/NotFoundSearch';
+import {
+	NOT_FOUND_GROUP_EVENT,
+	NOT_FOUND_TOUR_MESSAGE,
+} from '@/constants/index';
 
 const GroupEvent: FC = () => {
-	return <div>group event</div>;
+	const {
+		data,
+		fetchNextPage,
+		hasNextPage,
+		isLoading,
+		isError,
+		isRefetching,
+		refetch,
+	} = useGroupEventItemIntersectionObserver(
+		['get-group-events-by-belong-to-group'],
+		async ({ pageParam = 1 }) =>
+			await GroupEventService.getGroupEventsByBelongToGroup({
+				eventStartDate: TranslateDateFormat(new Date(), 'yyyy-MM-dd'),
+				page: pageParam,
+				limit: 10,
+				order: 'ASC',
+			}),
+	);
+
+	return (
+		<Format title={'group-events'}>
+			<div className={styles.container}>
+				{/* 헤더 */}
+				<Header />
+				<div className={styles.contents_container}>
+					<GroupSidebar />
+					<div className={styles.main_contents_container}>
+						<div className={styles.top_title_container}>
+							<div className={styles.top_title}>그룹 이벤트</div>
+						</div>
+						<div className={styles.group_event_container}>
+							{isLoading && <Skeleton />}
+
+							{data?.pages.map((page, pageIndex) => (
+								<AnimatePresence key={pageIndex}>
+									{page.list.map((event, index) => (
+										<GroupEventItem
+											key={event.id}
+											index={index}
+											page={page.page}
+											data={event}
+										/>
+									))}
+								</AnimatePresence>
+							))}
+
+							{isRefetching && (
+								<React.Fragment>
+									<Skeleton />
+									<Skeleton />
+									<Skeleton />
+								</React.Fragment>
+							)}
+
+							{data?.pages[0].list.length === 0 && (
+								<NotFoundSearch message={NOT_FOUND_GROUP_EVENT} />
+							)}
+						</div>
+					</div>
+				</div>
+			</div>
+		</Format>
+	);
 };
 
 export default GroupEvent;
--- frontend/constants/index.ts
+++ frontend/constants/index.ts
@@ -10,6 +10,7 @@ export const NOT_FOUND_MEMBER_MESSAGE = '해당 멤버를 찾을 수 없습니�
 export const NOT_FOUND_RECENT_MESSAGE = '최근 검색어가 없습니다.';
 export const NOT_FOUND_TOUR_MESSAGE = '해당 관광지를 찾을 수 없습니다.';
 export const NOT_FOUND_CHAT = '채팅 목록이 없습니다.';
+export const NOT_FOUND_GROUP_EVENT = '그룹 이벤트가 없습니다.';
 
 export const DEFAULT_CHAT_TYPE = 'DIRECT' satisfies Union<typeof ChatType>;
 export const DEFAULT_GROUP_CHAT_TYPE = 'GROUP' satisfies Union<typeof ChatType>;
