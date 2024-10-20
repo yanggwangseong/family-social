9cc49e08297172480e4c923ab05187d0e83f2976
YangGwangSeong <rhkdtjd_12@naver.com>
2024-10-19 23:51:46 -0700

    :sparkles: feat(frontend): 메인 오른쪽 사이드바 멤버탭에서 그룹 선택 selectbox 추가 (#388)

---
+++ frontend/components/ui/select/group/GroupSelect.tsx
@@ -0,0 +1,90 @@
+import React, { FC, useReducer } from 'react';
+import styles from './GroupSelect.module.scss';
+import { motion } from 'framer-motion';
+import { AiOutlineCheck, AiOutlineEye } from 'react-icons/ai';
+import { MdKeyboardArrowDown, MdKeyboardArrowUp } from 'react-icons/md';
+import {
+	toggleVariant,
+	toggleWrapperVariant,
+} from '@/utils/animation/toggle-variant';
+import { PiLightningDuotone } from 'react-icons/pi';
+import cn from 'classnames';
+import { MemberBelongToGroupsResponse } from '@/shared/interfaces/group.interface';
+
+const GroupSelect: FC<{
+	groupList: MemberBelongToGroupsResponse[];
+	selectedGroupId: string;
+	onSelectedGroupId: (groupId: string) => void;
+}> = ({ groupList, selectedGroupId, onSelectedGroupId }) => {
+	const [isSelectToggle, setIsSelectToggle] = useReducer(state => {
+		return !state;
+	}, false);
+
+	const handleSelectedGroupId = (groupId: string) => {
+		onSelectedGroupId(groupId);
+		setIsSelectToggle();
+	};
+
+	return (
+		<motion.div
+			className={styles.public_select_container}
+			initial={false}
+			animate={isSelectToggle ? 'open' : 'closed'}
+		>
+			<motion.div
+				className={styles.toggle_container}
+				onClick={setIsSelectToggle}
+				whileTap={{ scale: 0.97 }}
+			>
+				<div>
+					<AiOutlineEye size={22} />
+				</div>
+				<div className={styles.option_text}>그룹명이 와야 겠군</div>
+				<div>
+					{isSelectToggle ? (
+						<MdKeyboardArrowDown size={22} />
+					) : (
+						<MdKeyboardArrowUp size={22} />
+					)}
+				</div>
+			</motion.div>
+
+			<motion.div
+				className={styles.select_layer_modal_container}
+				variants={toggleWrapperVariant}
+				style={{ pointerEvents: isSelectToggle ? 'auto' : 'none' }}
+			>
+				<motion.div
+					className={styles.modal_title_container}
+					variants={toggleVariant}
+				>
+					<div>
+						<PiLightningDuotone size={22} />
+					</div>
+					<div className={styles.modal_title}>
+						선택된 그룹에 따라 멤버가 변경 됩니다
+					</div>
+				</motion.div>
+				{groupList.map((group, index) => (
+					<motion.div
+						key={index}
+						variants={toggleVariant}
+						className={cn(styles.select_item, {
+							[styles.active]: selectedGroupId === group.group.id,
+						})}
+						onClick={() => handleSelectedGroupId(group.group.id)}
+					>
+						{`${group.group.groupName}`}
+						{selectedGroupId === group.group.id && (
+							<div className={styles.icon_container}>
+								<AiOutlineCheck size={14} color="#e5855d" />
+							</div>
+						)}
+					</motion.div>
+				))}
+			</motion.div>
+		</motion.div>
+	);
+};
+
+export default GroupSelect;
