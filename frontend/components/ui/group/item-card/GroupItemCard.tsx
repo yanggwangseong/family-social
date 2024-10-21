f286be09b3ffd294c3b8e0bba2ca1e6190fd03d8
YangGwangSeong <rhkdtjd_12@naver.com>
2024-10-20 17:34:54 -0700

    :sparkles: feat(frontend): 그룹 페이지 내 그룹 페이지 수정 (#388)

--- frontend/components/ui/group/item-card/GroupItemCard.tsx
+++ frontend/components/ui/group/item-card/GroupItemCard.tsx
@@ -5,35 +5,28 @@ import GroupProfile from '@/ui/profile/group-profile/GroupProfile';
 import Link from 'next/link';
 import { motion } from 'framer-motion';
 import { INLINEBUTTONGESTURE } from '@/utils/animation/gestures';
+import { GroupItemCardProps } from './group-item-card.interface';
 
-const GroupItemCard: FC = () => {
+const GroupItemCard: FC<GroupItemCardProps> = ({ group }) => {
 	return (
-		<div className="w-full sm:w-1/2 px-2 py-3">
-			<div className="border border-solid border-customDark">
-				<GroupProfile
-					group={{
-						id: 'sdfsdf',
-						groupDescription: '양테스트를위한 그룹',
-						groupCoverImage: '/images/banner/sm/group-base-sm.png',
-						groupName: '양테스트',
-					}}
-				/>
-				<div className="p-3 w-full flex gap-2">
-					<motion.div className="w-2/3" {...INLINEBUTTONGESTURE}>
+		<div className={styles.group_item_card}>
+			<div className={styles.group_item_card_container}>
+				<GroupProfile group={group.group} />
+				<div className={styles.group_item_card_button_container}>
+					<motion.div
+						className={styles.group_item_card_button_container_button}
+						{...INLINEBUTTONGESTURE}
+					>
 						<Link
-							href={`/groups/33sdf`}
-							className="bg-customOrange text-customDark 
-							font-medium border border-solid border-customDark 
-							rounded px-2 py-1 text-base inline-block text-center
-							hover:bg-orange-500 w-full
-							"
+							href={`/groups/${group.group.id}`}
+							className={styles.group_item_card_button}
 						>
 							그룹 보기
 						</Link>
 					</motion.div>
 					<motion.div
 						{...INLINEBUTTONGESTURE}
-						className="w-1/3 items-center flex justify-center border border-solid border-customDark rounded cursor-pointer"
+						className={styles.group_icon_container}
 					>
 						<BsThreeDots size={24} />
 					</motion.div>
